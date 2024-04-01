993a42220f46e2f63e55a4e29dd7ca28cedf16bb
Lee <i876838@gmail.com>
2024-04-02 00:56:48 +0900

    baekjoon 1809

---
+++ baekjoon1809/baekjoon1809/baekjoon1809.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+		(___)
+		(o o)____ /
+		@@      \
+		\ ____, /
+		//   //
+		^^^^
+	*/
+
+	cout << "(___)\n(o o)____/\n @@      \\\n  \\ ____,/\n  //   //\n ^^   ^^\n";
+	//"(___)\n(o o)____/\n @@      \\\n  \\ ____,/\n  //   //\n ^^   ^^\n"puts
+
+	return 0;
+}
