2baa25f7116955cf52c62967fa3c310555686f03
Lee <i876838@gmail.com>
2024-04-06 21:48:44 +0900

    baekjoon 9086

---
+++ baekjoon9086/baekjoon9086/baekjoon9086.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	string s;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		cout << s[0] << s[s.size() - 1] << endl;
+	}
+
+	return 0;
+}
