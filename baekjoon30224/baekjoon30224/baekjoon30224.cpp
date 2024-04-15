ef21fcf0f9019134f1b225d6b159581b5346c383
Lee <i876838@gmail.com>
2024-04-15 22:15:29 +0900

    baekjoon 30224

---
+++ baekjoon30224/baekjoon30224/baekjoon30224.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	string s;
+	int result = -1, n = 0, flag = 0;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == '7') flag = 1;
+		n += (static_cast<int>(s[i]) - 48) * pow(10, s.size() - i - 1);
+	}
+	if (n % 7 == 0 && flag == 1) result = 3;
+	else if (n % 7 == 0 && flag != 1) result = 1;
+	else if (n % 7 != 0 && flag == 1) result = 2;
+	else result = 0;
+
+	cout << result << endl;
+
+	return 0;
+}
