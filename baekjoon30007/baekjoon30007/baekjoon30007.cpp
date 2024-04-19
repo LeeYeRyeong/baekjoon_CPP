107441f6351bbbbead7cb75da796feabb4addacc
Lee <i876838@gmail.com>
2024-04-19 21:18:49 +0900

    baekjoon 30007

---
+++ baekjoon30007/baekjoon30007/baekjoon30007.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// 필요한 물의 양 = 라면 계수 * (끓일 라면 수 - 1) + 기본 물의 양
+	// input : 라면계수 기본 물의 양 끓일 라면 수
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int count, water, noodle, need;
+		cin >> noodle >> water >> count;
+		cout << noodle * (count - 1) + water << endl;
+	}
+
+	return 0;
+}
