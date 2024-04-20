e1601b15623cef3ef2a0f21c1bd5478e7398e208
Lee <i876838@gmail.com>
2024-04-20 16:45:25 +0900

    baekjoon29863

---
+++ baekjoon29863/baekjoon29863/baekjoon29863.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sleep, wake;
+	cin >> sleep >> wake;
+	cout << (sleep > 3 ? (24 - sleep) + wake : wake - sleep) << endl;
+
+	return 0;
+}
