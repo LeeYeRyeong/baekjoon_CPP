4c182413f1bedceacea483337725160898fc29a6
Lee <i876838@gmail.com>
2024-04-22 21:31:14 +0900

    baekjoon 29731

---
+++ baekjoon29731/baekjoon29731/baekjoon29731.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string Rick[7] = { "Never gonna give you up" ,"Never gonna let you down","Never gonna run around and desert you","Never gonna make you cry","Never gonna say goodbye","Never gonna tell a lie and hurt you","Never gonna stop" };
+	string message;
+	int check[100] = {};
+	int n;
+
+	cin >> n;
+	cin.ignore();
+	
+	for (int i = 0; i < n; i++) {
+		getline(cin, message);
+		for (int j = 0; j < 7; j++) {
+			if (message == Rick[j]) {
+				check[i] = 1;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (check[i] == 0) {
+			cout << "Yes" << endl;
+			return 0;
+		}
+	}
+
+	cout << "No" << endl;
+
+	return 0;
+}
