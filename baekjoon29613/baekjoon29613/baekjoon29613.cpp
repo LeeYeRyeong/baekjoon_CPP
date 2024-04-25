b47687e68329d3a917c5aa07fdafd15e8eb72ee9
Lee <i876838@gmail.com>
2024-04-25 16:31:13 +0900

    baekjoon 29613

---
+++ baekjoon29613/baekjoon29613/baekjoon29613.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, odd = 0, even = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int number;
+		cin >> number;
+		if (number % 2 == 0) even++;
+		else odd++;
+	}
+
+	cout << (odd < even ? "Happy" : "Sad") << endl;
+
+	return 0;
+}
