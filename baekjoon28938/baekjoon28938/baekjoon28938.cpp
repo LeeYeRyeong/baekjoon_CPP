91761f3a03d748b7dde867ba02ef6f2170850ae4
Lee <i876838@gmail.com>
2024-04-25 16:39:57 +0900

    baekjoon 28938

---
+++ baekjoon28938/baekjoon28938/baekjoon28938.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, pos = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int number;
+		cin >> number;
+		pos += number;
+	}
+	if (pos == 0) cout << "Stay" << endl;
+	else if (pos < 0) cout << "Left" << endl;
+	else if (pos > 0) cout << "Right" << endl;
+
+	return 0;
+}
