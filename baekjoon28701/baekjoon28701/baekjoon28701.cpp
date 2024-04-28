2879ccf796d6a243a593fee554178ce439e851a7
Lee <i876838@gmail.com>
2024-04-28 22:11:52 +0900

    baekjoon 28701

---
+++ baekjoon28701/baekjoon28701/baekjoon28701.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n, count[3] = {};
+	cin >> n;
+	for (int i = 0; i < n; i++) count[0] += i + 1;
+	for (int i = 0; i < n; i++) count[1] = pow(count[0], 2);
+	for (int i = 0; i < n; i++) count[2] += pow(i + 1, 3);
+
+	for (int i = 0; i < 3; i++) cout << count[i] << endl;
+
+	return 0;
+}
