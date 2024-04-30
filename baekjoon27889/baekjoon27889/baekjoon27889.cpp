46d3d87897838b11261fa624e4d4a73faed7222c
Lee <i876838@gmail.com>
2024-04-30 21:09:20 +0900

    baekjoon 27889

---
+++ baekjoon27889/baekjoon27889/baekjoon27889.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s; 
+	string name[4] = { "North London Collegiate School","Branksome Hall Asia","Korea International School","St. Johnsbury Academy" };
+	cin >> s;
+	if (s == "NLCS") cout << name[0] << endl;
+	else if (s == "BHA") cout << name[1] << endl;
+	else if (s == "KIS") cout << name[2] << endl;
+	else if (s == "SJA") cout << name[3] << endl;
+
+	return 0;
+}
