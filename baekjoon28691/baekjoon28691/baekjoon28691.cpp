7f4b8ba1969147d1844d65d2f1c192d460d661c6
Lee <i876838@gmail.com>
2024-05-01 22:43:54 +0900

    baekjoon 28691

---
+++ baekjoon28691/baekjoon28691/baekjoon28691.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char c;
+	cin >> c;
+
+	if (c == 'M') cout << "MatKor" << endl;
+	else if (c == 'W') cout << "WiCys" << endl;
+	else if (c == 'C') cout << "CyKor" << endl;
+	else if (c == 'A') cout << "AlKor" << endl;
+	else if (c == '$') cout << "$clear" << endl;
+
+	return 0;
+}
