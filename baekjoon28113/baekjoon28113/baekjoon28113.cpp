3805aa1dfaaf59c324db46b73db8f3fe3ab9641a
Lee <i876838@gmail.com>
2024-05-03 22:46:32 +0900

    baekjoon 28113

---
+++ baekjoon28113/baekjoon28113/baekjoon28113.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, n, time;
+	cin >> n >> a >> b;
+	time = a - (n + b - n);
+	if (time == 0) cout << "Anything" << endl;
+	else if (time < 0) cout << "Bus" << endl;
+	else cout << "Subway" << endl;
+
+	return 0;
+}
