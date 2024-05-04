1698f66beb4320bdcf33dec10163e349e68d0e5f
Lee <i876838@gmail.com>
2024-05-04 22:18:46 +0900

    baekjoon 28444

---
+++ baekjoon28444/baekjoon28444/baekjoon28444.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int h, i, a, r, c, total = 0;
+	cin >> h >> i >> a >> r >> c;
+	total = (h * i) - (a * r * c);
+	cout << total << endl;
+
+	return 0;
+}
