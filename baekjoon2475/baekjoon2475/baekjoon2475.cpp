015c31d360b304ee9def66aafd9f8a7c9dbd3f4f
Lee <i876838@gmail.com>
2024-05-11 23:28:52 +0900

    baekjoon 2475

---
+++ baekjoon2475/baekjoon2475/baekjoon2475.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n[5];
+    int sum = 0;
+    for (int i = 0; i < 5; i++) cin >> n[i];
+    for (int i = 0; i < 5; i++) sum += (n[i] * n[i]);
+
+    cout << sum % 10 << endl;
+
+    return 0;
+}
