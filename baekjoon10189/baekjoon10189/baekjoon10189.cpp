5222cd84ebd64dd5eab473bd684723c18ce5d754
Lee <i876838@gmail.com>
2024-05-14 19:02:39 +0900

    baekjoon 10189

---
+++ baekjoon10189/baekjoon10189/baekjoon10189.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "#  # #### #### #  #" << endl << "#### #  # #  # # #" << endl << "#### #  # #  # # #" << endl << "#  # #### #### #  #" << endl;
+
+    return 0;
+}
