8e9b1aa02eb423a51c8122ff75433154c45e99a0
Lee <i876838@gmail.com>
2024-05-23 22:59:13 +0900

    baekjoon 5339

---
+++ baekjoon5339/baekjoon5339/baekjoon5339.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "     /~\\" << endl << "    ( oo|" << endl << "    _\\=/_" << endl << "   /  _  \\" << endl << "  //|/.\\|\\\\"
+		<< endl << " ||  \\ /  ||" << endl << "============" << endl << "|          |" << endl << "|          |" << endl << "|          |"
+		<< endl;
+	return 0;
+}
