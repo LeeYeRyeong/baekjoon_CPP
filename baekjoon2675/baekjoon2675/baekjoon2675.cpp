00c9102f07f2908c4ca2916746dea063a02bc6c7
Lee <i876838@gmail.com>
2024-05-26 15:17:08 +0900

    baekjoon2675

---
+++ baekjoon2675/baekjoon2675/baekjoon2675.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string word;
+		int count;
+		cin >> count >> word;
+		for (int j = 0; j < word.size(); j++) {
+			for (int k = 0; k < count; k++) {
+				cout << word[j];
+			}
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
