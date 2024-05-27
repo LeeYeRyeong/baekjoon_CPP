67782c4d37c6f8252793671e5c3ac58c2cced6a7
Lee <i876838@gmail.com>
2024-05-27 21:32:21 +0900

    baekjoon 11720

---
+++ baekjoon11720/baekjoon11720/baekjoon11720.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, sum = 0;
+	cin >> n;
+	cin >> s;
+	for (int i = 0; i < n; i++) sum += static_cast<int>(s[i] - 48);
+	cout << sum << endl;
+
+	return 0;
+}
