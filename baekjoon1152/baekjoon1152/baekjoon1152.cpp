3e3a5a7cd4603a5c460d709249357f68c5fdea4c
Lee <i876838@gmail.com>
2024-05-28 19:07:43 +0900

    baekjoon 1152

---
+++ baekjoon1152/baekjoon1152/baekjoon1152.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string sentence;
+	int count = 0, pos = 0;
+	getline(cin, sentence);
+	if (sentence[0] == ' ') pos++;
+	if (sentence[sentence.size() - 1] != ' ') sentence += ' ';
+	while (sentence[pos] != '\0') {
+		if (sentence[pos] == ' ') count++;
+		pos++;
+	}
+	cout << count << endl;
+
+	return 0;
+}
