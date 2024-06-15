b3961c3aa201d08e5145b45b67f030e36709d519
Lee <i876838@gmail.com>
2024-06-15 23:09:56 +0900

    baekjoon 1264

---
+++ baekjoon1264/baekjoon1264/baekjoon1264.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int count = 0;
+	while (s != "#") {
+		getline(cin, s);
+		if (s == "#") break;
+		for (int i = 0; s[i] != '\0'; i++) {
+			if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') count++;
+		}
+		cout << count << endl;
+		count = 0;
+	}
+
+	return 0;
+}
