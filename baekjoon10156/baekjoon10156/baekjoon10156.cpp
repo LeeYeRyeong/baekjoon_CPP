965b730128f239986440f492ebc428f3aca02dc7
Lee <i876838@gmail.com>
2024-06-16 23:20:38 +0900

    baekjoon 10156

---
+++ baekjoon10156/baekjoon10156/baekjoon10156.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k, n, m, money = 0;
+	cin >> k >> n >> m;
+	if (k * n > m) money = k * n - m;
+	cout << money << endl;
+
+	return 0;
+}
