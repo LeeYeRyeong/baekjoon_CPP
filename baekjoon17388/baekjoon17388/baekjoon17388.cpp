cc48fa09f9bb86feb7da42b8e716b89b71dc95b5
Lee <i876838@gmail.com>
2024-06-17 19:58:28 +0900

    baekjoon 17388

---
+++ baekjoon17388/baekjoon17388/baekjoon17388.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string msg = "OK";
+	int s, k, h;
+	cin >> s >> k >> h;
+	if (s + k + h < 100) {
+		msg = ((s < k ? s : k) < h ? (s < k ? "Soongsil" : "Korea") : "Hanyang");
+	}
+	cout << msg << endl;
+
+	return 0;
+}
