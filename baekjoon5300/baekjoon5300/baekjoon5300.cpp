2b533c6640c6c390e22358925d9fe84be6f04687
Lee <i876838@gmail.com>
2024-06-22 13:23:22 +0900

    baekjoon 5300

---
+++ baekjoon5300/baekjoon5300/baekjoon5300.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << i + 1 << " ";
+		if ((i + 1) % 6 == 0 || (i + 1 == n && (i + 1) % 6 != 0)) cout << "Go! ";
+	}
+	return 0;
+}
