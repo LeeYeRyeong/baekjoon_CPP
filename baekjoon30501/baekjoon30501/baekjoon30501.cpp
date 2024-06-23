bfad6ecac56a41bfce24ff06e95d8095a5e9cb1f
Lee <i876838@gmail.com>
2024-06-23 23:22:00 +0900

    baekjoon 30501

---
+++ baekjoon30501/baekjoon30501/baekjoon30501.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	string name, criminal;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> name;
+		for (int j = 0; name[j] != '\0'; j++) {
+			if (name[j] == 'S') {
+				criminal = name;
+				break;
+			}
+		}
+	}
+	cout << criminal << endl;
+	
+	return 0;
+}
