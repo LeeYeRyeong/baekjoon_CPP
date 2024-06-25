a9f225340bcff804f0c04edf538d1ba2d12f1664
Lee <i876838@gmail.com>
2024-06-25 22:23:53 +0900

    baekjoon 26532

---
+++ baekjoon26532/baekjoon26532/baekjoon26532.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned int n, m, seed = 0;
+	cin >> n >> m;
+	seed = ((n * m) % (4840 * 5) == 0 ? (n * m) / (4840 * 5) : (n * m) / (4840 * 5) + 1);
+	cout << seed << endl;
+
+	return 0;
+}
