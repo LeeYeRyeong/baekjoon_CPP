99cf7916ec40ff7903d6f4c32d9096c8c8cfe458
Lee <i876838@gmail.com>
2024-07-04 15:11:12 +0900

    baekjoon 20492

---
+++ baekjoon20492/baekjoon20492/baekjoon20492.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, result1 = 0, result2 = 0;
+	cin >> n;
+	result1 = n * 0.78;
+	result2 = n - (n * 0.2 * 0.22);
+	cout << result1 << endl << result2 << endl;
+
+	return 0;
+}
