2d98091dbd861d212a189e579a084e83b64c51cb
Lee <i876838@gmail.com>
2024-07-06 16:27:12 +0900

    baekjoon 29736

---
+++ baekjoon29736/baekjoon29736/baekjoon29736.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, k, x, count = 0;
+	cin >> a >> b >> k >> x;
+
+	for (int i = k - x; i <= k + x; i++) {
+		if (i<a || i>b) continue;
+		count++;
+	}
+
+	if (count > 0) cout << count << endl;
+	else cout << "IMPOSSIBLE" << endl;
+
+	return 0;
+}
