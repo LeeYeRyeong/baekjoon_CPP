1f1a6ccca1aa84ae6a7d363c2279d711613a754f
Lee <i876838@gmail.com>
2024-07-07 16:31:45 +0900

    baekjoon 30033

---
+++ baekjoon30033/baekjoon30033/baekjoon30033.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count = 0;
+	int plan[1000] = {}, study[1000] = {};
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> plan[i];
+	for (int i = 0; i < n; i++) cin >> study[i];
+
+	for (int i = 0; i < n; i++) {
+		if (plan[i] <= study[i]) count++;
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
