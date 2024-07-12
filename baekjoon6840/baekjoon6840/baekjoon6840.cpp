97a131cb6679c3e8acb00b4653e4c529bd035040
Lee <i876838@gmail.com>
2024-07-12 23:14:41 +0900

    baekjoon 6840

---
+++ baekjoon6840/baekjoon6840/baekjoon6840.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num[3] = {};
+	cin >> num[0] >> num[1] >> num[2];
+
+	for (int i = 0; i < 2; i++) {
+		if (num[i] > num[i + 1]) {
+			int temp = num[i];
+			num[i] = num[i + 1];
+			num[i + 1] = temp;
+		}
+	}
+
+	for (int i = 2; i > 0; i--) {
+		if (num[i] < num[i - 1]) {
+			int temp = num[i];
+			num[i] = num[i - 1];
+			num[i - 1] = temp;
+		}
+	}
+
+	cout << num[1] << endl;
+
+	return 0;
+}
