bb62cb2fe8f83e1cb8c72a4ac2f31972c289baac
Lee <i876838@gmail.com>
2024-07-15 22:15:47 +0900

    baekjonn 4504

---
+++ baekjoon4504/baekjoon4504/baekjoon4504.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, num;
+	cin >> n;
+	while (true) {
+		cin >> num;
+		if (num == 0) break;
+		if (num % n == 0) cout << num << " is a multiple of " << n << "." << endl;
+		else cout << num << " is NOT a multiple of " << n << "." << endl;
+	}
+
+	return 0;
+}
