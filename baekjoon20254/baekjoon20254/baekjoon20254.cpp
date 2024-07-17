1194f6f70b87620184f55f35839427e91f460de9
Lee <i876838@gmail.com>
2024-07-17 16:38:51 +0900

    baekjoon 20254

---
+++ baekjoon20254/baekjoon20254/baekjoon20254.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int ur, tr, uo, to, result = 0;
+	cin >> ur >> tr >> uo >> to;
+	result = 56 * ur + 24 * tr + 14 * uo + 6 * to;
+	cout << result << endl;
+
+	return 0;
+}
