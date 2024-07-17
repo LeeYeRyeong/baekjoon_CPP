4f8f641b1e2f9789953a1742b7703eb357ca51ad
Lee <i876838@gmail.com>
2024-07-17 16:38:15 +0900

    baekjoon 20233

---
+++ baekjoon20233/baekjoon20233/baekjoon20233.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, x, b, y, t;
+	int aCost = 0, bCost = 0;
+	cin >> a >> x >> b >> y >> t;
+	aCost = ((t - 30) > 0 ? t - 30 : 0) * x * 21 + a;
+	bCost = ((t - 45) > 0 ? t - 45 : 0) * y * 21 + b;
+	cout << aCost << " " << bCost << endl;
+
+	return 0;
+}
