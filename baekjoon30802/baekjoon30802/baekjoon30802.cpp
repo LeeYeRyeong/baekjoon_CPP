08c2f69564cded15390be78a0e06d1f9410a8cfe
Lee <i876838@gmail.com>
2024-07-21 16:27:47 +0900

    baekjoon 30802

---
+++ baekjoon30802/baekjoon30802/baekjoon30802.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, t, p, resultT = 0;
+	int size[6] = {};
+	cin >> n;
+	for (int i = 0; i < 6; i++) cin >> size[i];
+	cin >> t >> p;
+	
+	for (int i = 0; i < 6; i++) {
+		if (size[i] == 0) continue;
+		resultT += (size[i] % t == 0 ? size[i] / t : size[i] / t + 1);
+	}
+
+	cout << resultT << endl << n / p << " " << n % p << endl;
+
+	return 0;
+}
