bf17af4c659ca64787be0b475dbe46b2600b94b8
Lee <i876838@gmail.com>
2024-07-23 23:32:02 +0900

    baekjoon 4153

---
+++ baekjoon4153/baekjoon4153/baekjoon4153.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	long int a, b, c;
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) return 0;
+		a = a * a;
+		b = b * b;
+		c = c * c;
+		if (a + b == c || a + c == b || b + c == a) cout << "right" << endl;
+		else cout << "wrong" << endl;
+	}
+
+	return 0;
+}
