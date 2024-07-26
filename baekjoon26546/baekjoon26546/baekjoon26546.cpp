488ad0affe06f7765af1b977e2dc11181e81d3fc
Lee <i876838@gmail.com>
2024-07-26 23:07:35 +0900

    baekjoon 26546

---
+++ baekjoon26546/baekjoon26546/baekjoon26546.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n, start, finish;
+	string msg;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> msg >> start >> finish;
+		for (int j = 0; msg[j] != '\0'; j++) {
+			if (j >= start && j < finish) continue;
+			cout << msg[j];
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
