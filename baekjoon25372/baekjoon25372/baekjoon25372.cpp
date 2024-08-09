e2629555971ddcc2da74d2510cbb42b2eea55231
Lee <i876838@gmail.com>
2024-08-09 17:04:07 +0900

    baekjoon 25372

---
+++ baekjoon25372/baekjoon25372/baekjoon25372.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n;
+	string pw;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> pw;
+		if (pw.size() >= 6 && pw.size() <= 9) cout << "yes" << endl;
+		else cout << "no" << endl;
+	}
+
+	return 0;
+}
