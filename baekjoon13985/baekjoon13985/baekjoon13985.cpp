aee14f6bff94296077ce573db524a2a0c34ec575
Lee <i876838@gmail.com>
2024-08-10 15:31:43 +0900

    baekjoon 13985

---
+++ baekjoon13985/baekjoon13985/baekjoon13985.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	char plus, equal;
+	cin >> a >> plus >> b >> equal >> c;
+	cout << (a + b == c ? "YES" : "NO") << endl;
+
+	return 0;
+}
