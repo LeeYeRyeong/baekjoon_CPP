ba26c01dd2820362b69a65fca5e54ec66a4b4549
Lee <i876838@gmail.com>
2024-08-26 23:18:27 +0900

    baekjoon 21300

---
+++ baekjoon21300/baekjoon21300/baekjoon21300.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int result = 0;
+	int num[6] = {};
+	for (int i = 0; i < 6; i++) cin >> num[i];
+	for (int i = 0; i < 6; i++) result += num[i] * 5;
+
+	cout << result << endl;
+
+	return 0;
+}
