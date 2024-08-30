97b7016860473408ff896bf01df7cf9ede5571df
ryxxng <i876838@gmail.com>
2024-08-30 14:56:14 +0900

    baekjoon14581

---
+++ baekjoon14581/baekjoon14581/baekjoon14581.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	cin >> name;
+	cout << ":fan::fan::fan:" << endl << ":fan::" << name << "::fan:" << endl << ":fan::fan::fan:" << endl;
+
+	return 0;
+}
