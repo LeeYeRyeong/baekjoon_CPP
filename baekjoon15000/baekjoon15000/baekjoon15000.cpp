52a4497b0b0eb1fdf840881b4ee5a0f78a30ceac
ryxxng <i876838@gmail.com>
2024-08-31 14:35:11 +0900

    baekjoon 15000

---
+++ baekjoon15000/baekjoon15000/baekjoon15000.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) cout << static_cast<char>(toupper(s[i]));
+
+	return 0;
+}
