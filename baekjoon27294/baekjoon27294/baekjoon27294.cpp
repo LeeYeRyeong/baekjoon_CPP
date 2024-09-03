f299fa7e19ad3b0134f87acf149c98fa2c52bc88
ryxxng <i876838@gmail.com>
2024-09-03 16:38:50 +0900

    baekjoon27294

---
+++ baekjoon27294/baekjoon27294/baekjoon27294.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, s;
+	cin >> t >> s;
+	if (s == 0 && (t > 11 && t <= 16)) cout << 320 << endl;
+	else cout << 280 << endl;
+
+	return 0;
+}
