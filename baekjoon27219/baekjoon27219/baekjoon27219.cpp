9aec91ae4307e7de2e3f8e92f5cfdf4df93b5552
ryxxng <i876838@gmail.com>
2024-09-06 23:56:49 +0900

    baekjoon 27219

---
+++ baekjoon27219/baekjoon27219/baekjoon27219.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, countV, countI;
+	cin >> n;
+	countV = n / 5;
+	countI = n % 5;
+	for (int i = 0; i < countV; i++) cout << "V";
+	for (int i = 0; i < countI; i++) cout << "I";
+	
+	return 0;
+}
