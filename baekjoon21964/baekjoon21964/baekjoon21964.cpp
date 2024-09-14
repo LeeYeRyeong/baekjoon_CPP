0c2ed2fde77885ddaf2dbced641c67d2a82d54d5
ryxxng <i876838@gmail.com>
2024-09-14 16:38:03 +0900

    baekjoon 21964

---
+++ baekjoon21964/baekjoon21964/baekjoon21964.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int size;
+	string s;
+	cin >> size >> s;
+	for (int i = 5; i > 0; i--) cout << static_cast<char>(s[size - i]);
+
+	return 0;
+}
