281be58041953ed788c486418d9a4abeae26d87b
ryxxng <i876838@gmail.com>
2024-10-03 15:01:45 +0900

    baekjoon 24736

---
+++ baekjoon24736/baekjoon24736/baekjoon24736.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int goal[5] = { 6,3,2,1,2 };
+	int score1 = 0, score2 = 0, n;
+	for (int i = 0; i < 5; i++) {
+		cin >> n;
+		score1 += n * goal[i];
+	}
+	for (int i = 0; i < 5; i++) {
+		cin >> n;
+		score2 += n * goal[i];
+	}
+	cout << score1 << " " << score2 << endl;
+
+	return 0;
+}
