58ff5f1417ece89d2bcf6aa8c45e5b1d85241f7a
ryxxng <i876838@gmail.com>
2024-10-04 14:14:22 +0900

    baekjoon 10039

---
+++ baekjoon10039/baekjoon10039/baekjoon10039.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int score, sum = 0;
+	for (int i = 0; i < 5; i++) {
+		cin >> score;
+		score = (score < 40 ? 40 : score);
+		sum += score;
+	}
+	cout << sum / 5 << endl;
+
+	return 0;
+}
