12905ca660b51423867ea1fe5409df561d2def5b
ryxxng <i876838@gmail.com>
2024-10-05 15:07:06 +0900

    baekjoon 24082

---
+++ baekjoon24082/baekjoon24082/baekjoon24082.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    long long int result;
+    int n;
+    cin >> n;
+    result = pow(n, 3);
+    cout << result << endl;
+
+    return 0;
+}
