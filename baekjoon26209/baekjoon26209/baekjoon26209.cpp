2cbb7dc4ca8a13a4e5d4e9ebeaf47a6ac56defb3
ryxxng <i876838@gmail.com>
2024-10-09 23:49:18 +0900

    baekjoon 26209

---
+++ baekjoon26209/baekjoon26209/baekjoon26209.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string result;
+    int n;
+    for (int i = 0; i < 8; i++) {
+        cin >> n;
+        if (n == 9) {
+            cout << "F" << endl;
+            return 0;
+        }
+    }
+    cout << "S" << endl;
+    return 0;
+}
