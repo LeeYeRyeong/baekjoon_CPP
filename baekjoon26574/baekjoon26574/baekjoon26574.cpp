eb7fb5ab2d1609f85bbecaf455deb3d7776ce3d5
ryxxng <i876838@gmail.com>
2024-10-12 16:50:38 +0900

    baekjoon 26574

---
+++ baekjoon26574/baekjoon26574/baekjoon26574.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		cout << num << " " << num << endl;
+	}
+
+	return 0;
+}
