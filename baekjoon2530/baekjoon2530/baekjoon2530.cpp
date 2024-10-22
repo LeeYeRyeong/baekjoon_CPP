89192db68cede97eecf013d2245bc0d9c6045c62
ryxxng <i876838@gmail.com>
2024-10-22 22:35:17 +0900

    baekjoon 2530

---
+++ baekjoon2530/baekjoon2530/baekjoon2530.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int h, m, s, time;
+	cin >> h >> m >> s >> time;
+	s += time;
+	if (s >= 60) {
+		m += s / 60;
+		s = s % 60;
+	}
+	if (m >= 60) {
+		h += m / 60;
+		m %= 60;
+	}
+	if (h >= 24)h %= 24;
+	cout << h << " " << m << " " << s << " " << endl;
+
+	return 0;
+}
