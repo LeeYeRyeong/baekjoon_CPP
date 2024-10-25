0cc6230c6b7208735042cd812f2c28726e383f70
ryxxng <i876838@gmail.com>
2024-10-25 17:06:47 +0900

    baekjoon 19944

---
+++ baekjoon19944/baekjoon19944/baekjoon19944.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	if (m == 1 || m == 2) cout << "NEWBIE!" << endl;
+	else if (n >= m) cout << "OLDBIE!" << endl;
+	else cout << "TLE!" << endl;
+
+	return 0;
+}
