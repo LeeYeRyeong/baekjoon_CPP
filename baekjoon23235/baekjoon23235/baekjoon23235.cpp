8a9fa02e05785da4f9788d2b3aaeaaf73205b8f0
ryxxng <i876838@gmail.com>
2024-11-01 17:21:28 +0900

    baekjoon 23235

---
+++ baekjoon23235/baekjoon23235/baekjoon23235.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 1, tmp = 1;
+	int arr[100] = {};
+	while (1) {
+		cin >> n;
+		if (n == 0) break;
+		for (int i = 0; i < n; i++) cin >> arr[i];
+		cout << "Case " << tmp++ << ": Sorting... done!" << endl;
+	}
+	return 0;
+}
