f54cbcb6749323c2538404832c1ea18197378957
ryxxng <i876838@gmail.com>
2024-11-02 16:48:15 +0900

    baekjoon 31428

---
+++ baekjoon31428/baekjoon31428/baekjoon31428.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count = 0;
+	char trackList[10000] = {};
+	char track;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> trackList[i];
+	cin >> track;
+	for (int i = 0; i < n; i++) {
+		if (trackList[i] == track) count++;
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
