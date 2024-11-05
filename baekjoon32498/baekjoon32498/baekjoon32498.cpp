2195890b6f85f6496dbfcdecf2f203873a1c1b7b
ryxxng <i876838@gmail.com>
2024-11-05 21:25:33 +0900

    baekjoon 32498

---
+++ baekjoon32498/baekjoon32498/baekjoon32498.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, problem, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> problem;
+		if (problem % 2 != 0) count++;
+	}
+
+	cout << count << endl;
+
+	return 0;
+}
