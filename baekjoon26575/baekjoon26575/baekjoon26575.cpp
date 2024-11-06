e4daa955b47a391084337a35f829e27be4bf6fa4
ryxxng <i876838@gmail.com>
2024-11-06 16:07:32 +0900

    baekjoon 26575

---
+++ baekjoon26575/baekjoon26575/baekjoon26575.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		double d, f, p;
+		cin >> d >> f >> p;
+		cout.precision(2);
+		cout << "$" << fixed << d * f * p << endl;
+	}
+
+	return 0;
+}
