92d1535346c57a62e895e4b031e66aad0fbcc23f
ryxxng <i876838@gmail.com>
2024-11-13 16:56:39 +0900

    baekjoon 2752

---
+++ baekjoon2752/baekjoon2752/baekjoon2752.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, temp;
+	cin >> a >> b >> c;
+	if (a > c) {
+		temp = a;
+		a = c;
+		c = temp;
+	}
+	if (a > b) {
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	if (b > c) {
+		temp = b;
+		b = c;
+		c = temp;
+	}
+	cout << a << " " << b << " " << c << endl;
+
+	return 0;
+}
