4784b7e6c5200adf58a85af32d8b504ad70c4545
ryxxng <i876838@gmail.com>
2024-11-14 14:27:13 +0900

    baekjoon 2845

---
+++ baekjoon2845/baekjoon2845/baekjoon2845.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int l, p;
+	int count[5] = {};
+	cin >> l >> p;
+	for (int i = 0; i < 5; i++) cin >> count[i];
+	for (int i = 0; i < 5; i++) cout << count[i] - (l * p) << " ";
+
+	return 0;
+}
