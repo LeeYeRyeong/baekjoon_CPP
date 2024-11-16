0c03e9fadef1957db2c5b4a65b5b11d13d4362e6
ryxxng <i876838@gmail.com>
2024-11-17 01:47:44 +0900

    baekjoon 4299

---
+++ baekjoon4299/baekjoon4299/baekjoon4299.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int plus, minus, one, two;
+	cin >> plus >> minus;
+	if (minus < 0 || plus < minus) cout << -1;
+	else {
+		one = (plus + minus) / 2;
+		two = plus - one;
+		if (one + two == plus && one - two == minus) cout << one << " " << two;
+		else cout << -1;
+	}
+
+	return 0;
+}
