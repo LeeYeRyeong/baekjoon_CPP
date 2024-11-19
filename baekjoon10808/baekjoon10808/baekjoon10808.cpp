d8db9c77cbc92887ee8ffb2e08fa560dedcad77a
ryxxng <i876838@gmail.com>
2024-11-19 15:20:45 +0900

    baekjoon 10808

---
+++ baekjoon10808/baekjoon10808/baekjoon10808.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int count[26] = {};
+	getline(cin, s);
+	for (int i = 0; i < s.size(); i++) count[static_cast<int>(s[i] - 'a')]++;
+	for (int i = 0; i < 26; i++) cout << count[i] << " ";
+
+	return 0;
+}
