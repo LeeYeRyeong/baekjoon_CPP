b02b9daa7544e8dff6186b2df03ea3a7b199f74e
ryxxng <i876838@gmail.com>
2024-11-20 17:10:36 +0900

    baekjoon 5543

---
+++ baekjoon5543/baekjoon5543/baekjoon5543.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int buger1, buger2, buger3, drink1, drink2, setBuger, setDrink;
+	cin >> buger1 >> buger2 >> buger3 >> drink1 >> drink2;
+	setBuger = ((buger1 < buger2 ? buger1 : buger2) < buger3 ? (buger1 < buger2 ? buger1 : buger2) : buger3);
+	setDrink = (drink1 < drink2 ? drink1 : drink2);
+	cout << setBuger + setDrink - 50 << endl;
+
+	return 0;
+}
