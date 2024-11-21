3bae4ee7cf2ba21a4709b7901d660f6184de63c6
ryxxng <i876838@gmail.com>
2024-11-22 03:07:33 +0900

    baekjoon 5524

---
+++ baekjoon5524/baekjoon5524/baekjoon5524.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) cout << static_cast<char>(tolower(s[j]));
+		cout << endl;
+	}
+
+	return 0;
+}
