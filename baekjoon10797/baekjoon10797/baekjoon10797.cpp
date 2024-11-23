648bba56d5a585253c622d03b0411328a7b01aa4
ryxxng <i876838@gmail.com>
2024-11-24 01:46:47 +0900

    baekjoon 10797

---
+++ baekjoon10797/baekjoon10797/baekjoon10797.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int car[5] = {};
+	int n, count = 0;
+	cin >> n;
+	for (int i = 0; i < 5; i++) cin >> car[i];
+	for (int i = 0; i < 5; i++) if (car[i] == n) count++;
+	cout << count;
+
+	return 0;
+}
