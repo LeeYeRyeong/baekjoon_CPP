5ed06b829362d0a7a0493b45886f372d2681ae12
ryxxng <i876838@gmail.com>
2024-11-25 14:54:24 +0900

    baekjoon 5554

---
+++ baekjoon5554/baekjoon5554/baekjoon5554.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, time = 0;
+	cin >> a >> b >> c >> d;
+	time = a + b + c + d;
+	cout << static_cast<int>(time / 60) << endl << time % 60;
+
+	return 0;
+}
