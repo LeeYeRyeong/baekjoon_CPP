e3f9b76979d0eb08379897c639b3042d2e01952d
ryxxng <i876838@gmail.com>
2024-11-27 16:12:42 +0900

    baekjoon 5596

---
+++ baekjoon5596/baekjoon5596/baekjoon5596.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int score, total1 = 0, total2 = 0;
+	for (int i = 0; i < 4; i++) {
+		cin >> score;
+		total1 += score;
+	}
+	for (int i = 0; i < 4; i++) {
+		cin >> score;
+		total2 += score;
+	}
+	cout << (total1 > total2 ? total1 : total2);
+
+	return 0;
+}
