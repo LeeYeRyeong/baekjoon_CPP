794a14e1d96595cdf16ed9c1672f4e08c4c27014
ryxxng <i876838@gmail.com>
2024-11-28 16:04:45 +0900

    baekjoon 5532

---
+++ baekjoon5532/baekjoon5532/baekjoon5532.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int l, a, b, c, d, date;
+	cin >> l >> a >> b >> c >> d;
+	date = (a % c != 0 ? a / c + 1 : a / c) > (b % d != 0 ? b / d + 1 : b / d) ? (a % c != 0 ? a / c + 1 : a / c) : (b % d != 0 ? b / d + 1 : b / d);
+	cout << l - date << endl;
+
+	return 0;
+}
