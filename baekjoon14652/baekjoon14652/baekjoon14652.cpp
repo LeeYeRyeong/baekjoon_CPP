b84b7847e230bca566193efc1ea17d50936d3023
ryxxng <i876838@gmail.com>
2024-12-06 16:37:37 +0900

    baekjoon 14652

---
+++ baekjoon14652/baekjoon14652/baekjoon14652.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, k, count = 0;
+	cin >> n >> m >> k;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (count++ == k) {
+				cout << i << " " << j;
+				break;
+			}
+			//count++;
+		}
+		if (count > k) break;
+	}
+
+	return 0;
+}
