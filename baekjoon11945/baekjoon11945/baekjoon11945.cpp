79f27066cd2972d393a617e48b618db109cf9fa2
ryxxng <i876838@gmail.com>
2024-12-07 16:24:41 +0900

    baekjoon 11945

---
+++ baekjoon11945/baekjoon11945/baekjoon11945.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, size;
+	cin >> n >> size;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = size - 1; j >= 0; j--) cout << s[j];
+		cout << endl;
+	}
+	return 0;
+}
