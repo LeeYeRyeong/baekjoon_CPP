808a7b27d065a1b3a748dccefe4a96eb865ebca5
ryxxng <i876838@gmail.com>
2024-12-09 15:12:32 +0900

    baekjoon 32929

---
+++ baekjoon32929/baekjoon32929/baekjoon32929.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n % 3 == 0) cout << "S";
+	else if (n % 3 == 1) cout << "U";
+	else cout << "O";
+
+	return 0;
+}
