23c9b74f4623bb5c9972bb36a9124a636924b4b4
ryxxng <i876838@gmail.com>
2024-12-10 20:44:32 +0900

    baekjoon 9295

---
+++ baekjoon9295/baekjoon9295/baekjoon9295.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, dice1, dice2;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> dice1 >> dice2;
+		cout << "Case " << i + 1 << ": " << dice1 + dice2 << endl;
+	}
+	return 0;
+}
