9da036fa698641a2e45ff02a5bd18ce5ea96dd1a
ryxxng <i876838@gmail.com>
2024-12-13 17:43:54 +0900

    baekjoon 5575

---
+++ baekjoon5575/baekjoon5575/baekjoon5575.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int startH, startM, startS, finishH, finishM, finishS, resultH, resultM, resultS;
+	for (int i = 0; i < 3; i++) {
+		cin >> startH >> startM >> startS >> finishH >> finishM >> finishS;
+		if (finishS < startS) {
+			finishM--;
+			finishS += 60;
+		}
+		resultS = finishS - startS;
+		if (finishM < startM) {
+			finishH--;
+			finishM += 60;
+		}
+		resultM = finishM - startM;
+		resultH = finishH - startH;
+
+		cout << resultH << " " << resultM << " " << resultS << endl;
+	}
+
+	return 0;
+}
