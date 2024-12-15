0f4172976d782bc28c190288cb2b191a70ee3684
ryxxng <i876838@gmail.com>
2024-12-15 16:58:08 +0900

    baekjoon 13752

---
+++ baekjoon13752/baekjoon13752/baekjoon13752.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> count;
+		for (int j = 0; j < count; j++) cout << "=";
+		cout << endl;
+	}
+	return 0;
+}
