869739fac18d2e2ba8dab3bbae5b78a5ba6bbe7d
ryxxng <i876838@gmail.com>
2024-12-18 23:24:55 +0900

    baekjoon 16486

---
+++ baekjoon16486/baekjoon16486/baekjoon16486.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double const PI = 3.141592;
+	int d1, d2;
+	cin >> d1 >> d2;
+	cout << fixed << setprecision(6);
+	cout << d1 * 2 + static_cast<double>(d2) * PI * 2;
+
+	return 0;
+}
