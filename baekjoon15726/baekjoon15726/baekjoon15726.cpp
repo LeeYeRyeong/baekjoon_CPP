26d588dc34a10007e1993f4386d289e0519cc662
ryxxng <i876838@gmail.com>
2025-01-06 16:25:01 +0900

    baekjoon 15726

---
+++ baekjoon15726/baekjoon15726/baekjoon15726.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long a, b, c, temp1, temp2;
+	cin >> a >> b >> c;
+	temp1 = static_cast<double>(a * b) / c;
+	temp2 = (static_cast<double>(a) / b) * c;
+	cout << (temp1 > temp2 ? temp1 : temp2);
+	
+	return 0;
+}
