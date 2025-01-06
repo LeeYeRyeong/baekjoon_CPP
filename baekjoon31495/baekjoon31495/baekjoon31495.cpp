361d122b472853222d954a44adf04b6d9f1cb52e
ryxxng <i876838@gmail.com>
2025-01-06 16:42:40 +0900

    baekjoon 31495

---
+++ baekjoon31495/baekjoon31495/baekjoon31495.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+	if (s.size() > 2 && s[0] == '\"' && s[s.size() - 1] == '\"') for (int i = 1; i < s.size() - 1; i++) cout << s[i];
+	else cout << "CE";
+
+	return 0;
+}
