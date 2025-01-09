66c8e7b168e750b470e6a00d36b5d9dadfc263ce
ryxxng <i876838@gmail.com>
2025-01-09 16:53:45 +0900

    baekjoon 25640

---
+++ baekjoon25640/baekjoon25640/baekjoon25640.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string my_mbti, other_mbti;
+	int count = 0, n;
+	cin >> my_mbti >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> other_mbti;
+		if (my_mbti == other_mbti) count++;
+	}
+	cout << count;
+
+	return 0;
+}
