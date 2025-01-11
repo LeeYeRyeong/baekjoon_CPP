b13f2ec93b78af98a26a2b14d640eba747d1d79f
ryxxng <i876838@gmail.com>
2025-01-11 16:13:38 +0900

    baekjoon 25238

---
+++ baekjoon25238/baekjoon25238/baekjoon25238.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double d = 0.0;
+	int a, b;
+	cin >> a >> b;
+	d = a - static_cast<double>(a) * (b * 0.01);
+	cout << (d >= 100.0 ? 0 : 1);
+
+	return 0;
+}
