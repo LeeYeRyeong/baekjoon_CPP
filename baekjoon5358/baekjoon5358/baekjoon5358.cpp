b5c00d46adb97bda45393f70a6e1e9816f6c484c
ryxxng <i876838@gmail.com>
2025-01-12 16:46:27 +0900

    baekjoon 5358

---
+++ baekjoon5358/baekjoon5358/baekjoon5358.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	while (1) {
+		string s = "";
+		getline(cin, s);
+		if (s == "") break;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == 'i') cout << 'e';
+			else if (s[i] == 'e') cout << 'i';
+			else if (s[i] == 'I') cout << 'E';
+			else if (s[i] == 'E') cout << 'I';
+			else cout << s[i];
+		}
+		cout << endl;
+	}
+}
