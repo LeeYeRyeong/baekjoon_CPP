8d30d8a5d797648b555c39239c491708eedec3db
ryxxng <i876838@gmail.com>
2025-01-21 23:38:33 +0900

    baekjoon 14470

---
+++ baekjoon14470/baekjoon14470/baekjoon14470.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, time = 0;
+	cin >> a >> b >> c >> d >> e;
+	if (a < 0) time = a * -1 * c + d + (b * e);
+	else time = (b - a) * e;
+
+	cout << time;
+
+	return 0;
+}
