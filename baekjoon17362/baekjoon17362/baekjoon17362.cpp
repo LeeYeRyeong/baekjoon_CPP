c9cd61610c6f56b1fff94cb17ec98b10be706896
ryxxng <i876838@gmail.com>
2025-01-21 23:14:32 +0900

    baekjoon 17362

---
+++ baekjoon17362/baekjoon17362/baekjoon17362.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n % 8 == 0) cout << 2;
+	else if ((n % 8) <= 5) cout << n % 8;
+	else if (n % 8 == 6) cout << 4;
+	else if (n % 8 == 7) cout << 3;
+
+	return 0;
+}
