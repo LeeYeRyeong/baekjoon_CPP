e28aec7e82c7173f59248afa26239bfec148c112
ryxxng <i876838@gmail.com>
2025-02-01 13:48:30 +0900

    baekjoon 25191

---
+++ baekjoon25191/baekjoon25191/baekjoon25191.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, coke, beer;
+	cin >> n >> coke >> beer;
+	cout << ((coke / 2 + beer) < n ? (coke / 2 + beer) : n);
+
+	return 0;
+}
