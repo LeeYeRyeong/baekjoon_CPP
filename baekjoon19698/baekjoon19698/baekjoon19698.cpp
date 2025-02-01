e6e48fb53772229921605ba475daefdd3f1f843c
ryxxng <i876838@gmail.com>
2025-02-01 13:35:59 +0900

    baekjoon 19698

---
+++ baekjoon19698/baekjoon19698/baekjoon19698.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, w, h, l, result = 0;
+	cin >> n >> w >> h >> l;
+	result = ((w / l) * (h / l) < n ? (w / l) * (h / l) : n);
+	cout << result;
+
+	return 0;
+}
