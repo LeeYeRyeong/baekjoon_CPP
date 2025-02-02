5105e12346733ba1a2c39f51c1a0d972d3747936
ryxxng <i876838@gmail.com>
2025-02-02 13:47:20 +0900

    baekjoon 11549

---
+++ baekjoon11549/baekjoon11549/baekjoon11549.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count = 0;
+	int answer[5] = {};
+	cin >> n;
+	for (int i = 0; i < 5; i++) {
+		cin >> answer[i];
+		if (n == answer[i])count++;
+	}
+	cout << count;
+
+	return 0;
+}
