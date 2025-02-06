ac320aa6ae33bff8ab36943aecac8860be32e522
ryxxng <i876838@gmail.com>
2025-02-06 21:24:25 +0900

    baekjoon 25377

---
+++ baekjoon25377/baekjoon25377/baekjoon25377.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, time = 1001;
+	int a[100] = {}, b[100] = {};
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i] >> b[i];
+	for (int i = 0; i < n; i++) {
+		if (a[i] <= b[i] && b[i] < time) time = b[i];
+	}
+	cout << (time != 1001 ? time : -1);
+
+	return 0;
+}
