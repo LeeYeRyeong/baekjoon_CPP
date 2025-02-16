88fc08920360351d330457579e2dab65e0bc7d72
ryxxng <i876838@gmail.com>
2025-02-16 12:20:45 +0900

    baekjoon 27110

---
+++ baekjoon27110/baekjoon27110/baekjoon27110.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, c, result = 0;
+	cin >> n >> a >> b >> c;
+	result += (n < a ? n : a);
+	result += (n < b ? n : b);
+	result += (n < c ? n : c);
+
+	cout << result;
+
+	return 0;
+}
