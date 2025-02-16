bb6c716e9d67aa12c3d6601c8377ab838282d450
ryxxng <i876838@gmail.com>
2025-02-16 15:57:25 +0900

    baekjoon 17009

---
+++ baekjoon17009/baekjoon17009/baekjoon17009.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int apple[3], banana[3];
+	int scoreA = 0, scoreB = 0;
+	for (int i = 0; i < 3; i++) cin >> apple[i];
+	for (int i = 0; i < 3; i++) cin >> banana[i];
+	scoreA = apple[0] * 3 + apple[1] * 2 + apple[2];
+	scoreB = banana[0] * 3 + banana[1] * 2 + banana[2];
+
+	if (scoreA > scoreB) cout << "A";
+	else if (scoreA < scoreB) cout << "B";
+	else cout << "T";
+
+	return 0;
+}
