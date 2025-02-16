7e8a395126a468592edb69c7f1ccbee036788886
ryxxng <i876838@gmail.com>
2025-02-16 12:31:35 +0900

    baekjoon 13136

---
+++ baekjoon13136/baekjoon13136/baekjoon13136.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long r, c, n;
+	cin >> r >> c >> n;
+	cout << (r % n == 0 ? r / n : r / n + 1) * (c % n == 0 ? c / n : c / n + 1);
+
+	return 0;
+}
