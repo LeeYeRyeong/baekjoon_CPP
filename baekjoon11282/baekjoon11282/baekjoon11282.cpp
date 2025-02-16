43f5a190523f80bbca7294e959c087906ce57ba7
ryxxng <i876838@gmail.com>
2025-02-16 11:34:57 +0900

    baekjoon 11282

---
+++ baekjoon11282/baekjoon11282/baekjoon11282.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	// https://ddmanager.pe.kr/150
+	int n;
+	string s;
+	cin >> n;
+	n += 3071;
+	s.push_back(n / (64 * 64) + 234);
+	s.push_back(n / 64 % 64 + 128);
+	s.push_back(n % 64 + 128);
+	cout << s;
+
+	return 0;
+}
