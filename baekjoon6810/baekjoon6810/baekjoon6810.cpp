4cece72fc0353b4455cbf7f8b47e2c3e1afe6541
ryxxng <i876838@gmail.com>
2025-02-18 18:43:00 +0900

    baekjoon 6810

---
+++ baekjoon6810/baekjoon6810/baekjoon6810.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, sum = 91;
+	cin >> a >> b >> c;
+	sum += a + b * 3 + c;
+	cout << "The 1-3-sum is " << sum;
+
+	return 0;
+}
