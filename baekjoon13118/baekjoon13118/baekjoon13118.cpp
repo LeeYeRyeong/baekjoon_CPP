ca1a22dfbcdcb0084ef205d8ff7493a2d4af7361
ryxxng <i876838@gmail.com>
2025-02-18 23:06:21 +0900

    baekjoon 13118

---
+++ baekjoon13118/baekjoon13118/baekjoon13118.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int people[4] = {};
+	int x, y, r, result = 0;
+	for (int i = 0; i < 4; i++) cin >> people[i];
+	cin >> x >> y >> r;
+	for (int i = 0; i < 4; i++) if (people[i] == x) result = i + 1;
+	cout << result;
+
+	return 0;
+}
