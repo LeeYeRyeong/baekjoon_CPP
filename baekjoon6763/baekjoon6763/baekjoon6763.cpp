d7adb17fd48a8944fd781dc5844915c4b13ce2f2
ryxxng <i876838@gmail.com>
2025-02-18 23:07:40 +0900

    baekjoon 6763

---
+++ baekjoon6763/baekjoon6763/baekjoon6763.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int limit, speed;
+	cin >> limit >> speed;
+	if (speed > limit) {
+		cout << "You are speeding and your fine is $";
+		if (speed - limit >= 1 && speed - limit <= 20) cout << 100 << ".";
+		else if (speed - limit <= 30) cout << 270 << ".";
+		else cout << 500 << ".";
+	}
+	else cout << "Congratulations, you are within the speed limit!";
+
+	return 0;
+}
