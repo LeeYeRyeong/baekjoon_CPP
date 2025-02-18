0a589d75dc7ddca3df1d898c7901a33f65dbf56a
ryxxng <i876838@gmail.com>
2025-02-18 18:43:38 +0900

    baekjoon17356

---
+++ baekjoon17356/baekjoon17356/baekjoon17356.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	double result;
+	cin >> a >> b;
+	result = 1 / (1 + pow(10, (b - a) / 400.0));
+	cout << setprecision(16) << result;
+
+	return 0;
+}
