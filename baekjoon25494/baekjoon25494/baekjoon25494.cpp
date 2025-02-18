6ff76bfa4d2ecf711ee5c1667a8a8f09417172f8
ryxxng <i876838@gmail.com>
2025-02-18 19:15:45 +0900

    baekjoon 25494

---
+++ baekjoon25494/baekjoon25494/baekjoon25494.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, c, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b >> c;
+		for (int x = 1; x <= a; x++) {
+			for (int y = 1; y <= b; y++) {
+				for (int z = 1; z <= c; z++) {
+					if ((x % y == y % z) && (y % z == z % x)) count++;
+				}
+			}
+		}
+		cout << count << endl;
+		count = 0;
+	}
+
+	return 0;
+}
