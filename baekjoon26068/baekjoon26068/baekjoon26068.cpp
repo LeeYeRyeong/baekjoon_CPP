e61976212f7c7e5bbfc1d23607f48b21646827fe
ryxxng <i876838@gmail.com>
2025-02-19 22:56:20 +0900

    baekjoon 26068

---
+++ baekjoon26068/baekjoon26068/baekjoon26068.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, day, count = 0;
+	char a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b >> day;
+		if (day <= 90) count++;
+	}
+	cout << count;
+
+	return 0;
+}
