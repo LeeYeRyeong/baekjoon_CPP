17d2d6f38fef974c1d1015d6f1419fce421de5ad
ryxxng <i876838@gmail.com>
2025-02-24 18:24:36 +0900

    baekjoon 6764

---
+++ baekjoon6764/baekjoon6764/baekjoon6764.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int depth[4] = {};
+	cin >> depth[0] >> depth[1] >> depth[2] >> depth[3];
+	if ((depth[0] < depth[1] && depth[1] < depth[2] && depth[2] < depth[3])) cout << "Fish Rising";
+	else if ((depth[0] > depth[1] && depth[1] > depth[2] && depth[2] > depth[3])) cout << "Fish Diving";
+	else if	((depth[0] == depth[1] && depth[1] == depth[2] && depth[2] == depth[3])) cout << "Fish At Constant Depth";
+	else cout << "No Fish";
+
+	return 0;
+}
