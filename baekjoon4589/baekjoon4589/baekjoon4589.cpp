32197401f46f679eacabaae72c231d881531bdd5
ryxxng <i876838@gmail.com>
2025-02-24 19:08:54 +0900

    baekjoon 4589

---
+++ baekjoon4589/baekjoon4589/baekjoon4589.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int length[3] = {};
+	int n;
+	cin >> n;
+	cout << "Gnomes: \n";
+	for (int i = 0; i < n; i++) {
+		cin >> length[0] >> length[1] >> length[2];
+		if ((length[0] < length[1] && length[1] < length[2]) || (length[0] > length[1] && length[1] > length[2])) cout << "Ordered\n";
+		else cout << "Unordered\n";
+	}
+	return 0;
+}
