49c8e1f13fa9354be3bf037fe6714721e383c9c3
ryxxng <i876838@gmail.com>
2025-02-24 17:51:52 +0900

    baekjoon 19602

---
+++ baekjoon19602/baekjoon19602/baekjoon19602.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int s, m, l, happy = 0;
+	cin >> s >> m >> l;
+	happy = s + m * 2 + l * 3;
+	cout << (happy >= 10 ? "happy" : "sad");
+	return 0;
+}
