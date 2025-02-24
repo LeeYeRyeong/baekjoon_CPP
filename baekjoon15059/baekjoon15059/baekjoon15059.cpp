d69451f4a782b3e1346b74b1dfa28c260c02c77d
ryxxng <i876838@gmail.com>
2025-02-24 18:01:26 +0900

    baekjoon 15059

---
+++ baekjoon15059/baekjoon15059/baekjoon15059.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int need[3] = {}, prep[3] = {};
+	int count = 0;
+	cin >> prep[0] >> prep[1] >> prep[2] >> need[0] >> need[1] >> need[2];
+	for (int i = 0; i < 3; i++) {
+		if (need[i] > prep[i]) count += need[i] - prep[i];
+	}
+	cout << count;
+
+	return 0;
+}
