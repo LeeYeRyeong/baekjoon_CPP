72c469c2471cc9b3a8c6f4da8435739acc16367c
ryxxng <i876838@gmail.com>
2025-02-26 12:26:48 +0900

    baekjoon 14038

---
+++ baekjoon14038/baekjoon14038/baekjoon14038.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int count = 0, result = -1;
+	char c;
+	for (int i = 0; i < 6; i++) {
+		cin >> c;
+		if (c == 'W') count++;
+	}
+	if (count >= 5) result = 1;
+	else if (count >= 3) result = 2;
+	else if (count >= 1) result = 3;
+	cout << result;
+
+	return 0;
+}
