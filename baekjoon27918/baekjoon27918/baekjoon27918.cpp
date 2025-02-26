57fff8a88860bd688ebd7135b2ef45b1c20dcebb
ryxxng <i876838@gmail.com>
2025-02-26 13:32:33 +0900

    baekjoon 27918

---
+++ baekjoon27918/baekjoon27918/baekjoon27918.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int pointD = 0, pointP = 0, n;
+	char c;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> c;
+		if (pointD - pointP == 2 || pointP - pointD == 2) continue;
+		if (c == 'D') pointD++;
+		else pointP++;
+	}
+	cout << pointD << ":" << pointP;
+
+	return 0;
+}
