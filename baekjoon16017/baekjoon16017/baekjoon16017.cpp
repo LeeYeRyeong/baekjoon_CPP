6c69bb9002af237176dfe381dbdfcd17bad41972
ryxxng <i876838@gmail.com>
2025-02-26 13:36:10 +0900

    baekjoon 16017

---
+++ baekjoon16017/baekjoon16017/baekjoon16017.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num[4] = {};
+	bool check = false;
+	for (int i = 0; i < 4; i++) cin >> num[i];
+	if ((num[0] == 8 || num[0] == 9) && (num[1] == num[2]) && (num[3] == 8 || num[3] == 9)) check = true;
+	
+	cout << (check ? "ignore" : "answer");
+
+	return 0;
+}
