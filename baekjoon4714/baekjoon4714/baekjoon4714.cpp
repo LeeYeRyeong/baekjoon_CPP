5800e640e4a8e2c8dba16ea890a46998586868e5
ryxxng <i876838@gmail.com>
2025-02-28 15:14:55 +0900

    baekjoon 4714

---
+++ baekjoon4714/baekjoon4714/baekjoon4714.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double x;
+	while (1) {
+		cin >> x;
+		if (x == -1.0) break;
+		cout << fixed << "Objects weighing " << setprecision(2) << x << " on Earth will weigh " << x * 0.167 << " on the moon.\n";
+	}
+	return 0;
+}
