e56c0e1029bee51a369120aed1bc41f696b8a0ad
ryxxng <i876838@gmail.com>
2025-02-28 15:16:56 +0900

    baekjoon 28295

---
+++ baekjoon28295/baekjoon28295/baekjoon28295.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, sum = 0;
+	for (int i = 0; i < 10; i++) {
+		cin >> n;
+		sum += n;
+	}
+	if (sum % 4 == 0) cout << 'N';
+	else if (sum % 4 == 1) cout << 'E';
+	else if (sum % 4 == 2) cout << 'S';
+	else if (sum % 4 == 3) cout << 'W';
+
+	return 0;
+}
