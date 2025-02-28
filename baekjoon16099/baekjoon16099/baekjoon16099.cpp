b351e4534398173fe013a94d9098af55beceb16b
ryxxng <i876838@gmail.com>
2025-02-28 18:49:56 +0900

    baekjoon 16099

---
+++ baekjoon16099/baekjoon16099/baekjoon16099.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	long lt, wt, le, we, t, e;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> lt >> wt >> le >> we;
+		t = lt * wt;
+		e = le * we;
+		cout << (t == e ? "Tie\n" : (t < e ? "Eurecom\n" : "TelecomParisTech\n"));
+	}
+	return 0;
+}
