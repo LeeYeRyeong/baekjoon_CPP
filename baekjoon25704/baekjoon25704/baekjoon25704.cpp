8f8280977c45451e1cda5f947e4f71ce2806658d
ryxxng <i876838@gmail.com>
2025-02-28 18:43:39 +0900

    baekjoon 25704

---
+++ baekjoon25704/baekjoon25704/baekjoon25704.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, prize, sale = 0;
+	cin >> n >> prize;
+	if (n >= 5 && n < 10) sale = 500;
+	else if (n >= 10 && n < 15) sale = (prize * 0.1 > 500 ? prize * 0.1 : 500);
+	else if (n >= 15 && n < 20) sale = (prize * 0.1 > 2000 ? prize * 0.1 : 2000);
+	else if (n >= 20) sale = (prize * 0.25 > 2000 ? prize * 0.25 : 2000);
+	cout << (prize - sale > 0 ? prize - sale : 0);
+
+	return 0;
+}
