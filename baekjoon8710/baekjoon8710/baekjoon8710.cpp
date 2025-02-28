20465f3c64b2656139af1cfc36e860931f359c9e
ryxxng <i876838@gmail.com>
2025-02-28 18:49:06 +0900

    baekjoon 8710

---
+++ baekjoon8710/baekjoon8710/baekjoon8710.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, k, count = 0;
+	cin >> n >> m >> k;
+	count = ((m - n) % k == 0 ? (m - n) / k : (m - n) / k + 1);
+	cout << count;
+
+	return 0;
+}
