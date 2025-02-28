c001bf2de953733552a8d51c3dbfcd8d274d6d2e
ryxxng <i876838@gmail.com>
2025-02-28 18:44:22 +0900

    baekjoon 18411

---
+++ baekjoon18411/baekjoon18411/baekjoon18411.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, sum = 0;
+	cin >> a >> b >> c;
+	if (a + b > sum) sum = a + b;
+	if (a + c > sum) sum = a + c;
+	if (c + b > sum) sum = c + b;
+	cout << sum;
+
+	return 0;
+}
