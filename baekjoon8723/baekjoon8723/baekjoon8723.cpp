d4d661488b4068a08d69c39358d7d3e77349c28c
ryxxng <i876838@gmail.com>
2025-02-28 15:15:50 +0900

    baekjoon 8723

---
+++ baekjoon8723/baekjoon8723/baekjoon8723.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if ((pow(a, 2) + pow(b, 2) == pow(c, 2)) || (pow(a, 2) + pow(c, 2) == pow(b, 2)) || (pow(c, 2) + pow(b, 2) == pow(a, 2))) cout << 1;
+	else if (a == b && b == c) cout << 2;
+	else cout << 0;
+
+	return 0;
+}
