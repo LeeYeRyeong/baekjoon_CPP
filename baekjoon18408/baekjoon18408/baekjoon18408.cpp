2fecb03988798d74cd1d4ace7d53c939d5bcedf1
ryxxng <i876838@gmail.com>
2025-02-28 18:45:03 +0900

    baekjoon 18408

---
+++ baekjoon18408/baekjoon18408/baekjoon18408.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count = 0;
+	for (int i = 0; i < 3; i++) {
+		cin >> n;
+		(n == 1 ? count++ : count--);
+	}
+	cout << (count > 0 ? 1 : 2);
+
+	return 0;
+}
