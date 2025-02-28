c738f83f655adb259b6b024ca62a0d1729d2b13b
ryxxng <i876838@gmail.com>
2025-02-28 18:50:34 +0900

    baekjoon 28352

---
+++ baekjoon28352/baekjoon28352/baekjoon28352.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long result = 1;
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) result *= i;
+	cout << result / static_cast<long long>(24 * 60 * 60 * 7);
+
+	return 0;
+}
