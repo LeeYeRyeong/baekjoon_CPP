674df45c905aa9127be89ce44df580cfbf54a7f8
ryxxng <i876838@gmail.com>
2025-03-01 15:27:49 +0900

    baekjoon 28097

---
+++ baekjoon28097/baekjoon28097/baekjoon28097.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, time, studyTime = -8;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> time;
+		studyTime += time + 8;
+	}
+	cout << studyTime / 24 << " " << studyTime % 24;
+
+	return 0;
+}
