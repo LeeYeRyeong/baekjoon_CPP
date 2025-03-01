4f69387f3f417e9511c917a2e37d258871d49761
ryxxng <i876838@gmail.com>
2025-03-01 15:29:00 +0900

    baekjoon 25591

---
+++ baekjoon25591/baekjoon25591/baekjoon25591.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, a, b, c, d, q, r, result1 = 0, result2 = 0;
+	cin >> n >> m;
+	a = 100 - n;
+	b = 100 - m;
+	c = 100 - (a + b);
+	d = a * b;
+	q = d / 100;
+	r = d % 100;
+	result1 = (q > 0 ? q + c : c);
+	result2 = r;
+	cout << a << " " << b << " " << c << " " << d << " " << q << " " << r << '\n';
+	cout << result1 << " " << result2;
+
+	return 0;
+}
