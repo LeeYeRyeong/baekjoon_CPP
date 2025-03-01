76d4a720ea848001e208f4f57136b982d97c2847
ryxxng <i876838@gmail.com>
2025-03-01 15:26:43 +0900

    baekjoon 27310

---
+++ baejoon27310/baejoon27310/baejoon27310.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int underCount = 0, colonCount = 0, level = 0;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == '_') underCount++;
+		else if (s[i] == ':') colonCount++;
+	}
+	level = s.size() + colonCount + underCount * 5;
+	cout << level;
+
+	return 0;
+}
