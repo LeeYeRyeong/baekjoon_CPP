859964a791a018ba63adbb00411e3b8b66aefad5
ryxxng <i876838@gmail.com>
2025-03-03 16:34:59 +0900

    baekjoon 13623

---
+++ baekjoon13623/baekjoon13623/baekjoon13623.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a == b && b == c) cout << '*';
+	else if (a == b && b != c) cout << 'C';
+	else if (a == c && b != c) cout << 'B';
+	else if (b == c && b != a) cout << 'A';
+
+	return 0;
+}
