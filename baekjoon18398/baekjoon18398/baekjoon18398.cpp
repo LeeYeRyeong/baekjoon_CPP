91706926af9fc7d0de4888dea5705c09918d4578
ryxxng <i876838@gmail.com>
2025-03-03 16:33:15 +0900

    baekjoon 18398

---
+++ baekjoon18398/baekjoon18398/baekjoon18398.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, a, b;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> a >> b;
+			cout << a + b << " " << a * b << '\n';
+		}
+	}
+	return 0;
+}
