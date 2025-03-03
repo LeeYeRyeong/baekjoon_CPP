b28d0ec4be290db929f30e33cca128f6f3ad6fd4
ryxxng <i876838@gmail.com>
2025-03-03 16:31:30 +0900

    baekjoon 20976

---
+++ baekjoon20976/baekjoon20976/baekjoon20976.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, result;
+	cin >> a >> b >> c;
+	if ((a >= b && a <= c) || (b >= a && a >= c)) result = a;
+	if ((a <= b && b <= c) || (a >= b && b >= c)) result = b;
+	if ((c >= a && c <= b) || (a >= c && c >= b)) result = c;
+	cout << result;
+
+	return 0;
+}
