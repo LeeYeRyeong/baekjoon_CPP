26c2e4d28f52d7bd8b8ddc29f70ecf3fbdfdaa1a
ryxxng <i876838@gmail.com>
2025-03-03 16:35:32 +0900

    baekjoon 15025

---
+++ baekjoon15025/baekjoon15025/baekjoon15025.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	if (n == 0 && m == 0) cout << "Not a moose";
+	else if ((n + m) % 2 == 0 && n == m) cout << "Even " << (n > m ? n * 2 : m * 2);
+	else cout << "Odd " << (n > m ? n * 2 : m * 2);
+
+	return 0;
+}
