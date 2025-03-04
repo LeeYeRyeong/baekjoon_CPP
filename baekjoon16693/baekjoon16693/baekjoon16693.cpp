f74218e8c510436db0fba89e6ac67fd1bec87b81
ryxxng <i876838@gmail.com>
2025-03-04 11:24:59 +0900

    baekjoon 16693

---
+++ baekjoon16693/baekjoon16693/baekjoon16693.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a1, p1, r1, p2;
+	cin >> a1 >> p1 >> r1 >> p2;
+	cout << (p1 * 1.0 / a1 < p2 / (r1 * r1 * 3.14159265359) ? "Slice of pizza" : "Whole pizza");
+
+	return 0;
+}
