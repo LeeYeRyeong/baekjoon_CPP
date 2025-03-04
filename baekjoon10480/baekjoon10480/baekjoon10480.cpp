66c40c1311faf871f6fd2281c9f25b3270d01074
ryxxng <i876838@gmail.com>
2025-03-04 11:23:47 +0900

    baekjoon 10480

---
+++ baekjoon10480/baekjoon10480/baekjoon10480.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		cout << n << (n % 2 == 0 ? " is even\n" : " is odd\n");
+	}
+	return 0;
+}
