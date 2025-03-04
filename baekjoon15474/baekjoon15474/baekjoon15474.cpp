b4ceaddd2406b867727fe0d10914c8314f6df986
ryxxng <i876838@gmail.com>
2025-03-04 11:24:24 +0900

    baekjoon 15474

---
+++ baekjoon15474/baekjoon15474/baekjoon15474.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, c, d;
+	cin >> n >> a >> b >> c >> d;
+	cout << ((n % a == 0 ? (n / a) * b : (n / a + 1) * b) < (n % c == 0 ? (n / c) * d : (n / c + 1) * d) ?
+		(n % a == 0 ? (n / a) * b : (n / a + 1) * b) : (n % c == 0 ? (n / c) * d : (n / c + 1) * d));
+
+	return 0;
+}
