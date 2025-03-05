7122711104e60a5ed526323230174d548eb7238d
ryxxng <i876838@gmail.com>
2025-03-05 17:17:06 +0900

    baekjoon 18414

---
+++ baekjoon18414/baekjoon18414/baekjoon18414.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, l, r, less = 100000, min;
+	cin >> x >> l >> r;
+	for (int i = l; i <= r; i++) {
+		if (less > (x - i < 0 ? i - x : x - i)) {
+			less = (x - i < 0 ? i - x : x - i);
+			min = i;
+		}
+	}
+	cout << min;
+
+	return 0;
+}
