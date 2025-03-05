287b2a59ef73701c6e19fe4ed2fabd2110924e50
ryxxng <i876838@gmail.com>
2025-03-05 17:19:01 +0900

    baekjoon 29766

---
+++ baekjoon29766/baekjoon29766/baekjoon29766.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int count = 0;
+	cin >> s;
+	if (s.size() >= 4) {
+		for (int i = 0; i <= s.size() - 4; i++) {
+			if (s[i] == 'D' && s[i + 1] == 'K' && s[i + 2] == 'S' && s[i + 3] == 'H') count++;
+		}
+	}
+	cout << count;
+
+	return 0;
+}
