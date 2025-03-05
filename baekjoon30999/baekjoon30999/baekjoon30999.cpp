9fa395459ca4c6fdd59dfdffba61bf463a220117
ryxxng <i876838@gmail.com>
2025-03-05 17:19:36 +0900

    baekjoon 30999

---
+++ baekjoon30999/baekjoon30999/baekjoon30999.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, count = 0, good = 0;
+	char c;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> c;
+			if (c == 'O') good++;
+		}
+		if (good > m / 2) count++;
+		good = 0;
+	}
+	cout << count;
+
+	return 0;
+}
