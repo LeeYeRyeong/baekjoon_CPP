64873ac459a0b553caf8cf53106d2c2a64c70e55
ryxxng <i876838@gmail.com>
2025-03-05 17:18:19 +0900

    baekjoon 28281

---
+++ baekjoon28281/baekjoon28281/baekjoon28281.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int price[100000] = {};
+	int n, x, cost = 2000;
+	cin >> n >> x;
+	for (int i = 0; i < n; i++) cin >> price[i];
+	for (int i = 0; i < n - 1; i++) {
+		if (cost > price[i] + price[i + 1]) cost = price[i] + price[i + 1];
+	}
+
+	cout << cost * x;
+
+	return 0;
+}
