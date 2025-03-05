f6b46551dceee872bbc64f2f44605e1f1047ef09
ryxxng <i876838@gmail.com>
2025-03-05 17:16:21 +0900

    baekjoon 8558

---
+++ baekjoon8558/baekjoon8558/baekjoon8558.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n > 4) cout << 0;
+	else if (n == 4) cout << 4;
+	else if (n == 3) cout << 6;
+	else if (n == 2) cout << 2;
+	else cout << 1;
+
+	return 0;
+}
