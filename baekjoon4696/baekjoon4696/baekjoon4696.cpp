bd6bed963d224ee11e6eab902abc761fda79dd8c
ryxxng <i876838@gmail.com>
2025-03-05 17:17:41 +0900

    baekjoon 4696

---
+++ baekjoon4696/baekjoon4696/baekjoon4696.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double n;
+	while (1) {
+		cin >> n;
+		if (n == 0.0) break;
+		cout << fixed << setprecision(2) << 1 + n + n * n + n * n * n + n * n * n * n << '\n';
+	}
+
+	return 0;
+}
