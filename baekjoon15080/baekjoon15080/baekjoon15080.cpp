2364f417d0b2fe9d3d604815e84ffe6d4e171a71
ryxxng <i876838@gmail.com>
2025-03-07 19:45:26 +0900

    baekjoon 15080

---
+++ baekjoon15080/baekjoon15080/baekjoon15080.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int startHour, startMin, startSec, endHour, endMin, endSec, time = 0;
+	char c;
+	cin >> startHour >> c >> startMin >> c >> startSec >> endHour >> c >> endMin >> c >> endSec;
+	if (endSec < startSec) {
+		endMin--;
+		endSec += 60;
+	}
+	if (endMin < startMin) {
+		endHour--;
+		endMin += 60;
+	}
+	if (endHour < startHour) endHour += 24;
+	time += (endSec - startSec) + ((endMin - startMin) * 60) + ((endHour - startHour) * 60 * 60);
+	cout << time;
+
+	return 0;
+}
