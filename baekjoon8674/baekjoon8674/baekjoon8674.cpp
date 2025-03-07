268dd573cb7ad0fabffd4426f7c2971c050b11a5
ryxxng <i876838@gmail.com>
2025-03-07 19:44:20 +0900

    baekjoon 8674

---
+++ baekjoon8674/baekjoon8674/baekjoon8674.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long a, b, result1 = 0, result2 = 0;
+	cin >> a >> b;
+	if (a * b % 2 != 0) {
+		result1 = (a * b - (a / 2 * b) > (a / 2 * b) ? a * b - (a / 2 * b) - (a / 2 * b) : (a / 2 * b) - a * b - (a / 2 * b));
+		result2 = (a * b - (b / 2 * a) > (b / 2 * a) ? a * b - (b / 2 * a) - (b / 2 * a) : (b / 2 * a) - a * b - (b / 2 * a));
+	}
+	cout << (result1 < result2 ? result1 : result2);
+	
+	return 0;
+}
