8b74b29c9da9f94c499dba02e325467761af985e
ryxxng <i876838@gmail.com>
2025-03-07 19:45:57 +0900

    baekjoon 30031

---
+++ baekjoon30031/baekjoon30031/baekjoon30031.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, w, h, amount = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> w >> h;
+		if (w == 136) amount += 1000;
+		else if (w == 142) amount += 5000;
+		else if (w == 148) amount += 10000;
+		else if (w == 154) amount += 50000;
+	}
+	cout << amount;
+
+	return 0;
+}
