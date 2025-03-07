a617b3233db9da46414dec7a3c7a6040e98d4248
ryxxng <i876838@gmail.com>
2025-03-07 19:44:57 +0900

    baekjoon 6825

---
+++ baekjoon6825/baekjoon6825/baekjoon6825.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double weight, height, BMI = 0.0;
+	cin >> weight >> height;
+	BMI = weight / (height * height);
+	if (BMI > 25) cout << "Overweight";
+	else if (BMI >= 18.5) cout << "Normal weight";
+	else cout << "Underweight";
+
+	return 0;
+}
