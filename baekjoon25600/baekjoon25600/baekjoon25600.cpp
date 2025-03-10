68ea20b47543424803ff1c8422cd1136c27efbd1
ryxxng <i876838@gmail.com>
2025-03-10 22:07:18 +0900

    baekjoon 25600

---
+++ baekjoon25600/baekjoon25600/baekjoon25600.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, d, g, score, result = -1;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> d >> g;
+		score = a * (d + g);
+		if (a == d + g)score *= 2;
+		result = (result < score ? score : result);
+	}
+	cout << result;
+
+	return 0;
+}
