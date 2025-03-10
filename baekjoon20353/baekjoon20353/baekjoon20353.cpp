7c66523e74226b1c99f4d761a2be05555f64e2d0
ryxxng <i876838@gmail.com>
2025-03-10 22:06:08 +0900

    baekjoon 20353

---
+++ baekjoon20353/baekjoon20353/baekjoon20353.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	long n;
+	double result = 0.0;
+	cin >> n;
+	result = 4.0 * (sqrt(n));
+	cout << fixed << setprecision(8) << result;
+
+	return 0;
+}
