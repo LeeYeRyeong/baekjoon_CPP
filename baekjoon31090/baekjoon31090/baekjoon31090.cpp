289f6321e81d803891f18594abc707019e2ea9d8
ryxxng <i876838@gmail.com>
2025-03-10 22:03:56 +0900

    baekjoon 31090

---
+++ baekjoon31090/baekjoon31090/baekjoon31090.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, year;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> year;
+		if ((year + 1) % (year % 100) == 0) cout << "Good\n";
+		else cout << "Bye\n";
+	}
+	return 0;
+}
