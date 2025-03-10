f1a80580c77c6be879bd52c65e0df0cc608810ad
ryxxng <i876838@gmail.com>
2025-03-10 22:05:39 +0900

    baekjoon 30979

---
+++ baekjoon30979/baekjoon30979/baekjoon30979.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, f, time = 0;
+	cin >> t >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> f;
+		time += f;
+	}
+	cout << (t > time ? "Padaeng_i Cry" : "Padaeng_i Happy");
+
+	return 0;
+}
