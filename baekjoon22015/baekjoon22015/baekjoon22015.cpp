e0ed76a8a4d5c395c4cab70d32f0c0d8cfbfef72
ryxxng <i876838@gmail.com>
2025-03-10 22:04:25 +0900

    baekjoon 22015

---
+++ baekjoon22015/baekjoon22015/baekjoon22015.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, big = -1, count = 0;
+	cin >> a >> b >> c;
+	if (big < a) big = a;
+	if (big < b) big = b;
+	if (big < c) big = c;
+	count = big - a + big - b + big - c;
+	cout << count;
+
+	return 0;
+}
