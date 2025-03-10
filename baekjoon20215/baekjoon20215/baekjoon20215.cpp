b2fc864e0c6bb48484ab79ac2359d224875247af
ryxxng <i876838@gmail.com>
2025-03-10 22:07:49 +0900

    baekjoon 20215

---
+++ baekjoon20215/baekjoon20215/baekjoon20215.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int w, h;
+	cin >> w >> h;
+	cout << fixed << setprecision(9) << w + h - sqrt(static_cast<double>(pow(w, 2) + pow(h, 2)));
+
+	return 0;
+}
