c619360f50a24f02d27d48e457de96f50d05984a
ryxxng <i876838@gmail.com>
2025-03-10 22:06:47 +0900

    baekjoon 20673

---
+++ baekjoon20673/baekjoon20673/baekjoon20673.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	if (n <= 50 && m <= 10) cout << "White";
+	else if (m > 30) cout << "Red";
+	else cout << "Yellow";
+
+	return 0;
+}
