8ed5875b828e69f952a3aeb31aeb1ee143dd01b7
ryxxng <i876838@gmail.com>
2025-03-11 18:56:14 +0900

    baekjoon 15051

---
+++ baekjoon15051/baekjoon15051/baekjoon15051.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[3] = {};
+	int floor, time = 0, max = 0;
+	for (int i = 0; i < 3; i++) {
+		cin >> a[i];
+		if (max < a[i]) {
+			max = a[i];
+			floor = i;
+		}
+	}
+
+	if ((floor == 0 && max < a[1] + a[2]) || (floor == 2 && max < a[1] + a[0])) {
+		floor = 1;
+		max = a[1];
+	}
+
+	for (int i = 0; i < 3; i++) time += (i < floor ? (floor - i) * 2 * a[i] : (i - floor) * 2 * a[i]);
+
+	cout << time;
+
+	return 0;
+}
