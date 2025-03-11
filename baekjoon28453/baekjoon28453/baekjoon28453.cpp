63843a3d07df94a0b815ed92bec7b2449393eaac
ryxxng <i876838@gmail.com>
2025-03-11 18:54:30 +0900

    baekjoon 28453

---
+++ baekjoon28453/baekjoon28453/baekjoon28453.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, level;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> level;
+		if (level == 300) cout << "1 ";
+		else if (level >= 275) cout << "2 ";
+		else if (level >= 250) cout << "3 ";
+		else cout << "4 ";
+	}
+
+	return 0;
+}
