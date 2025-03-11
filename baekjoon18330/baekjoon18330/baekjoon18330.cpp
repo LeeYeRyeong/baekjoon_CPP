33155c2768bd0824b46ca191c694423bf504caa7
ryxxng <i876838@gmail.com>
2025-03-11 18:52:55 +0900

    baekjoon 18330

---
+++ baekjoon18330/baekjoon18330/baekjoon18330.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, cost = 0;
+	cin >> n >> m;
+	cost = (n - m > 60 ? ((60 - m) * 1500 + (n - 60) * 3000) : (n * 1500));
+	cout << cost;
+
+	return 0;
+}
