bce8ca730f8dd461ded02198c8e7e459c998bb1b
ryxxng <i876838@gmail.com>
2025-03-11 18:53:58 +0900

    baekjoon 30868

---
+++ baekjoon30868/baekjoon30868/baekjoon30868.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> count;
+		for (int j = 0; j < count / 5; j++) cout << "++++ ";
+		for (int j = 0; j < count % 5; j++) cout << "|";
+		cout << '\n';
+	}
+
+	return 0;
+}
