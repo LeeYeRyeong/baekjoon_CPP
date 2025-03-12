62a9f310473a5bcb32010a5e4534bad01471742f
ryxxng <i876838@gmail.com>
2025-03-12 23:16:28 +0900

    baekjoon 30468

---
+++ baekjoon30468/baekjoon30468/baekjoon30468.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int s, d, i, l, n;
+	cin >> s >> d >> i >> l >> n;
+	cout << (n * 4 - (s + d + i + l) > 0 ? n * 4 - (s + d + i + l) : 0);
+
+	return 0;
+}
