e7670b7ab11b195b6de84df4a79059c9906bd8db
ryxxng <i876838@gmail.com>
2025-03-12 23:08:24 +0900

    baekjoon 14065

---
+++ baekjoon14065/baekjoon14065/baekjoon14065.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double m, l;
+	cin >> m;
+	l = (100000 / (m * 1609.344)) * 3.785411784;
+	cout << fixed << setprecision(6) << l;
+
+	return 0;
+}
