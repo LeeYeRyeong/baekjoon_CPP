1ac44f1f164b5bf132f30ae1f925535a00de509d
ryxxng <i876838@gmail.com>
2025-03-12 23:09:34 +0900

    baekjoon 15128

---
+++ baekjoon15128/baekjoon15128/baekjoon15128.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long p1, p2, q1, q2;
+	cin >> p1 >> q1 >> p2 >> q2;
+	cout << ((p1 * p2) % (q1 * q2 * 2) == 0 ? 1 : 0);
+
+	return 0;
+}
