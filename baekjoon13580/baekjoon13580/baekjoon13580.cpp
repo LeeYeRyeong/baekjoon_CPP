199774de409a03ca6a293021a155eb063d5ffd34
ryxxng <i876838@gmail.com>
2025-03-12 23:17:19 +0900

    baekjoon 13580

---
+++ baekjoon13580/baekjoon13580/baekjoon13580.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << (a == c || b == c || a == b || a + b == c || a + c == b || b + c == a ? 'S' : 'N');
+
+	return 0;
+}
