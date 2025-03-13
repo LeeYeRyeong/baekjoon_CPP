ac1fb00700d20376b9281a2b3e6c93f2be1e8958
ryxxng <i876838@gmail.com>
2025-03-13 15:35:16 +0900

    baekjoon 9772

---
+++ baekjoon9772/baekjoon9772/baekjoon9772.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x, y;
+	do {
+		cin >> x >> y;
+		if (x > 0 && y > 0) cout << "Q1\n";
+		else if (x < 0 && y > 0) cout << "Q2\n";
+		else if (x < 0 && y < 0) cout << "Q3\n";
+		else if (x > 0 && y < 0) cout << "Q4\n";
+		else cout << "AXIS\n";
+	} while (x != 0 || y != 0);
+
+	return 0;
+}
