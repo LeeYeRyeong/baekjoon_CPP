fba455e17fb1870495cf92dff5b061adb8445dbd
ryxxng <i876838@gmail.com>
2025-03-13 15:36:06 +0900

    baekjoon 6888

---
+++ baekjoon9699/baekjoon9699/baekjoon9699.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, max = -1, num;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 5; j++) {
+			cin >> num;
+			if (max < num) max = num;
+		}
+		cout << "Case #" << i + 1 << ": " << max << '\n';
+		max = -1;
+	}
+
+	return 0;
+}
