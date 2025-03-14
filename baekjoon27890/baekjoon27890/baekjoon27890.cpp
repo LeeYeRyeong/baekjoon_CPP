aac5587c4a3e9672175a9afdbfb2036220b90b48
ryxxng <i876838@gmail.com>
2025-03-14 17:44:57 +0900

    baekjoon 27890

---
+++ baekjoon27890/baekjoon27890/baekjoon27890.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int h, n;
+	cin >> h >> n;
+	for (int i = 0; i < n; i++) h = (h % 2 == 0 ? h / 2 ^ 6 : 2 * h ^ 6);
+	cout << h;
+
+	return 0;
+}
