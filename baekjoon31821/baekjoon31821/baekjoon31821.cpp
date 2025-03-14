a7299ec30617d6c02d13eb50c645f0e804129416
ryxxng <i876838@gmail.com>
2025-03-14 17:44:03 +0900

    baekjoon 31821

---
+++ baekjoon31821/baekjoon31821/baekjoon31821.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int price[10] = {};
+	int n, m, num, cost = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> price[i];
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> num;
+		cost += price[num - 1];
+	}
+	cout << cost;
+
+	return 0;
+}
