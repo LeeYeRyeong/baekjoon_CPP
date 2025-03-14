d9e5d2ab633c7b6be57d5fee14a2927719e7a231
ryxxng <i876838@gmail.com>
2025-03-14 17:43:00 +0900

    baekjoon 29738

---
+++ baekjoon29738/baekjoon29738/baekjoon29738.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		if (n > 4500) cout << "Case #" << i + 1 << ": Round 1\n";
+		else if (n > 1000) cout << "Case #" << i + 1 << ": Round 2\n";
+		else if (n > 25)cout << "Case #" << i + 1 << ": Round 3\n";
+		else cout << "Case #" << i + 1 << ": World Finals\n";
+	}
+
+	return 0;
+}
