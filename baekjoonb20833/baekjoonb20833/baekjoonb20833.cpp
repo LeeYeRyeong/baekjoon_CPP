20d3dea966cb5a17f10d3edcbe02a564fe3087d4
ryxxng <i876838@gmail.com>
2025-03-15 10:27:41 +0900

    baekjoon 20833

---
+++ baekjoonb20833/baekjoonb20833/baekjoonb20833.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	long result = 0;
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) result += pow(i, 3);
+	cout << result;
+
+	return 0;
+}
