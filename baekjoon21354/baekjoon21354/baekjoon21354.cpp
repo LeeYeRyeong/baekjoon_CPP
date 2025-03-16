511b5ba5d8f3d5d5bf7378129310440dc25cc8cf
ryxxng <i876838@gmail.com>
2025-03-16 10:34:49 +0900

    baekjoon 21354

---
+++ baekjoon21354/baekjoon21354/baekjoon21354.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, p;
+	cin >> a >> p;
+	cout << (a * 7 == p * 13 ? "lika" : (a * 7 > p * 13 ? "Axel" : "Petra"));
+
+	return 0;
+}
