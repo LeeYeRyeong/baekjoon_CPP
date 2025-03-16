1885626000084c70304591c4c67b51da0566c0ec
ryxxng <i876838@gmail.com>
2025-03-16 10:34:18 +0900

    baekjoon11257

---
+++ baekjoon11257/baekjoon11257/baekjoon11257.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    int a, b, c, n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s >> a >> b >> c;
+        cout << s << ' ' << a + b + c << ' ';
+        if (a + b + c >= 55 && a > 10 && b > 7 && c >= 12) cout << "PASS\n";
+        else cout << "FAIL\n";
+    }
+
+    return 0;
+}
