9c18b246206d049ee8e547bb67a7d79c43f3d0f7
ryxxng <i876838@gmail.com>
2025-03-17 21:29:03 +0900

    baekjoon 21335

---
+++ baekjoon21335/baekjoon21335/baekjoon21335.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double area, r, length;
+	cin >> area;
+	r = sqrt(area / 3.141592);
+	length = 2 * 3.141592 * r;
+	cout << fixed << setprecision(9) << length;
+
+	return 0;
+}
