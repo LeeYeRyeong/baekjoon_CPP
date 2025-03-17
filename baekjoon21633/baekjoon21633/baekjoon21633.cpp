3d1d5640c4466d57010ead538e737f08e6f58f3e
ryxxng <i876838@gmail.com>
2025-03-17 20:32:11 +0900

    baekjoon 21633

---
+++ baekjoon21633/baekjoon21633/baekjoon21633.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double k, transfer;
+	cin >> k;
+	transfer = (k * 0.01) + 25;
+	cout << fixed << setprecision(2) << (transfer < 100 ? 100 : (transfer > 2000 ? 2000 : transfer));
+
+	return 0;
+}
