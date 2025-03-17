8308b9fbf053699f1b5cf36ed639bf00477af994
ryxxng <i876838@gmail.com>
2025-03-17 21:28:31 +0900

    baekjoon 24087

---
+++ baekjoon24087/baekjoon24087/baekjoon24087.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, s;
+	cin >> s >> a >> b;
+	cout << (s <= a ? 250 : ((s - a) % b == 0 ? (s - a) / b * 100 + 250 : ((s - a) / b + 1) * 100 + 250));
+
+	return 0;
+}
