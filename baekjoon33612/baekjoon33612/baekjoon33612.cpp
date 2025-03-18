f7594a988174b4bbb1f945d872521dffe20e768d
ryxxng <i876838@gmail.com>
2025-03-18 20:54:25 +0900

    baekjoon 33612

---
+++ baekjoon33612/baekjoon33612/baekjoon33612.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, date = 0;
+	cin >> n;
+	date = (n - 1) * 7 + 8;
+	cout << 2024 + (date % 12 == 0 ? date / 12 - 1 : date / 12) << ' ' << (date % 12 == 0 ? 12 : date % 12);
+
+	return 0;
+}
