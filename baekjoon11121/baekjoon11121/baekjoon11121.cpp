801ceb9dc51d84c13653e1621e16a0340ce8e1fc
ryxxng <i876838@gmail.com>
2025-03-18 20:53:38 +0900

    baekjoon 11121

---
+++ baekjoon11121/baekjoon11121/baekjoon11121.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input, output;
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> input >> output;
+		cout << (input == output ? "OK\n" : "ERROR\n");
+	}
+
+	return 0;
+}
