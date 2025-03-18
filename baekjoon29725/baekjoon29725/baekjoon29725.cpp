be48cf0fa2ad4d1dc73e59eab2abf8417f470434
ryxxng <i876838@gmail.com>
2025-03-18 20:55:22 +0900

    baekjoon 29725

---
+++ baekjoon29725/baekjoon29725/baekjoon29725.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char chess;
+	int scoreWhite = 0, scoreBlack = 0;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			cin >> chess;
+			if (chess == 'P') scoreWhite += 1;
+			else if (chess == 'p') scoreBlack += 1;
+			else if (chess == 'N' || chess == 'B') scoreWhite += 3;
+			else if (chess == 'n' || chess == 'b') scoreBlack += 3;
+			else if (chess == 'R') scoreWhite += 5;
+			else if (chess == 'r') scoreBlack += 5;
+			else if (chess == 'Q') scoreWhite += 9;
+			else if (chess == 'q') scoreBlack += 9;
+		}
+	}
+	cout << scoreWhite - scoreBlack;
+
+	return 0;
+}
