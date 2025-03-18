17dd7c8825eece4418a1778ab91d3672bb36950f
ryxxng <i876838@gmail.com>
2025-03-18 20:54:55 +0900

    baekjoon 24860

---
+++ baekjoon24860/baekjoon24860/baekjoon24860.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long result, v, d, j, vk, jk, vd, jd;
+	cin >> vk >> jk >> vd >> jd >> v >> d >> j;
+	result = v * d * j * vk * jk + v * d * j * vd * jd;
+	cout << result;
+
+	return 0;
+}
