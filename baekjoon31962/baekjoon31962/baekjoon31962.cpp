7c718eae7cf18a22bc5b983bebbc446329bb07b9
ryxxng <i876838@gmail.com>
2025-03-18 20:56:19 +0900

    baekjoon 31962

---
+++ baekjoon31962/baekjoon31962/baekjoon31962.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, n, s, t, max = -1;
+	cin >> n >> x;
+	for (int i = 0; i < n; i++) {
+		cin >> s >> t;
+		if (max < s && s + t <= x) max = s;
+	}
+	cout << max;
+
+	return 0;
+}
