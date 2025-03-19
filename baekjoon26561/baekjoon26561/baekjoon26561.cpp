b053d157944e18442dece9a759faabb92a0df976
ryxxng <i876838@gmail.com>
2025-03-19 20:20:54 +0900

    baekjoon 26561

---
+++ baekjoon26561/baekjoon26561/baekjoon26561.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long people, time;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> people >> time;
+		cout << people - (time / 7) + (time / 4) << '\n';
+	}
+
+	return 0;
+}
