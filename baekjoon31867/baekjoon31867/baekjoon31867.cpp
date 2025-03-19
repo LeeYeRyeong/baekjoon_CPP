0e192e95b7622aa954fd1fa7a2e130062c566f09
ryxxng <i876838@gmail.com>
2025-03-19 20:22:37 +0900

    baekjoon 31867

---
+++ baekjoon31867/baekjoon31867/baekjoon31867.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, even = 0, odd = 0;
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		if (static_cast<int>(s[i]) % 2 == 0) even++;
+		else odd++;
+	}
+	cout << (even == odd ? -1 : (even > odd ? 0 : 1));
+
+	return 0;
+}
