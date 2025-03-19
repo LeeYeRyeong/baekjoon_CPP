96981f0734e10fa1941649422750c72eb4469b2f
ryxxng <i876838@gmail.com>
2025-03-19 20:19:15 +0900

    baekjoon 31822

---
+++ baekjoon31822/baekjoon31822/baekjoon31822.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string code, recode;
+	int n, count = 0;
+	cin >> recode >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> code;
+		if (recode.substr(0, 5) == code.substr(0, 5)) count++;
+	}
+	cout << count;
+
+	return 0;
+}
