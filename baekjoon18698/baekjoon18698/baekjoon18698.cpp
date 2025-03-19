b5f8b2a92eb220fc42df05a62d0454680d416c5f
ryxxng <i876838@gmail.com>
2025-03-19 20:20:22 +0900

    baekjoon 18698

---
+++ baekjoon18698/baekjoon18698/baekjoon18698.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			if (s[j] == 'U') count++;
+			else break;
+		}
+		cout << count << '\n';
+		count = 0;
+	}
+
+	return 0;
+}
