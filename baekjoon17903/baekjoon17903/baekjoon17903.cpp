33e49fba49b1b0f80a7e71052030e18ab8dcc42d
ryxxng <i876838@gmail.com>
2025-03-19 20:21:25 +0900

    baekjoon 17903

---
+++ baekjoon17903/baekjoon17903/baekjoon17903.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num[20];
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) cin >> num[j];
+	}
+	cout << (n >= 8 ? "satisfactory" : "unsatisfactory");
+
+	return 0;
+}
