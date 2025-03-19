05900f4843ba67b29ec992c11135f4f438c99f7a
ryxxng <i876838@gmail.com>
2025-03-19 20:23:06 +0900

    baekjoon 30045

---
+++ baekjoon30045/baekjoon30045/baekjoon30045.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < s.size() - 1; j++) {
+			if ((s[j] == '0' && s[j + 1] == '1') || s[j] == 'O' && s[j + 1] == 'I') {
+				count++;
+				break;
+			}
+		}
+	}
+	cout << count;
+
+	return 0;
+}
