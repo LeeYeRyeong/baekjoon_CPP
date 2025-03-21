fd3bc647d09d0d1cc649e26d636734e125a6b439
ryxxng <i876838@gmail.com>
2025-03-21 16:27:40 +0900

    baekjoon 25858

---
+++ baekjoon25858/baekjoon25858/baekjoon25858.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int problem[30] = {};
+	int n, dollar, sum = 0;
+	cin >> n >> dollar;
+	for (int i = 0; i < n; i++) {
+		cin >> problem[i];
+		sum += problem[i];
+	}
+	for (int i = 0; i < n; i++) cout << (dollar / sum) * problem[i] << '\n';
+
+	return 0;
+}
