b983dc39fe1bf29d64b8a2c5be22b12b3e51b608
ryxxng <i876838@gmail.com>
2025-03-21 16:25:15 +0900

    baekjoon 24751

---
+++ baekjoon24751/baekjoon24751/baekjoon24751.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << fixed << setprecision(10) << 100.0 / static_cast<double>(n) << '\n' << 100.0 / (100.0 - static_cast<double>(n));
+
+	return 0;
+}
