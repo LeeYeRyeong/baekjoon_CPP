b5591a39482f3aca4ff5db654e0d08f8873c1198
ryxxng <i876838@gmail.com>
2025-03-21 16:24:22 +0900

    baekjoon 10188

---
+++ baekjoon10188/baekjoon10188/baekjoon10188.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, w, h;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> w >> h;
+		for (int j = 0; j < h; j++) {
+			for (int k = 0; k < w; k++) {
+				cout << 'X';
+			}
+			cout << '\n';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
