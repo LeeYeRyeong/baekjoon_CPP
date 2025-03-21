e5ea84a23553e9f4a5880eca12a3ea1cb2394d38
ryxxng <i876838@gmail.com>
2025-03-21 16:28:14 +0900

    baekjoon 31994

---
+++ baekjoon31994/baekjoon31994/baekjoon31994.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name, resultName;
+	int count, resultCount = 0;
+	for (int i = 0; i < 7; i++) {
+		cin >> name >> count;
+		if (count > resultCount) {
+			resultCount = count;
+			resultName = name;
+		}
+	}
+	cout << resultName;
+
+	return 0;
+}
