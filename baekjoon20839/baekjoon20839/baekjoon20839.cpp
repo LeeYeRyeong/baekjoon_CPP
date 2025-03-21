59c1f98bf00173482eae2d0f1be9a3afd2787226
ryxxng <i876838@gmail.com>
2025-03-21 16:27:14 +0900

    baekjoon 20839

---
+++ baekjoon20839/baekjoon20839/baekjoon20839.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, c, e, studentA, studentC, studentE;
+	cin >> a >> c >> e >> studentA >> studentC >> studentE;
+	if (a <= studentA && c <= studentC && e <= studentE) cout << 'A';
+	else if (static_cast<double>(a) / 2 <= studentA && c <= studentC && e <= studentE) cout << 'B';
+	else if (c <= studentC && e <= studentE) cout << 'C';
+	else if (static_cast<double>(c) / 2.0 <= studentC && e <= studentE) cout << 'D';
+	else cout << 'E';
+
+	return 0;
+}
