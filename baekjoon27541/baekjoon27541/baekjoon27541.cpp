ee3c89fd606ebc361ed7d01a9b2692cb6409327b
ryxxng <i876838@gmail.com>
2025-03-21 16:28:39 +0900

    baekjoon 27541

---
+++ baekjoon27541/baekjoon27541/baekjoon27541.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n >> s;
+	if (s[n - 1] == 'G') for (int i = 0; i < n - 1; i++) cout << s[i];
+	else cout << s << 'G';
+
+	return 0;
+}
