2e543d6f3911d29125147906766e81ad2d2e08e9
ryxxng <i876838@gmail.com>
2025-03-21 16:29:13 +0900

    baekjoon 13496

---
+++ baekjoon13496/baekjoon13496/baekjoon13496.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, num, speed, day, distance, value, count = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> num >> speed >> day;
+		for (int j = 0; j < num; j++) {
+			cin >> distance >> value;
+			if (speed * day >= distance) count += value;
+		}
+		cout << "Data Set " << i + 1 << ":\n" << count << "\n\n";
+		count = 0;
+	}
+
+	return 0;
+}
