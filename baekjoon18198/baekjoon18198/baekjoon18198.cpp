28981af6bc271e0184472d0c4888e504a31e9d7e
ryxxng <i876838@gmail.com>
2025-03-21 16:25:38 +0900

    baekjoon 18198

---
+++ baekjoon18198/baekjoon18198/baekjoon18198.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int scoreA = 0, scoreB = 0;
+	cin >> s;
+	for (int i = 0; i < s.size() / 2; i++) {
+		if (s[i * 2] == 'A') scoreA += s[i * 2 + 1];
+		else scoreB += s[i * 2 + 1];
+	}
+	cout << (scoreA > scoreB ? 'A' : 'B');
+
+	return 0;
+}
