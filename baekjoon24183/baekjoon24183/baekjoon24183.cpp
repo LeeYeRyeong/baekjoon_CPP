392c1b2ca9e70aeabc2e49d8bca8dd3d14e49fbc
ryxxng <i876838@gmail.com>
2025-03-21 16:23:50 +0900

    baekjoon 24183

---
+++ baekjoon24183/baekjoon24183/baekjoon24183.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double gram;
+	int a, b, c;
+	cin >> a >> b >> c;
+	gram = a * 0.229 * 0.324 * 2 + b * 0.297 * 0.420 * 2+ c * 0.210 * 0.297;
+	cout << fixed << setprecision(6) << gram;
+
+	return 0;
+}
