7835b232f0e5affc6e94392375d4d078413f0155
ryxxng <i876838@gmail.com>
2025-03-22 10:35:09 +0900

    baekjoon 30794

---
+++ baekjoon30794/baekjoon30794/baekjoon30794.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n >> s;
+	if (s == "miss") n *= 0;
+	else if (s == "bad") n *= 200;
+	else if (s == "cool") n *= 400;
+	else if (s == "great") n *= 600;
+	else if (s == "perfect") n *= 1000;
+	cout << n;
+
+	return 0;
+}
