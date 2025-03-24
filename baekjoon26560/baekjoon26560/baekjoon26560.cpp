6415640eec5f0dd95da7facc0d75fa5c2a6b3242
ryxxng <i876838@gmail.com>
2025-03-24 20:06:05 +0900

    baekjoon 26560

---
+++ baekjoon26560/baekjoon26560/baekjoon26560.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; i++) {
+		getline(cin, s);
+		if (s[s.size() - 1] != '.') cout << s << ".\n";
+		else cout << s << '\n';
+	}
+
+	return 0;
+}
