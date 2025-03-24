078685dc072697cf7c82771c06b79422d084f4d7
ryxxng <i876838@gmail.com>
2025-03-24 20:08:32 +0900

    baekjoon 26736

---
+++ baekjoon26736/baekjoon26736/baekjoon26736.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int countA = 0, countB = 0;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'A') countA++;
+		else countB++;
+	}
+	cout << countA << " : " << countB;
+
+	return 0;
+}
