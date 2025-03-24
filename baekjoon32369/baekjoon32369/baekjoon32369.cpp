3a5faaa07fd858dbc230977aa0c1330d0a72a51e
ryxxng <i876838@gmail.com>
2025-03-24 20:06:57 +0900

    baekjoon 32369

---
+++ baekjoon32369/baekjoon32369/baekjoon32369.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, badOnion = 1, goodOnion = 1;
+	cin >> n >> a >> b;
+	for (int i = 0; i < n; i++) {
+		goodOnion += a;
+		badOnion += b;
+		if (goodOnion < badOnion) {
+			int temp = goodOnion;
+			goodOnion = badOnion;
+			badOnion = temp;
+		}
+		else if (goodOnion == badOnion) badOnion--;
+	}
+	cout << goodOnion << ' ' << badOnion;
+
+	return 0;
+}
