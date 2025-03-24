cd0d823367a538a0b9f2ad9578d97367b5b6a495
ryxxng <i876838@gmail.com>
2025-03-24 20:08:11 +0900

    baekjoon 24294

---
+++ baekjoon24294/baekjoon24294/baekjoon24294.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << a + b * 2 + c + d * 2 + (a < c ? c - a : a - c) + 4;
+
+	return 0;
+}
