af7c17b288a3864257d22df5d04ecd08f4a4b807
ryxxng <i876838@gmail.com>
2025-03-24 20:05:39 +0900

    baekjoon 24365

---
+++ baekjoon24365/baekjoon24365/baekjoon24365.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, mid = 0;
+	cin >> a >> b >> c;
+	mid = (a + b + c) / 3;
+	cout << (mid - a) * 2 + (mid - b);
+
+	return 0;
+}
