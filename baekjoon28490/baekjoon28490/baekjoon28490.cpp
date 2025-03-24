816076e5744440b541ffe3f58f71f0329d1e22f7
ryxxng <i876838@gmail.com>
2025-03-24 20:07:23 +0900

    baekjoon 28490

---
+++ baekjoon28490/baekjoon28490/baekjoon28490.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, w, h, area = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> w >> h;
+		area = (area < w * h ? w * h : area);
+	}
+	cout << area;
+
+	return 0;
+}
