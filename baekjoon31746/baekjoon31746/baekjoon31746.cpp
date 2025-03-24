eba7c01224bc2904cad878a4d697c28d0ec8d6f5
ryxxng <i876838@gmail.com>
2025-03-24 20:06:30 +0900

    baekjoon 31746

---
+++ baekjoon31746/baekjoon31746/baekjoon31746.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s = "SciComLove", reverseS = "evoLmoCicS";
+	int n;
+	cin >> n;
+	cout << (n % 2 == 0 ? s : reverseS);
+
+	return 0;
+}
