9f34c1313dbb924e259b886a528928b198febb2d
ryxxng <i876838@gmail.com>
2025-03-25 19:22:25 +0900

    baekjoon 32288

---
+++ baekjoon32288/baekjoon32288/baekjoon32288.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 'I') cout << 'i';
+		else cout << 'L';
+	}
+
+	return 0;
+}
