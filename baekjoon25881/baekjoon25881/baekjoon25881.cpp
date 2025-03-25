e065db8acece90f4380c7d5de4051fcc0348fed7
ryxxng <i876838@gmail.com>
2025-03-25 19:21:38 +0900

    baekjoon 25881

---
+++ baekjoon25881/baekjoon25881/baekjoon25881.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, originCost, addCost, kwh;
+	cin >> originCost >> addCost >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> kwh;
+		cout << kwh << ' ' << (kwh > 1000 ? (1000 * originCost) + (kwh - 1000) * addCost : kwh * originCost) << '\n';
+	}
+
+	return 0;
+}
