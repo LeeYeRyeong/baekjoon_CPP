b17974dee99c624badd6801cddd4ad3da39cd4e2
ryxxng <i876838@gmail.com>
2025-03-25 19:22:49 +0900

    baekjoon 21665

---
+++ baekjoon21665/baekjoon21665/baekjoon21665.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, errorCount = 0;
+	char image[100][100] = {}, reverseImage[100][100] = {};
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) cin >> image[i][j];
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) cin >> reverseImage[i][j];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (image[i][j] == reverseImage[i][j]) errorCount++;
+		}
+	}
+	cout << errorCount;
+
+	return 0;
+}
