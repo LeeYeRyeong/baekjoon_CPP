ad25f020e8028797e442e189d5a2c63e7c5e551e
ryxxng <i876838@gmail.com>
2025-03-25 19:22:03 +0900

    baekjoon 24356

---
+++ baekjoon24356/baekjoon24356/baekjoon24356.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t1, m1, t2, m2, time = 0, spin;
+	cin >> t1 >> m1 >> t2 >> m2;
+	if (m2 < m1) {
+		t2--;
+		m2 += 60;
+	}
+	if (t2 < t1) t2 += 24;
+	time += (m2 - m1) + (t2 - t1) * 60;
+	spin = time / 30;
+	cout << time << ' ' << spin;
+
+	return 0;
+}
