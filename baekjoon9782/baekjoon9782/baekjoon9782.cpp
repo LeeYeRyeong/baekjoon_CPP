0a456df7c71a9a76a71b75ff97bb2576d971e70d
ryxxng <i876838@gmail.com>
2025-03-25 19:21:16 +0900

    baekjoon 9782

---
+++ baekjoon9782/baekjoon9782/baekjoon9782.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int arr[100] = {};
+	double mid = 0.0f;
+	int n, count = 1;
+	while (1) {
+		cin >> n;
+		if (n == 0) break;
+		for (int i = 0; i < n; i++) cin >> arr[i];
+		if (n % 2 == 0) mid = (static_cast<double>(arr[n / 2 - 1]) + arr[n / 2]) / 2.0;
+		else mid = arr[n / 2];
+		cout << "Case " << count++ << ": " << fixed << setprecision(1) << mid << '\n';
+	}
+
+	return 0;
+}
