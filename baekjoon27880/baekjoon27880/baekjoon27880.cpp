ecc6491ac16612dd54530df67db4e4a0eafc0366
ryxxng <i876838@gmail.com>
2025-03-26 17:39:50 +0900

    baekjoon 27880

---
+++ baekjoon27880/baekjoon27880/baekjoon27880.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, h = 0;
+	for (int i = 0; i < 4; i++) {
+		cin >> s >> n;
+		if (s == "Es") h += n * 21;
+		else h += n * 17;
+	}
+	cout << h;
+
+	return 0;
+}
