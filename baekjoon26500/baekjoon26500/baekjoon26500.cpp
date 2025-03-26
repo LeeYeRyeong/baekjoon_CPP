40eb95dfad40d43acaebbbe2f8f573b615bbd09f
ryxxng <i876838@gmail.com>
2025-03-26 17:41:18 +0900

    baekjoon 26500

---
+++ baekjoon26500/baekjoon26500/baekjoon26500.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double a, b;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		cout << fixed << setprecision(1) << (a > b ? a - b : b - a) << '\n';
+	}
+
+	return 0;
+}
