1a95a3d52d8fd06d04f0399c82eccabe5fbd03de
ryxxng <i876838@gmail.com>
2025-03-26 17:38:48 +0900

    baekjoon 26332

---
+++ baekjoon26332/baekjoon26332/baekjoon26332.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count, prize;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> count >> prize;
+		cout << count << ' ' << prize << '\n' << count * prize - (count - 1) * 2 << '\n';
+	}
+	return 0;
+}
