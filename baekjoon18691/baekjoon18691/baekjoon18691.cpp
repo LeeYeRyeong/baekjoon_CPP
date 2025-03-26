0b13a2567c385144cfb613889a63b41d7887a5d3
ryxxng <i876838@gmail.com>
2025-03-26 17:39:19 +0900

    baekjoon 18691

---
+++ baekjoon18691/baekjoon18691/baekjoon18691.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, groupNum, currentCount, needCount;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> groupNum >> currentCount >> needCount;
+		if (currentCount >= needCount) cout << 0 << '\n';
+		else if (groupNum == 1) cout << needCount - currentCount << '\n';
+		else if (groupNum == 2) cout << 3 * (needCount - currentCount) << '\n';
+		else if (groupNum == 3) cout << 5 * (needCount - currentCount) << '\n';
+	}
+
+	return 0;
+}
