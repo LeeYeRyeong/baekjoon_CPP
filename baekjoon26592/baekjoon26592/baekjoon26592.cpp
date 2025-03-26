5dc29266cc5f0a0700c4821c15c0eaa4c540c099
ryxxng <i876838@gmail.com>
2025-03-26 17:40:53 +0900

    baekjoon 26592

---
+++ baekjoon26592/baekjoon26592/baekjoon26592.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double area, base, height;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> area >> base;
+		height = area * 2 / base;
+		cout << "The height of the triangle is " << fixed << setprecision(2) <<height << " units\n";
+	}
+
+	return 0;
+}
