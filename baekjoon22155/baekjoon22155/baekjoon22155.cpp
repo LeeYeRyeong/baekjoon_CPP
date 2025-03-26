7b52c5fc20912b418a3f2dc0c7d69b6c607f9c98
ryxxng <i876838@gmail.com>
2025-03-26 17:40:23 +0900

    baekjoon 22155

---
+++ baekjoon22155/baekjoon22155/baekjoon22155.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		if ((a <= 1 && b <= 2) || (a <= 2 && b <= 1)) cout << "Yes\n";
+		else cout << "No\n";
+	}
+
+	return 0;
+}
