65e1159decaa09c7c2ae946bf758e14bdba463dc
ryxxng <i876838@gmail.com>
2025-03-27 23:03:39 +0900

    baekjoon 25932

---
+++ baekjoon25932/baekjoon25932/baekjoon25932.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num[10] = {};
+	int n;
+	bool mack = false, zack = false;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 10; j++) {
+			cin >> num[j];
+			if (num[j] == 17) zack = true;
+			else if (num[j] == 18) mack = true;
+		}
+		for (int j = 0; j < 10; j++) cout << num[j] << ' ';
+		cout << '\n';
+		if (mack == true && zack == true) cout << "both\n";
+		else if (mack == false && zack == false) cout << "none\n";
+		else if (mack == true) cout << "mack\n";
+		else if (zack == true) cout << "zack\n";
+		cout << '\n';
+		mack = false;
+		zack = false;
+	}
+
+	return 0;
+}
