7d48a1986404e32688362567a0ba733d89b6d910
ryxxng <i876838@gmail.com>
2025-03-28 17:45:26 +0900

    baekjoon 26742

---
+++ baekjoon26742/baekjoon26742/baekjoon26742.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int white = 0, black = 0;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'B') black++;
+		else if (s[i] == 'C') white++;
+	}
+	cout << (white / 2) + (black / 2);
+
+	return 0;
+}
