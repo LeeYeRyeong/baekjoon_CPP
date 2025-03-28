9a38f980b56cb62f5cfd0a16510fad43ea983150
ryxxng <i876838@gmail.com>
2025-03-28 17:44:14 +0900

    baekjoon 25893

---
+++ baekjoon25893/baekjoon25893/baekjoon25893.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int score[3] = {};
+	int n, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> score[j];
+			if (score[j] >= 10) count++;
+		}
+		for (int j = 0; j < 3; j++) cout << score[j] << ' ';
+		if (count == 0) cout << "\nzilch\n\n";
+		else if (count == 1) cout << "\ndouble\n\n";
+		else if (count == 2) cout << "\ndouble-double\n\n";
+		else if (count == 3) cout << "\ntriple-double\n\n";
+		count = 0;
+	}
+
+	return 0;
+}
