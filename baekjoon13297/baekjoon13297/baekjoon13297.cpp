aebb60afb34721d214a088665a3db8a5d4d2f9fc
ryxxng <i876838@gmail.com>
2025-03-28 17:46:18 +0900

    baekjoon 13297

---
+++ baekjoon13297/baekjoon13297/baekjoon13297.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		cout << s.size() << '\n';
+	}
+
+	return 0;
+}
