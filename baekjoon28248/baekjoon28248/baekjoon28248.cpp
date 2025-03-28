1ec7e29004362082fa0022a7e9a9c4271fd2fd4d
ryxxng <i876838@gmail.com>
2025-03-28 17:41:25 +0900

    baekjoon 28248

---
+++ baekjoon28248/baekjoon28248/baekjoon28248.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, score = 0;
+	cin >> n >> m;
+	score += (n * 50) - (m * 10) + (n > m ? 500 : 0);
+	cout << score;
+
+	return 0;
+}
