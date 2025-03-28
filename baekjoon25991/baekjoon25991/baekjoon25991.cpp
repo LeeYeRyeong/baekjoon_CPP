d77455a100d345a2205d53190527507747a99efb
ryxxng <i876838@gmail.com>
2025-03-28 17:45:02 +0900

    baekjoon 25591

---
+++ baekjoon25991/baekjoon25991/baekjoon25991.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double length, sum = 0, result;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> length;
+		sum += pow(length, 3);
+	}
+	result = cbrt(sum * 1.0);
+	cout << fixed << setprecision(13) << result;
+
+	return 0;
+}
