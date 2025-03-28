5d2c456b3514e53e6121ed72a2eddbf92927511b
ryxxng <i876838@gmail.com>
2025-03-28 17:39:23 +0900

    baekjoon 5565

---
+++ baekjoon5565/baekjoon5565/baekjoon5565.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int total, book;
+	cin >> total;
+	for (int i = 0; i < 9; i++) {
+		cin >> book;
+		total -= book;
+	}
+	cout << total;
+
+	return 0;
+}
