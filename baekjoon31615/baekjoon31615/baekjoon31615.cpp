721c3fd8ea3f084e553c9d08885241e7bbdecca1
ryxxng <i876838@gmail.com>
2025-03-31 23:06:11 +0900

    baekjoon 31615

---
+++ baekjoon31615/baekjoon31615/baekjoon31615.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, sum = 0, count = 0;
+	cin >> n >> m;
+	sum = n + m;
+	while (sum > 0) {
+		count++;
+		sum /= 10;
+	}
+	cout << count;
+
+	return 0;
+}
