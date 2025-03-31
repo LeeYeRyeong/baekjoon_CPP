2ebda86d4ce1b54da6dd92f1c452745391f4cf52
ryxxng <i876838@gmail.com>
2025-03-31 23:07:17 +0900

    baekjoon 26731

---
+++ baekjoon26731/baekjoon26731/baekjoon26731.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int check[26] = {};
+	char result;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) check[s[i] - 65]++;
+	for (int i = 0; i < 26; i++) {
+		if (check[i] == 0) result = static_cast<char>(i + 65);
+	}
+	cout << result;
+
+	return 0;
+}
