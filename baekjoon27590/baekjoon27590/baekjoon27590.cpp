f05c9445a1bb92c775edc39e7ed09b168d3046be
ryxxng <i876838@gmail.com>
2025-04-01 23:34:54 +0900

    baekjoon 27590

---
+++ baekjoon27590/baekjoon27590/baekjoon27590.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int ds, ys, dm, ym, sun, moon;
+	cin >> ds >> ys >> dm >> ym;
+	sun = ys - ds;
+	moon = ym - dm;
+	while (sun != moon) {
+		if (sun < moon) sun += ys;
+		else moon += ym;
+	}
+	cout << sun;
+
+	return 0;
+}
