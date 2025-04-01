fe46d74bc31b3619cad0ac02cbe0c23a8770db91
ryxxng <i876838@gmail.com>
2025-04-01 23:35:21 +0900

    baekjoon 31306

---
+++ baekjoon31306/baekjoon31306/baekjoon31306.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int count = 0, countY = 0;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') count++;
+		else if (s[i] == 'y') countY++;
+	}
+	cout << count << ' ' << count + countY;
+
+	return 0;
+}
