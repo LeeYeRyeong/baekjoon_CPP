6a3610af66d54bfcd5119df69b88dc4095d4d39e
ryxxng <i876838@gmail.com>
2025-04-01 23:37:21 +0900

    baekjoon 32642

---
+++ baekjoon32642/baekjoon32642/baekjoon32642.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long angry = 0, status = 0;
+	int n, rain;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> rain;
+		if (rain == 0) status -= 1;
+		else status += 1;
+		angry += status;
+	}
+	cout << angry;
+
+	return 0;
+}
