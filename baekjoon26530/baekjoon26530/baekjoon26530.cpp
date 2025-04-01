6fa7c7d4663f504f5d0df598136d78322477f2fb
ryxxng <i876838@gmail.com>
2025-04-01 23:36:15 +0900

    baekjoon 26530

---
+++ baekjoon26530/baekjoon26530/baekjoon26530.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	double prize, sum = 0;
+	int t, n, count;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> name >> count >> prize;
+			sum += prize * count;
+		}
+		cout << "$" << fixed << setprecision(2) << sum << '\n';
+		sum = 0;
+	}
+
+	return 0;
+}
