55173a86979fd177286e69b4676f7dd963168931
ryxxng <i876838@gmail.com>
2025-04-01 23:41:46 +0900

    baekjoon 27239

---
+++ baekjoon27239/baekjoon27239/baekjoon27239.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << (n % 8 == 0 ? 'h' : static_cast<char>(n % 8 + 96)) << (n % 8 == 0 ? n / 8 : n / 8 + 1);
+
+	return 0;
+}
