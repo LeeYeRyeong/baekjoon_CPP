9b74c74f6bfe5435e89a08824cfbdcf671252133
ryxxng <i876838@gmail.com>
2025-04-01 23:35:50 +0900

    baekjoon 24196

---
+++ baekjoon24196/baekjoon24196/baekjoon24196.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s, result;
+	int check;
+	cin >> s;
+	for (int i = 0; i < s.size(); i += check) {
+		result += s[i];
+		check = static_cast<int>(s[i] - 64);
+		if (i != s.size() - 1 && check + i > s.size() - 1) result += s[s.size() - 1];
+	}
+	cout << result;
+
+	return 0;
+}
