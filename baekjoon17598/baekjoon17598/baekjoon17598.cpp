37fc786254d32b44353c0d54b134e64bf8eba6ee
ryxxng <i876838@gmail.com>
2025-04-01 23:41:22 +0900

    baekjoon 17598

---
+++ baekjoon17598/baekjoon17598/baekjoon17598.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int lion = 0, tiger = 0;
+	for (int i = 0; i < 9; i++) {
+		cin >> s;
+		if (s == "Lion") lion++;
+		else if (s == "Tiger") tiger++;
+	}
+	cout << (lion > tiger ? "Lion" : "Tiger");
+
+	return 0;
+}
