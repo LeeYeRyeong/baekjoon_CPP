a05844c8e76a2826161285b8fa0b597e42358ace
ryxxng <i876838@gmail.com>
2025-04-02 19:55:52 +0900

    baekjoon 26340:

---
+++ baekjoon26340/baekjoon26340/baekjoon26340.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, w, h, c, resultW, resultH;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> w >> h >> c;
+		resultW = w;
+		resultH = h;
+		for (int j = 0; j < c; j++) {
+			if (resultW > resultH) resultW /= 2;
+			else resultH /= 2;
+		}
+		cout << "Data set: " << w << ' ' << h << ' ' << c << '\n' << (resultW > resultH ? resultW : resultH) << ' '
+			<< (resultW < resultH ? resultW : resultH) << "\n\n";
+	}
+
+	return 0;
+}
