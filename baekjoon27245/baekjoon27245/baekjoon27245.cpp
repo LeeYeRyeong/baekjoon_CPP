e5e1bae6280e60753fd9019fec7fed575febc196
ryxxng <i876838@gmail.com>
2025-04-02 19:57:00 +0900

    baekjoon 27245

---
+++ baekjoon27245/baekjoon27245/baekjoon27245.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int w, h, l;
+	cin >> w >> h >> l;
+	if (((w < h ? w : h) >= l * 2) && (w > h ? w : h) <= (w < h ? w : h) * 2) cout << "good";
+	else cout << "bad";
+
+	return 0;
+}
