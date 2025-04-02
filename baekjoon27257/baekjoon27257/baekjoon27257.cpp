9ff721f7759a203e394e727a226caaeed7fde33b
ryxxng <i876838@gmail.com>
2025-04-02 19:58:57 +0900

    baekjoon 27257

---
+++ baekjoon27257/baekjoon27257/baekjoon27257.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int beauty = 0;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == '0') beauty++;
+	}
+	for (int i = s.size() - 1; i >= 0; i--) {
+		if (s[i] == '0') beauty--;
+		else break;
+	}
+	cout << beauty;
+
+	return 0;
+}
