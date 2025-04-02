fdae47ad88838696a7d24008f5024473846eb70a
ryxxng <i876838@gmail.com>
2025-04-02 19:58:34 +0900

    baekjoon 30791

---
+++ baekjoon30791/baekjoon30791/baekjoon30791.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int vote[5] = {};
+	int count = 0;
+	for (int i = 0; i < 5; i++) cin >> vote[i];
+	for (int i = 1; i < 5; i++) {
+		if ((vote[i] > vote[0] ? vote[i] - vote[0] : vote[0] - vote[i]) <= 1000) count++;
+	}
+	cout << count;
+
+	return 0;
+}
