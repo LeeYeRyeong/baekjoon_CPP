df19dacaef3d543eddc1b7b4948faf3cfba72700
ryxxng <i876838@gmail.com>
2025-04-03 15:28:34 +0900

    baekjoon 29986

---
+++ baekjoon29986/baekjoon29986/baekjoon29986.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, h, need, count = 0;
+	cin >> n >> h;
+	for (int i = 0; i < n; i++) {
+		cin >> need;
+		if (need <= h) count++;
+	}
+	cout << count;
+
+	return 0;
+}
