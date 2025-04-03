291af426fb70d76a5c77b68d6512a0a8015f6925
ryxxng <i876838@gmail.com>
2025-04-03 15:30:09 +0900

    baekjoon 30793

---
+++ baekjoon30793/baekjoon30793/baekjoon30793.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double n, m;
+	cin >> n >> m;
+	if (n / m < 0.2) cout << "weak";
+	else if (n / m < 0.4) cout << "normal";
+	else if (n / m < 0.6) cout << "strong";
+	else cout << "very strong";
+
+	return 0;
+}
