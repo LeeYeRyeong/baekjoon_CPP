2a8acbf2d6176bbb4ab1109b424f692ca355a70a
ryxxng <i876838@gmail.com>
2025-04-03 15:29:01 +0900

    baekjoon 32684

---
+++ baekjoon32684/baekjoon32684/baekjoon32684.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double scoreCho = 72, scoreHan = 73.5;
+	int cho[6] = {}, han[6] = {}, score[6] = { 13,7,5,3,3,2 };
+	for (int i = 0; i < 6; i++) cin >> cho[i];
+	for (int i = 0; i < 6; i++) cin >> han[i];
+	for (int i = 0; i < 6; i++) {
+		scoreCho += score[i] * cho[i];
+		scoreHan += score[i] * han[i];
+	}
+	cout << (scoreCho > scoreHan ? "cocjr0208" : "ekwoo");
+
+	return 0;
+}
