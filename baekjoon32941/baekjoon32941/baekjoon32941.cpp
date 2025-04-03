b557eb3f33f262e5fb986f94798e18b7acf1a76d
ryxxng <i876838@gmail.com>
2025-04-03 15:31:49 +0900

    baekjoon 32941

---
+++ baekjoon32941/baekjoon32941/baekjoon32941.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, x, n, count, aval;
+	int student[10] = {};
+	bool check = true;
+	cin >> t >> x >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> count;
+		for (int j = 0; j < count; j++) {
+			cin >> aval;
+			if (aval == x) student[i] = 1;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (student[i] != 1) {
+			check = false;
+			break;
+		}
+	}
+	cout << (check ? "YES" : "NO");
+
+	return 0;
+}
