6e45c238b145e981e2dd037bbe8a86df49dad1b4
ryxxng <i876838@gmail.com>
2025-04-03 15:31:15 +0900

    baekjoon 31048

---
+++ baekjoon31048/baekjoon31048/baekjoon31048.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, num, result = 1;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		if (num > 4) cout << 0;
+		else {
+			for (int i = 1; i <= num; i++) result *= i;
+			cout << result % 10;
+		}
+		cout << '\n';
+		result = 1;
+	}
+
+	return 0;
+}
