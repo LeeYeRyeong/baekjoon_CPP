77fc936642c6b6c11e0644a9010bc02d892ffece
ryxxng <i876838@gmail.com>
2025-04-03 15:30:52 +0900

    baekjoon 31518

---
+++ baekjoon31518/baekjoon31518/baekjoon31518.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int check[3] = { 0,0,0 };
+	int n, number;
+	cin >> n;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> number;
+			if (number == 7) check[i] = 1;
+		}
+	}
+	cout << (check[0] == 1 && check[1] == 1 && check[2] == 1 ? 777 : 0);
+
+	return 0;
+}
