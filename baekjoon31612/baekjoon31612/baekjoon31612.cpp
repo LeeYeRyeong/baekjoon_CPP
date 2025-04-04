bbb98207bc2554e059ae36a7f9bdd042e6f87f3c
ryxxng <i876838@gmail.com>
2025-04-04 21:51:01 +0900

    baekjoon 31612

---
+++ baekjoon31612/baekjoon31612/baekjoon31612.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, count = 0;
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 'o') count += 1;
+		else count += 2;
+	}
+	cout << count;
+
+	return 0;
+}
