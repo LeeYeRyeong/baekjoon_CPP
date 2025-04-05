b6493c44ac7dfa56f0fa890bb2175477c7bb5709
ryxxng <i876838@gmail.com>
2025-04-05 10:35:17 +0900

    baekjoon 31281

---
+++ baekjoon31281/baekjoon31281/baekjoon31281.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long a, b, c, mid = 0;
+	cin >> a >> b >> c;
+	if (a > b && a < c || a >c && a < b) mid = a;
+	else if (b > a && b < c || b > c && b < a) mid = b;
+	else if (c > a && c < b || c > b && c < a) mid = c;
+	cout << mid;
+
+	return 0;
+}
