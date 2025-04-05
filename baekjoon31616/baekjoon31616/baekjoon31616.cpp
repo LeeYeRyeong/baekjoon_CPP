4ac41f501f8b15850f4084c3b773f234788f005e
ryxxng <i876838@gmail.com>
2025-04-05 10:34:54 +0900

    baekjoon 31616

---
+++ baekjoon31616/baekjoon31616/baekjoon31616.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int  n;
+	bool check = true;
+	cin >> n >> s;
+	for (int i = 0; i < s.size() - 1; i++) {
+		if (s[i] != s[i + 1]) {
+			check = false;
+			break;
+		}
+	}
+	cout << (check ? "Yes" : "No");
+
+	return 0;
+}
