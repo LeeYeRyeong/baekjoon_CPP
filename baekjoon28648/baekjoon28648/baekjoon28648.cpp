3618c4f85438632de208085f847d3533c7115cdb
ryxxng <i876838@gmail.com>
2025-04-08 21:03:43 +0900

    baekjoon 28648

---
+++ baekjoon28648/baekjoon28648/baekjoon28648.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, t, l, min = 200;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> t >> l;
+        if (min > t + l) min = t + l;
+    }
+    cout << min;
+
+    return 0;
+}
