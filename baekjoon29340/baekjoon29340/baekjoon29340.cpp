d1634ba97e2dc6c221c974b28546d22f196920e3
ryxxng <i876838@gmail.com>
2025-04-08 21:05:29 +0900

    baekjoon 29340

---
+++ baekjoon29340/baekjoon29340/baekjoon29340.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1, s2, result;
+	cin >> s1 >> s2;
+	for (int i = 0; i < s1.size(); i++) {
+		result += (static_cast<int>(s1[i]) > static_cast<int>(s2[i]) ? s1[i] : s2[i]);
+	}
+	cout << result;
+
+	return 0;
+}
