cbc834b0224dbcdeebcda34ff4558551ee3f3636
ryxxng <i876838@gmail.com>
2025-04-08 21:04:57 +0900

    baekjoon 31656

---
+++ baekjoon31656/baekjoon31656/baekjoon31656.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s, result;
+	getline(cin, s);
+	result += s[0];
+	for (int i = 1; i < s.size(); i++) {
+		if (result[result.size() - 1] != s[i]) result += s[i];
+	}
+	cout << result;
+
+	return 0;
+}
