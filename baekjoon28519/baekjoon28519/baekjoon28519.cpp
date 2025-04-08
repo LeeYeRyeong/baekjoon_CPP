0a9d28c98a9bb77c36791b51ab4d01f4196e2a68
ryxxng <i876838@gmail.com>
2025-04-08 21:04:32 +0900

    baekjoon 28519

---
+++ baekjoon28519/baekjoon28519/baekjoon28519.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	cout << ((n > m ? (n - m) : m - n) > 1 ? (n > m ? m * 2 + 1 : n * 2 + 1) : n + m);
+
+	return 0;
+}
