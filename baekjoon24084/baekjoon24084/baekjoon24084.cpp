54fab33b720399021058b6725d4ae2a6526abef9
ryxxng <i876838@gmail.com>
2025-04-08 21:04:11 +0900

    baekjoon 24084

---
+++ baekjoon24084/baekjoon24084/baekjoon24084.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n >> s;
+	for (int i = 0; i < n - 1; i++) {
+		if (s[i + 1] == 'J') cout << s[i] << '\n';
+	}
+
+	return 0;
+}
