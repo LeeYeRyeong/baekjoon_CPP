19b2e45227d06be2a80ed7cbc55d95375346a451
ryxxng <i876838@gmail.com>
2025-04-08 21:02:42 +0900

    baekjoon 31609

---
+++ baekjoon31609/baekjoon31609/baekjoon31609.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int digitNumber[10] = {};
+	int n, num;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		digitNumber[num] = 1;
+	}
+	for (int i = 0; i < 10; i++) {
+		if (digitNumber[i] == 1) cout << i << '\n';
+	}
+
+	return 0;
+}
