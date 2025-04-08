fda373e2382f251a2c3669ace19f3a2c7f6495b8
ryxxng <i876838@gmail.com>
2025-04-08 21:03:10 +0900

    baekjoon 20336

---
+++ baekjoon20336/baekjoon20336/baekjoon20336.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+	string menu[100][42];
+	int count[100] = {};
+	int n, result, index;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> count[i];
+		for (int j = 0; j < count[i]; j++) cin >> menu[i][j];
+	}
+	srand(time(NULL));
+	index = rand() % n;
+	result = count[index];
+	cout << result << '\n';
+	for (int i = 0; i < result; i++) cout << menu[index][i] << '\n';
+
+	return 0;
+}
