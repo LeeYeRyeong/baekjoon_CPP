e470ee14bc7e962a7744775ca4600518274d993f
ryxxng <i876838@gmail.com>
2025-04-08 21:02:18 +0900

    baekjoon 31636:

---
+++ baekjoon31636/baekjoon31636/baekjoon31636.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    int n;
+    bool check = false;
+    cin >> n >> s;
+    for (int i = 0; i < n - 2; i++) {
+        if (s[i] == 'o' && s[i + 1] == 'o' && s[i + 2] == 'o') {
+            check = true;
+            break;
+        }
+    }
+    cout << (check ? "Yes" : "No");
+
+    return 0;
+
+}
