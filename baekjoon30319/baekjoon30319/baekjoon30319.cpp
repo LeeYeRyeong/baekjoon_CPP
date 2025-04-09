a71ebb3e7966ae23a2fe9d674f0356f57dacbb7f
ryxxng <i876838@gmail.com>
2025-04-09 12:59:15 +0900

    baekjoon 30319

---
+++ baekjoon30319/baekjoon30319/baekjoon30319.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int year, month, date;
+	char dash;
+	cin >> year >> dash >> month >> dash >> date;
+	if (month > 9 || (month == 9 && date > 16)) cout << "TOO LATE";
+	else cout << "GOOD";
+
+	return 0;
+}
