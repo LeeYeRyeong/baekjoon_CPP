a2b2cd72af6b3c7e13033c207cda91bb93de107e
ryxxng <i876838@gmail.com>
2025-04-09 21:32:29 +0900

    baekjoon 29683

---
+++ baekjoon29683/baekjoon29683/baekjoon29683.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, cost, count = 0;
+	cin >> n >> a;
+	for (int i = 0; i < n; i++) {
+		cin >> cost;
+		count += cost / a;
+	}
+	cout << count;
+
+	return 0;
+}
