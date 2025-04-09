1fa42994998f9a5c373e5c1055be43fcdd57d67a
ryxxng <i876838@gmail.com>
2025-04-09 21:31:59 +0900

    baekjoon 33046

---
+++ baekjoon33046/baekjoon33046/baekjoon33046.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int dice1, dice2, dice3, dice4, n = 1;
+	cin >> dice1 >> dice2 >> dice3 >> dice4;
+	n = (((n + dice1 + dice2) % 4 == 0 ? 3 : ((n + dice1 + dice2) % 4 - 1)) == 0 ? 4 : ((n + dice1 + dice2) % 4 == 0 ? 3 : ((n + dice1 + dice2) % 4 - 1)));
+	cout << (((n + dice3 + dice4) % 4 == 0 ? 3 : (n + dice3 + dice4) % 4 - 1) == 0 ? 4 : ((n + dice3 + dice4) % 4 == 0 ? 3 : (n + dice3 + dice4) % 4 - 1));
+
+	return 0;
+}
