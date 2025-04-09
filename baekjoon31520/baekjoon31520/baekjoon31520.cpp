2427026e847331b65b5d082374e64d71a7137c7d
ryxxng <i876838@gmail.com>
2025-04-09 11:52:55 +0900

    baekjoon 31520

---
+++ baekjoon31520/baekjoon31520/baekjoon31520.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int result;
+	bool check = true;
+	cin >> s;
+	if (s.size() == 1 && s[0] != '1') check = false;
+	if (s[0] != '1') check = false;
+	else {
+		for (int i = 0; i < s.size() - 1; i++) {
+			if (static_cast<int>(s[i]) + 1 != static_cast<int>(s[i + 1])) {
+				check = false;
+				break;
+			}
+		}
+	}
+	result = (check ? s.size() : -1);
+	cout << result;
+
+	return 0;
+}
