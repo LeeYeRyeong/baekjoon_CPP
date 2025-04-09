ea624a207f549b700d20e3d61217c416862e4e42
ryxxng <i876838@gmail.com>
2025-04-09 21:33:00 +0900

    baekjoon 30585

---
+++ baekjoon30585/baekjoon30585/baekjoon30585.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int red = 0, blue = 0, r, c;
+	char color;
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cin >> color;
+			if (color == '0') red++;
+			else if (color == '1') blue++;
+		}
+	}
+	cout << (red < blue ? red : blue);
+
+	return 0;
+}
