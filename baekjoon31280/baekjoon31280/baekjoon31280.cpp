ac26d9a05218f9f35bad43e542b353884b7fa44d
ryxxng <i876838@gmail.com>
2025-04-09 12:58:29 +0900

    baekjoon 31280

---
+++ baekjoon31280/baekjoon31280/baekjoon31280.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int mouse, min = 500, count = 0;
+	for (int i = 0; i < 4; i++) {
+		cin >> mouse;
+		count += mouse;
+		if (min > mouse) min = mouse;
+	}
+	count -= (min - 1);
+	cout << count;
+
+	return 0;
+}
