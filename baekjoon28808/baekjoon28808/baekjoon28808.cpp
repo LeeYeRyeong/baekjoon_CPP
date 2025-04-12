1cb826ff7bbb9da2d135c055a6a2633488b09b4d
ryxxng <i876838@gmail.com>
2025-04-12 09:57:48 +0900

    baekjoon 28808

---
+++ baekjoon28808/baekjoon28808/baekjoon28808.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, count = 0;
+	char correct[100][100] = {};
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) cin >> correct[i][j];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (correct[i][j] == '+') {
+				count++;
+				break;
+			}
+		}
+	}
+	cout << count;
+
+	return 0;
+}
