e05291a3e82b7544a006896b71855360cb2e26b1
ryxxng <i876838@gmail.com>
2025-04-13 10:41:29 +0900

    baekjoon 30658

---
+++ baekjoon30658/baekjoon30658/baekjoon30658.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[100] = {};
+	int n;
+	do {
+		cin >> n;
+		for (int i = 0; i < n; i++) cin >> arr[i];
+		for (int i = n - 1; i >= 0; i--) cout << arr[i] << '\n';
+		if (n != 0) cout << 0 << '\n';
+	} while (n != 0);
+
+	return 0;
+}
