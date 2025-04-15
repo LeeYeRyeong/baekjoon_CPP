d6c77ca4c74b87132453d884a26a9729b6bfb27a
ryxxng <i876838@gmail.com>
2025-04-15 11:19:56 +0900

    baekjoon 32306

---
+++ baekjoon32306/baekjoon32306/baekjoon32306.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int shoot1[3] = {}, shoot2[3] = {}, score[3] = { 1,2,3 };
+	int score1 = 0, score2 = 0;
+	for (int i = 0; i < 3; i++) cin >> shoot1[i];
+	for (int i = 0; i < 3; i++) cin >> shoot2[i];
+	for (int i = 0; i < 3; i++) {
+		score1 += shoot1[i] * score[i];
+		score2 += shoot2[i] * score[i];
+	}
+	cout << (score1 == score2 ? 0 : (score1 > score2 ? 1 : 2));
+
+	return 0;
+}
