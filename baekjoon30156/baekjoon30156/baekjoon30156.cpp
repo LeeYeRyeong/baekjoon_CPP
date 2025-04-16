26ab63b385dfa2805e334d7c52a935f5f25d7ad7
ryxxng <i876838@gmail.com>
2025-04-16 19:49:10 +0900

    baekjoon 30156

---
+++ baekjoon30156/baekjoon30156/baekjoon30156.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, countAmber = 0, countBrass = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < s.size(); j++) {
+			if (s[j] == 'a') countAmber++;
+			else countBrass++;
+		}
+		cout << (countAmber < countBrass ? countAmber : countBrass) << '\n';
+		countAmber = 0;
+		countBrass = 0;
+	}
+
+	return 0;
+}
