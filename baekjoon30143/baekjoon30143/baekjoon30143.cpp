fbe65248271ca8291acbf9c5971ac13e9281a3f6
ryxxng <i876838@gmail.com>
2025-04-16 18:43:35 +0900

    baekjoon 30143

---
+++ baekjoon30143/baekjoon30143/baekjoon30143.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, a, d, sum = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n >> a >> d;
+		for (int j = 0; j < n; j++) sum += a + d * j;
+		cout << sum << '\n';
+		sum = 0;
+	}
+
+	return 0;
+}
