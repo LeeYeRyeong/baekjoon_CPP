b94011eddcf09137a836c1ca6606c3219b3aef40
ryxxng <i876838@gmail.com>
2025-04-16 18:43:59 +0900

    baekjoon 32776

---
+++ baekjoon32776/baekjoon32776/baekjoon32776.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int s_ab, m_a, f_ab, m_b;
+	cin >> s_ab >> m_a >> f_ab >> m_b;
+	cout << (s_ab <= 240 || (s_ab <= (m_a + f_ab + m_b)) ? "high speed rail" : "flight");
+
+	return 0;
+}
