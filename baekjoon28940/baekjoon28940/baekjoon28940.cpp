e92bb7898bd916f78f8f061d9cb3eb3307a25edc
ryxxng <i876838@gmail.com>
2025-04-17 12:50:36 +0900

    baekjoon 28940

---
+++ baekjoon28940/baekjoon28940/baekjoon28940.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int w, h, a, b, n, count = -1;
+	cin >> w >> h >> n >> a >> b;
+	if (w >= a && h >= b) count = (n % ((w / a) * (h / b)) == 0 ? n / ((w / a) * (h / b)) : n / ((w / a) * (h / b)) + 1);
+	cout << count;
+
+	return 0;
+}
