427231f52b923019b3db684af41ec64864fcf6be
ryxxng <i876838@gmail.com>
2025-04-17 14:07:26 +0900

    baekjoon 29546

---
+++ baekjoon29546/baekjoon29546/baekjoon29546.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string pic[1000] = {};
+	int n, m, a, b;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> pic[i];
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		for (int j = a - 1; j < b; j++) cout << pic[j] << '\n';
+	}
+
+	return 0;
+}
