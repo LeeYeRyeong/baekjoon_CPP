cb8760d817ac147695248f908731235c6910f696
ryxxng <i876838@gmail.com>
2025-04-18 21:58:37 +0900

    baekjoon 28960

---
+++ baekjoon28960/baekjoon28960/baekjoon28960.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double h, l, a, b;
+	cin >> h >> l >> a >> b;
+	cout << ((a / 2 <= h && b <= l) || (b / 2 <= h && a <= l) ? "YES" : "NO");
+
+	return 0;
+}
