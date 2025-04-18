fbda467ece35766cb06efb051da8922f75e753ce
ryxxng <i876838@gmail.com>
2025-04-18 19:59:12 +0900

    baekjoon 21875

---
+++ baekjoon21875/baekjoon21875/baekjoon21875.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string posA, posB;
+	int x, y;
+	cin >> posA >> posB;
+	x = (posA[0] > posB[0] ? static_cast<int>(posA[0] - posB[0]) : static_cast<int>(posB[0] - posA[0]));
+	y = (posA[1] > posB[1] ? static_cast<int>(posA[1] - posB[1]) : static_cast<int>(posB[1] - posA[1]));
+	cout << (x < y ? x : y) << ' ' << (x > y ? x : y);
+
+	return 0;
+}
