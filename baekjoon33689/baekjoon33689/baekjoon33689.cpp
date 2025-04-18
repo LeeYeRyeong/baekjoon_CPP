a81eaa435b27ba3580ad0bb3c4fe8638dd1e56df
ryxxng <i876838@gmail.com>
2025-04-18 13:17:35 +0900

    baekjoon 33689

---
+++ baekjoon33689/baekjoon33689/baekjoon33689.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		if (s[0] == 'C') count++;
+	}
+	cout << count;
+
+	return 0;
+}
