131e86e014c4f26165d1b2cb4ceeb1fafd97b960
ryxxng <i876838@gmail.com>
2025-04-25 21:20:55 +0900

    baekjoon 31134

---
+++ baekjoon31134/baekjoon31134/baekjoon31134.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long x;
+	int t;
+	//scanf_s("%d", &t);
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		//scanf_s("%d", &t);
+		scanf("%ld", &x);
+		printf("%ld\n", x * 2 - 1);
+	}
+
+	return 0;
+}
