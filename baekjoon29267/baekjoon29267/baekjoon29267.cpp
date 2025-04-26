161ba1739e2d914cb639f90228b061aef51526cd
ryxxng <i876838@gmail.com>
2025-04-26 21:57:51 +0900

    baekjoon 29267

---
+++ baekjoon29267/baekjoon29267/baekjoon29267.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string action;
+	int n, k, currentCount = 0, saveCount = 0, resultCount = 0;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> action;
+		if (action == "save") saveCount = currentCount;
+		else if (action == "shoot") currentCount--;
+		else if (action == "ammo") currentCount += k;
+		resultCount = (action == "load" ? saveCount : currentCount);
+		cout << resultCount << '\n';
+		currentCount = resultCount;
+	}
+
+	return 0;
+}
