03844014792aeed1af1c12e0bb23f3ea0096f643
ryxxng <i876838@gmail.com>
2025-04-27 22:43:38 +0900

    baekjoon 17912

---
+++ baekjoon17912/baekjoon17912/baekjoon17912.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, min = 1000000000, trash, index = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> trash;
+		if (trash < min) {
+			min = trash;
+			index = i;
+		}
+	}
+	cout << index;
+
+	return 0;
+}
