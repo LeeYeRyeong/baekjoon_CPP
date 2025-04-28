d571358c0e412f927a5396bf75f692e67e72a086
ryxxng <i876838@gmail.com>
2025-04-28 21:47:27 +0900

    baekjoon 33162

---
+++ baekjoon33162/baekjoon33162/baekjoon33162.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, dis = 0;
+	cin >> x;
+	for (int i = 0; i < x; i++) dis += (i % 2 == 0 ? 3 : -2);
+	cout << dis;
+
+	return 0;
+}
