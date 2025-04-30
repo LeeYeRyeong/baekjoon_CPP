8a3410762d1744497a263f636b11fbfddc9c0abd
ryxxng <i876838@gmail.com>
2025-04-30 19:16:09 +0900

    baekjoon 29097

---
+++ baekjoon29097/baekjoon29097/baekjoon29097.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a, b, c, n, m, k, max = 0;
+	cin >> n >> m >> k >> a >> b >> c;
+	if (n * a > max) max = n * a;
+	if (m * b > max) max = m * b;
+	if (k * c > max) max = k * c;
+	if (max == n * a) cout << "Joffrey ";
+	if (max == m * b) cout << "Robb ";
+	if (max == k * c) cout << "Stannis ";
+	
+	return 0;
+}
