2b5edd91a780213ca7bfcbb8a3adc2d28863a36e
ryxxng <i876838@gmail.com>
2025-04-30 19:15:26 +0900

    baekjoon 28635

---
+++ baekjoon28635/baekjoon28635/baekjoon28635.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, a, b, button = 0;
+	cin >> m >> a >> b;
+	button = (a > b ? m - a + b : b - a);
+	cout << button;
+
+	return 0;
+}
