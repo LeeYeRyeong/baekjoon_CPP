4fad2966a1df33c118e1a7a73f258aed690df32e
ryxxng <i876838@gmail.com>
2025-04-30 19:16:57 +0900

    baekjoon 33163

---
+++ baekjoon33163/baekjoon33163/baekjoon33163.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 'J') cout << 'O';
+		else if (s[i] == 'O') cout << 'I';
+		else if (s[i] == 'I') cout << 'J';
+	}
+
+	return 0;
+}
