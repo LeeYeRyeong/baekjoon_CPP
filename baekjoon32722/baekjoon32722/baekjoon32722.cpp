7b71db841ea1ad289f9c4f438ca2493178d689a1
ryxxng <i876838@gmail.com>
2025-05-01 21:31:49 +0900

    baekjoon 32722

---
+++ baekjoon32722/baekjoon32722/baekjoon32722.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << ((a == 1 || a == 3) && (b == 6 || b == 8) && (c == 2 || c == 5) ? "JAH" : "EI");
+
+	return 0;
+}
