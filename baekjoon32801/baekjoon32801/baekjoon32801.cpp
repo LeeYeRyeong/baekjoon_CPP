f8feca49c8c6c70d73100d52b28809058e8a0297
ryxxng <i876838@gmail.com>
2025-05-01 21:30:03 +0900

    baekjoon 32801

---
+++ baekjoon32801/baekjoon32801/baekjoon32801.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, countF = 0, countB = 0, countFB = 0;
+	cin >> n >> a >> b;
+	for (int i = 1; i <= n; i++) {
+		if (i % a == 0 && i % b == 0) countFB++;
+		else if (i % a == 0) countF++;
+		else if (i % b == 0) countB++;
+	}
+	cout << countF << ' ' << countB << ' ' << countFB;
+
+	return 0;
+}
