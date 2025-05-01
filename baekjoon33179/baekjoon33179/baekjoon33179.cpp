5da8292cfc8cf9b88878a1cc0aa5fdd94436b3c9
ryxxng <i876838@gmail.com>
2025-05-01 17:39:23 +0900

    baekjoon 33179

---
+++ baekjoon33179/baekjoon33179/baekjoon33179.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, paper, total = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> paper;
+		total += (paper % 2 == 1 ? paper / 2 + 1 : paper / 2);
+	}
+	cout << total;
+
+	return 0;
+}
