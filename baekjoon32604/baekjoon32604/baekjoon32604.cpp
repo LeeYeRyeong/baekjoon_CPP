5b683dfdd56f58e0e57a05e826e2d517c022852a
ryxxng <i876838@gmail.com>
2025-05-01 17:31:04 +0900

    baekjoon 32604

---
+++ baekjoon32604/baekjoon32604/baekjoon32604.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int scoreA, scoreB, previousScoreA = 0, previousScoreB = 0, n;
+	bool check = true;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> scoreA >> scoreB;
+		if (scoreA >= previousScoreA && scoreB >= previousScoreB) {
+			previousScoreA = scoreA;
+			previousScoreB = scoreB;
+		}
+		else check = false;
+	}
+	cout << (check ? "yes" : "no");
+	
+	return 0;
+}
