589eae815e7914033ad9ef1525d524089a9ef3a3
ryxxng <i876838@gmail.com>
2025-05-01 21:31:07 +0900

    baekjoon 33191

---
+++ baekjoon33191/baekjoon33191/baekjoon33191.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int money, watermelon, pomegranates, nuts;
+	cin >> money >> watermelon >> pomegranates >> nuts;
+	if (money >= watermelon) cout << "Watermelon";
+	else if (money >= pomegranates) cout << "Pomegranates";
+	else if (money >= nuts) cout << "Nuts";
+	else cout << "Nothing";
+
+	return 0;
+}
