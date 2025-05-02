5e5c50ad79e309de75ac225f60fddd8fa8db7082
ryxxng <i876838@gmail.com>
2025-05-02 14:43:47 +0900

    baekjoon 32905

---
+++ baekjoon32905/baekjoon32905/baekjoon32905.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, checkA = 0;
+	char c;
+	bool check = true;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> c;
+			if (c == 'A') checkA++;
+		}
+		if (checkA != 1) check = false;
+		checkA = 0;
+	}
+	cout << (check ? "Yes" : "No");
+
+	return 0;
+}
