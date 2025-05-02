8bb3a166ed36f8d273cccb50654607ec057c3965
ryxxng <i876838@gmail.com>
2025-05-02 14:37:45 +0900

    baekjoon 33651

---
+++ baekjoon33651/baekjoon33651/baekjoon33651.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s, uapcStr = "UAPC";
+	int uapc[4] = {};
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'U') uapc[0] = 1;
+		else if (s[i] == 'A') uapc[1] = 1;
+		else if (s[i] == 'P') uapc[2] = 1;
+		else if (s[i] == 'C') uapc[3] = 1;
+	}
+	for (int i = 0; i < 4; i++)
+		if (uapc[i] == 0) cout << uapcStr[i];
+
+	return 0;
+}
