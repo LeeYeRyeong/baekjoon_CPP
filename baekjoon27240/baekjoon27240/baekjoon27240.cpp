b72946e4c028d9abe0b86c40054a576471139825
ryxxng <i876838@gmail.com>
2025-05-02 18:52:50 +0900

    baekjoon27240

---
+++ baekjoon27240/baekjoon27240/baekjoon27240.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, s, t, start, end;
+	cin >> n >> a >> b >> s >> t;
+	start = (s < t ? s : t);
+	end = (s > t ? s : t);
+	if (start >= a + 1 && end <= b - 1) cout << "City";
+	else if ((start >= 1 && end <= a) || (start >= b && end <= n)) cout << "Outside";
+	else cout << "Full";
+
+	return 0;
+}
