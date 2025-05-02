97dfa0b2a12a7c84b865c65bb12bc00432ef95fc
ryxxng <i876838@gmail.com>
2025-05-02 18:55:03 +0900

    baekjoon 2884

---
+++ baekjoon bronze3/baekjoon2884/baekjoon2884.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int h, m;
+	cin >> h >> m;
+	if (m < 45) {
+		m += 60;
+		h = (h > 0 ? h - 1 : 23);
+	}
+	cout << h << ' ' << m - 45;
+
+	return 0;
+}
