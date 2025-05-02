13f234a19d872303c324bbc6f1a9e9d8f914cf61
ryxxng <i876838@gmail.com>
2025-05-03 01:32:12 +0900

    baekjoon 10818

---
+++ baekjoon bronze3/baekjoon10818/baekjoon10818.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, min = 1000000, max = -1000000, value;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> value;
+		if (min > value) min = value;
+		if (max < value) max = value;
+	}
+	cout << min << ' ' << max;
+
+	return 0;
+}
