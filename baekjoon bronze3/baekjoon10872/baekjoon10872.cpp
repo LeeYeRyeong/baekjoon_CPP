83141bb5f020095708233cd1d891abf35cf1c436
ryxxng <i876838@gmail.com>
2025-05-03 01:34:22 +0900

    baekjoon 10872

---
+++ baekjoon bronze3/baekjoon10872/baekjoon10872.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, fac = 1;
+	cin >> n;
+	if (n > 1) {
+		for (int i = 1; i <= n; i++) fac *= i;
+	}
+	cout << fac;
+
+	return 0;
+}
