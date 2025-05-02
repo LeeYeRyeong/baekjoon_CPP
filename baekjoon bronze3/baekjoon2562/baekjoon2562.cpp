50520242a1480dcb91792caf5ce683a06c7f5474
ryxxng <i876838@gmail.com>
2025-05-03 01:32:58 +0900

    baekjoon 2562

---
+++ baekjoon bronze3/baekjoon2562/baekjoon2562.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, max = 0, index;
+	for (int i = 0; i < 9; i++) {
+		cin >> n;
+		if (n > max) {
+			max = n;
+			index = i + 1;
+		}
+	}
+	cout << max << '\n' << index;
+
+	return 0;
+}
