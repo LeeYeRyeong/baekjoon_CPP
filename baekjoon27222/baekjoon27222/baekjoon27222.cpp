0eb55e3bb37518d8343aa658bc9f48f9d535321c
ryxxng <i876838@gmail.com>
2025-05-02 14:50:09 +0900

    baekjoon 27222

---
+++ baekjoon27222/baekjoon27222/baekjoon27222.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int check[1000];
+	int n, x, y, increase = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> check[i];
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		if (y > x && check[i] == 1) increase += y - x;
+	}
+	cout << increase;
+
+	return 0;
+}
