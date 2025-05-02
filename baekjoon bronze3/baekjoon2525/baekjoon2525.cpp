440d5c9bb74b35b23e74b3e9c1597a7cdafaaa53
ryxxng <i876838@gmail.com>
2025-05-03 01:33:39 +0900

    baekjoon 2525

---
+++ baekjoon bronze3/baekjoon2525/baekjoon2525.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int currentHour, currentMin, time;
+	cin >> currentHour >> currentMin >> time;
+	currentMin += time;
+	currentHour += currentMin / 60;
+	cout << (currentHour > 23 ? currentHour % 24 : currentHour) << ' ' << currentMin % 60;
+
+	return 0;
+}
