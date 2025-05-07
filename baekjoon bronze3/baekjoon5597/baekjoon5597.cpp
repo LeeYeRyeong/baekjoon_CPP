54fa5ff88bce29fcd4bd75ab0b553e1f925560c6
ryxxng <i876838@gmail.com>
2025-05-07 15:09:11 +0900

    baekjoon 5597

---
+++ baekjoon bronze3/baekjoon5597/baekjoon5597.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int student[30] = {};
+    int n;
+    for (int i = 0; i < 28; i++) {
+        cin >> n;
+        student[n - 1] = 1;
+    }
+    for (int i = 0; i < 30; i++) if (student[i] == 0) cout << i + 1 << '\n';
+
+    return 0;
+}
