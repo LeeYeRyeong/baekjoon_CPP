2199f7e1f05911175115630463d25b5de22c6c1b
ryxxng <i876838@gmail.com>
2025-05-07 19:36:32 +0900

    baekjoon 33810

---
+++ baekjoon33810/baekjoon33810/baekjoon33810.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s, message = "SciComLove";
+	int count = 0;
+	cin >> s;
+	for (int i = 0; i < message.size(); i++) {
+		if (message[i] != s[i])count++;
+	}
+	cout << count;
+
+	return 0;
+}
