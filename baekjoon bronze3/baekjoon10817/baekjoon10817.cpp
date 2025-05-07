493016c5a0ae0633c1ce6949f6eb1661f10bd921
ryxxng <i876838@gmail.com>
2025-05-07 19:38:46 +0900

    baekjoon 10817

---
+++ baekjoon bronze3/baekjoon10817/baekjoon10817.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, mid;
+	cin >> a >> b >> c;
+	if ((a >= b && a <= c) || (a >= c && a <= b)) mid = a;
+	if ((b >= a && b <= c) || (b >= c && b <= a)) mid = b;
+	if ((c >= b && c <= a) || (c >= a && c <= b)) mid = c;
+	cout << mid;
+
+	return 0;
+}
