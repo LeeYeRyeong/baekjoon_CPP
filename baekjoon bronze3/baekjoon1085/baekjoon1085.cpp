3ab309f0d7a2549b9791c520b229d409f6f654f8
ryxxng <i876838@gmail.com>
2025-05-07 15:09:56 +0900

    baekjoon 1085

---
+++ baekjoon bronze3/baekjoon1085/baekjoon1085.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, w, h, min = 1000;
+    cin >> x >> y >> w >> h;
+    if (min > x) min = x;
+    if (min > y) min = y;
+    if (min > (w > x ? w - x : x - w)) min = (w > x ? w - x : x - w);
+    if (min > (h > y ? h - y : y - h)) min = (h > y ? h - y : y - h);
+    cout << min;
+
+    return 0;
+}
