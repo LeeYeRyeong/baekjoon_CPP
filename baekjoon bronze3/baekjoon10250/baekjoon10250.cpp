2fd8b8ba6b4f45c81448ad7755fd727e48e38a95
ryxxng <i876838@gmail.com>
2025-05-07 19:37:40 +0900

    baekjoon 10250

---
+++ baekjoon bronze3/baekjoon10250/baekjoon10250.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, w, h, n, room = 0, count = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> h >> w >> n;
+		for (int j = 1; j <= w; j++) {
+			for (int k = 1; k <= h; k++) {
+				if (n == ++count) room += (k * 100 + j);
+			}
+			if (n <= count) {
+				cout << room << '\n';
+				break;
+			}
+		}
+		room = 0;
+		count = 0;
+	}
+
+	return 0;
+}
