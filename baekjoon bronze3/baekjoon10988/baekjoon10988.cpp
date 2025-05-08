0ab657cdc35eb4fa92f5416e98dcf055c07fac7b
ryxxng <i876838@gmail.com>
2025-05-08 22:45:17 +0900

    baekjoon 10988

---
+++ baekjoon bronze3/baekjoon10988/baekjoon10988.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	bool check = true;
+	cin >> s;
+	for (int i = 0; i < s.size() / 2; i++) {
+		if (s[i] != s[s.size() - 1 - i]) {
+			check = false;
+			break;
+		}
+	}
+	cout << check;
+}
