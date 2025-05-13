46552b16e9864c99dda2ba9a3e325e40a7954335
ryxxng <i876838@gmail.com>
2025-05-13 11:50:20 +0900

    baekjoon 10810

---
+++ baekjoon bronze3/baekjoon10810/baekjoon10810.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int basket[100] = {};
+	int n, m, ball, start, end;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> start >> end >> ball;
+		for (int j = start - 1; j < end; j++) basket[j] = ball;
+	}
+	for (int i = 0; i < n; i++) cout << basket[i] << ' ';
+
+	return 0;
+}
