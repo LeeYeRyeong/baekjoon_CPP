d4a23433056fae6b4968e2710b3a114b9cc62866
ryxxng <i876838@gmail.com>
2025-05-13 11:50:59 +0900

    baekjoon 2738

---
+++ baekjoon bronze3/baekjoon2738/baekjoon2738.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[100][100] = {}, b[100][100] = {}, merge[100][100] = {};
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) cin >> a[i][j];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) cin >> b[i][j];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) merge[i][j] = a[i][j] + b[i][j];
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) cout << merge[i][j] << ' ';
+		cout << '\n';
+	}
+
+	return 0;
+}
