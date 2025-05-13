c79bb8a2b0bfdb485ef6f11cc9cc1258f2d1fd5d
ryxxng <i876838@gmail.com>
2025-05-13 11:51:35 +0900

    baekjoon 4153

---
+++ baekjoon bronze3/baekjoon4153/baekjoon4153.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) break;
+		cout << ((sqrt(pow(a, 2) + pow(b, 2)) == c) || sqrt(pow(c, 2) + pow(b, 2)) == a || sqrt(pow(a, 2) + pow(c, 2)) == b ? "right\n" : "wrong\n");
+	}
+
+	return 0;
+}
