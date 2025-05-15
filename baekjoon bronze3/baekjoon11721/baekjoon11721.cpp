37488c99fa24b90889b3624a5d791b37b8d10bf2
ryxxng <i876838@gmail.com>
2025-05-15 15:40:43 +0900

    baekjoon 11721

---
+++ baekjoon bronze3/baekjoon11721/baekjoon11721.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		cout << s[i];
+		if ((i + 1) % 10 == 0) cout << '\n';
+	}
+
+	return 0;
+}
