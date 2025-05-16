2f83dc52e05941ac01d3b57b2c5ac3ff9b703798
ryxxng <i876838@gmail.com>
2025-05-17 02:10:51 +0900

    baekjoon 2754

---
+++ baekjoon bronze3/baekjoon2754/baekjoon2754.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	char grade[3] = {};
+	double score = 0.0f;
+	cin >> grade;
+	score += (grade[0] != 'F' ? 69 - static_cast<int>(grade[0]) : 0);
+	if (grade[1] == '+') score += 0.3;
+	else if (grade[1] == '-') score -= 0.3;
+	cout << fixed << setprecision(1) << score;
+
+	return 0;
+}
