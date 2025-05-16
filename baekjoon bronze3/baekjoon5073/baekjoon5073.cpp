43cc14991ad6b3aee35bd5b8a7b98d3fa332427f
ryxxng <i876838@gmail.com>
2025-05-17 02:11:25 +0900

    baekjoon 5073

---
+++ baekjoon bronze3/baekjoon5073/baekjoon5073.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	while (1) {
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) break;
+		else if (a == b && b == c) cout << "Equilateral\n";
+		else if ((b + c <= a) || (a + c <= b) || (b + a <= c)) cout << "Invalid\n";
+		else if (a == b || b == c || c == a) cout << "Isosceles\n";
+		else cout << "Scalene\n";
+	}
+
+	return 0;
+}
