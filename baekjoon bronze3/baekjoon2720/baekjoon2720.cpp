c7cae82d2ec8d8063b0cca8906ed74034c3c3ca2
ryxxng <i876838@gmail.com>
2025-05-16 16:29:46 +0900

    baekjoon 2720

---
+++ baekjoon bronze3/baekjoon2720/baekjoon2720.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int money, q = 0, d = 0, n = 0, p = 0, t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> money;
+		q = money / 25;
+		money %= 25;
+		d = money / 10;
+		money %= 10;
+		n = money / 5;
+		money %= 5;
+		p = money;
+		cout << q << ' ' << d << ' ' << n << ' ' << p << '\n';
+		q, d, n, p = 0, 0, 0, 0;
+	}
+
+	return 0;
+}
