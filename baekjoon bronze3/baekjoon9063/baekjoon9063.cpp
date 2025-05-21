047347cfe71678264187f8685854461b078fedc3
ryxxng <i876838@gmail.com>
2025-05-21 22:28:28 +0900

    baekjoon 9063

---
+++ baekjoon bronze3/baekjoon9063/baekjoon9063.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x_min = 10000, x_max = -10000, y_min = 100000, y_max = -10000, t, x, y;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> x >> y;
+		if (x_min > x) x_min = x;
+		if (x_max < x) x_max = x;
+		if (y_min > y) y_min = y;
+		if (y_max < y) y_max = y;
+	}
+	cout << (x_max - x_min) * (y_max - y_min);
+
+	return 0;
+}
