35c3c722715e0e86737be1eea6f714c72fc73131
ryxxng <i876838@gmail.com>
2025-05-21 14:32:52 +0900

    baekjoon 2455

---
+++ baekjoon bronze3/baekjoon2455/baekjoon2455.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int max = 0, trainIn, trainOut, current = 0;
+	for (int i = 0; i < 4; i++) {
+		cin >> trainOut >> trainIn;
+		current += trainIn - trainOut;
+		if (current > max) max = current;
+	}
+	cout << max;
+
+	return 0;
+}
