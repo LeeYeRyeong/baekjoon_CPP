3a507e899eaa71035da12e90a2b6a5dd731045be
ryxxng <i876838@gmail.com>
2025-05-21 14:31:58 +0900

    baekjoon 10162

---
+++ baekjoon bronze3/baekjoon10162/baekjoon10162.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, a = 0, b = 0, c = 0;
+	cin >> t;
+	a = t / 300, t %= 300;
+	b = t / 60, t %= 60;
+	c = t / 10, t %= 10;
+	if (t != 0) cout << -1;
+	else cout << a << ' ' << b << ' ' << c;
+
+	return 0;
+}
