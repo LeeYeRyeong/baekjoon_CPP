9b81acc3678a74bd77f8e32d30aa3ffda5501674
ryxxng <i876838@gmail.com>
2025-05-21 14:31:04 +0900

    baekjoon 3053

---
+++ baekjoon bronze3/baekjoon3053/baekjoon3053.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	const double pi = 3.14159265359;
+	double n, areaU = 0.0, areaT = 0.0;
+	cin >> n;
+	areaU = n * n * pi;
+	areaT = n * n * 2;
+	cout << fixed << setprecision(6) << areaU << '\n' << areaT;
+
+	return 0;
+}
