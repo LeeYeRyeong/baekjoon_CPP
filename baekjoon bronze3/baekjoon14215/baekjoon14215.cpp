341fb8db5bc0a1807d1603aace64dc4edf350157
ryxxng <i876838@gmail.com>
2025-05-22 12:04:12 +0900

    baekjoon 14215

---
+++ baekjoon bronze3/baekjoon14215/baekjoon14215.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, dis = 0, max = 0;
+	cin >> a >> b >> c;
+	dis = a + b + c;
+	if (max < a) max = a;
+	if (max < b) max = b;
+	if (max < c) max = c;
+	cout << (max < dis - max ? dis : 2 * (dis - max) - 1);
+
+	return 0;
+}
