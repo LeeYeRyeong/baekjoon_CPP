54defff47f249c8946b6a086c6078b6b804975e8
ryxxng <i876838@gmail.com>
2025-05-22 12:02:04 +0900

    baekjoon 33964

---
+++ baekjoon33964/baekjoon33964/baekjoon33964.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y, sub, min;
+	cin >> x >> y;
+	min = (x < y ? x : y);
+	sub = (x < y ? y - x : x - y);
+	for (int i = 0; i < sub; i++) cout << 1;
+	for (int i = 0; i < min; i++) cout << 2;
+
+	return 0;
+}
