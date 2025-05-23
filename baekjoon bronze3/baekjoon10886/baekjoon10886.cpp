45184e6760b6c61190b8f60274b43c99edb118f8
ryxxng <i876838@gmail.com>
2025-05-23 13:28:22 +0900

    baekjoon 10886

---
+++ baekjoon bronze3/baekjoon10886/baekjoon10886.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, check = 0;
+	bool cute;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> cute;
+		if (cute == 1) check++;
+	}
+	cout << (check > n / 2 ? "Junhee is cute!" : "Junhee is not cute!");
+
+	return 0;
+}
