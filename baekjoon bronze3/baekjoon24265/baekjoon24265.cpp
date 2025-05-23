82511a3e96949dcf2c6536e9a1812441eb0a01b5
ryxxng <i876838@gmail.com>
2025-05-23 13:29:17 +0900

    baekjoon 24265

---
+++ baekjoon bronze3/baekjoon24265/baekjoon24265.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long count = 0;
+	int n;
+	cin >> n;
+	for (int i = 1; i < n; i++) count += i;
+	cout << count << '\n' << 2;
+
+	return 0;
+}
