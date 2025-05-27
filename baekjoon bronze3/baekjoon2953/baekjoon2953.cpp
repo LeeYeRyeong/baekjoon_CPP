7e6cde134a39a9936e89a769d462db5f11d6b754
ryxxng <i876838@gmail.com>
2025-05-27 20:24:50 +0900

    baekjoon 2953

---
+++ baekjoon bronze3/baekjoon2953/baekjoon2953.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int score[5] = {};
+	int n, max = 0, index;
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 4; j++) {
+			cin >> n;
+			score[i] += n;
+		}
+		if (score[i] > max) max = score[i], index = i + 1;
+	}
+	cout << index << ' ' << max;
+
+	return 0;
+}
