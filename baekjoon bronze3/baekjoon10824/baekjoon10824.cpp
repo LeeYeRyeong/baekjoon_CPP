e0dc0f03f78300b9d17e0f382bc37849a995b4f2
ryxxng <i876838@gmail.com>
2025-05-27 20:26:49 +0900

    baekjoon 10824

---
+++ baekjoon bronze3/baekjoon10824/baekjoon10824.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string a, b, c, d, s1, s2;
+    long long num1, num2;
+    cin >> a >> b >> c >> d;
+    s1 = a + b, s2 = c + d;
+    num1 = stoll(s1), num2 = stoll(s2);
+    cout << num1 + num2;
+
+    return 0;
+}
