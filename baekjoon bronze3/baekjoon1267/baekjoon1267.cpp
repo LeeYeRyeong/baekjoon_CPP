370c89ca90b403f3d1ad293cbf6f88c8855f3887
ryxxng <i876838@gmail.com>
2025-05-27 20:27:57 +0900

    baekjoon 1267

---
+++ baekjoon bronze3/baekjoon1267/baekjoon1267.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, time, yCost = 0, mCost = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> time;
+		yCost += (time / 30 + 1) * 10, mCost += (time / 60 + 1) * 15;
+	}
+	if (yCost == mCost) cout << "Y M " << yCost;
+	else if (yCost > mCost) cout << "M " << mCost;
+	else cout << "Y " << yCost;
+
+	return 0;
+}
