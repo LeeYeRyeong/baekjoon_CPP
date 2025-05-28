d2890578eb9d86a70930e06ba279f736cc8685e1
ryxxng <i876838@gmail.com>
2025-05-28 22:11:38 +0900

    baekjoon 9085

---
+++ baekjoon bronze3/baekjoon9085/baekjoon9085.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, sum = 0, num;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> num;
+			sum += num;
+		}
+		cout << sum << '\n';
+		sum = 0;
+	}
+
+	return 0;
+}
