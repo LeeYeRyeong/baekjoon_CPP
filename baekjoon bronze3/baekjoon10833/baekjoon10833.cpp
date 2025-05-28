caafbac2cf56aa3d96e2401aef6d4f511918dde1
ryxxng <i876838@gmail.com>
2025-05-28 22:09:19 +0900

    baekjoon 10833

---
+++ baekjoon bronze3/baekjoon10833/baekjoon10833.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, student, apple, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> student >> apple;
+		count += (student > apple ? apple : apple % student);
+	}
+	cout << count;
+
+	return 0;
+}
