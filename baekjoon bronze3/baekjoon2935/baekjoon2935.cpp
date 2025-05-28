5cb9b129f9f48838bef1a9afd9e52815affe1285
ryxxng <i876838@gmail.com>
2025-05-28 17:08:27 +0900

    baekjoon 2935

---
+++ baekjoon bronze3/baekjoon2935/baekjoon2935.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1, s2;
+	char c;
+	cin >> s1 >> c >> s2;
+	if (c == '*') {
+		cout << 1;
+		for (int i = 0; i < s1.size() - 1 + s2.size() - 1; i++) cout << 0;
+	}
+	else {
+		if (s1.size() == s2.size()) {
+			cout << 2;
+			for (int i = 0; i < s1.size() - 1; i++) cout << 0;
+		}
+		else {
+			cout << 1;
+			for (int i = 0; i < (s1.size() > s2.size() ? s1.size() - 1 : s2.size() - 1); i++) {
+				if (i == (s1.size() > s2.size() ? s1.size() - s2.size() - 1 : s2.size() - s1.size() - 1)) cout << 1;
+				else cout << 0;
+			}
+		}
+	}
+
+	return 0;
+}
