ff100436a6977299fde5f15b5396e6c1957f632f
ryxxng <i876838@gmail.com>
2025-05-28 22:12:14 +0900

    baekjoon 10102

---
+++ baekjoon bronze3/baekjoon10102/baekjoon10102.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, countA = 0, countB = 0;
+	cin >> n >> s;
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 'A') countA++;
+		else if (s[i] == 'B') countB++;
+	}
+	cout << (countA == countB ? "Tie" : countA > countB ? "A" : "B");
+
+	return 0;
+}
