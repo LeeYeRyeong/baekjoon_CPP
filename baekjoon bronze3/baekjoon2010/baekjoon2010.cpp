a7e45d54f185d5b5aa1e2d6a61fdf826c6272274
ryxxng <i876838@gmail.com>
2025-05-28 17:07:36 +0900

    baekjoon 2010

---
+++ baekjoon bronze3/baekjoon2010/baekjoon2010.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, plug, computer = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> plug;
+		computer += --plug;
+	}
+	cout << ++computer;
+
+	return 0;
+}
