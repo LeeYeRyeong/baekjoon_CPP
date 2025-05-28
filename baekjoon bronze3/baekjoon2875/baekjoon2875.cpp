0a3c5121fb739785328e6eeaeb2997d7902eabf3
ryxxng <i876838@gmail.com>
2025-05-28 22:07:36 +0900

    baekjoon 2875

---
+++ baekjoon bronze3/baekjoon2875/baekjoon2875.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, k, team = 0;
+	cin >> n >> m >> k;
+	while (1) {
+		n -= 2, m -= 1;
+		if (n + m < k || n < 0 || m < 0) break;
+		team++;
+	}
+	cout << team;
+
+	return 0;
+}
