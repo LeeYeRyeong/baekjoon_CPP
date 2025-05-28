4e4bfbdcd0da6552598631a96917dce7e48c2a8c
ryxxng <i876838@gmail.com>
2025-05-28 22:08:32 +0900

    baekjoon 5063

---
+++ baekjoon bronze3/baekjoon5063/baekjoon5063.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, r, c, e;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> r >> e >> c;
+		cout << (r == e - c ? "does not matter\n" : r > e - c ? "do not advertise\n" : "advertise\n");
+	}
+	
+	return 0;
+}
