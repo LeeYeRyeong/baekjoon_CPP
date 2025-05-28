bff93d019e532caa0e208536b35004a84ab5f913
ryxxng <i876838@gmail.com>
2025-05-28 22:13:08 +0900

    baekjoon 10214

---
+++ baekjoon bronze3/baekjoon10214/baekjoon10214.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, y, k, scoreY = 0, scoreK = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		for (int j = 0; j < 9; j++) {
+			cin >> y >> k;
+			scoreY += y, scoreK += k;
+		}
+		cout << (scoreY == scoreK ? "Draw\n" : scoreY > scoreK ? "Yonsei\n" : "Korea\n");
+		scoreY = 0, scoreK = 0;
+	}
+
+	return 0;
+}
