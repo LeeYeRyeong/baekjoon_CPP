af0ca20c4a1d97946d4811c3496feb37bb9e8869
ryxxng <i876838@gmail.com>
2025-05-28 17:13:46 +0900

    baekjoon 1547

---
+++ baekjoon bronze3/baekjoon1547/baekjoon1547.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, b, index = 1;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		if (index == a) index = b;
+		else if (index == b) index = a;
+	}
+	cout << index;
+
+	return 0;
+}
