6ccd9d63e3f7e8f2d3fd6d8ac7d9b1aac811ab1f
ryxxng <i876838@gmail.com>
2025-05-28 22:10:41 +0900

    baekjoon 10103

---
+++ baekjoon bronze3/baekjoon10103/baekjoon10103.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, scoreC = 100, scoreS = 100, c, s;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> c >> s;
+		if (c > s) scoreS -= c;
+		else if (c < s) scoreC -= s;
+	}
+	cout << scoreC << '\n' << scoreS;
+
+	return 0;
+}
