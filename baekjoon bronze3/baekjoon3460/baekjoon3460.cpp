01eb8e550f0964141377a0b843f881391fc522cf
ryxxng <i876838@gmail.com>
2025-05-28 22:09:57 +0900

    baekjoon 3460

---
+++ baekjoon bronze3/baekjoon3460/baekjoon3460.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, index = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		while (n > 0) {
+			if (n % 2 == 1) cout << index << ' ';
+			n /= 2, index++;
+		}
+		cout << '\n';
+		index = 0;
+	}
+
+	return 0;
+}
