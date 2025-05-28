71aa9b9efc823eab58583cb3b09ff0413eb6bac7
ryxxng <i876838@gmail.com>
2025-05-28 17:16:36 +0900

    baekjoon 2506

---
+++ baekjoon bronze3/baekjoon2506/baekjoon2506.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, score = 0, count = 0, answer;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> answer;
+		if (answer == 1) score += ++count;
+		else count = 0;
+	}
+	cout << score;
+
+	return 0;
+}
