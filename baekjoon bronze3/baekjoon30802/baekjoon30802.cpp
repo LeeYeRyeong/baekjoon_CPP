4e459b0beab439b8722febfd2f1132ee8663e63f
ryxxng <i876838@gmail.com>
2025-05-28 17:09:06 +0900

    baekjoon 30802

---
+++ baekjoon bronze3/baekjoon30802/baekjoon30802.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int shirt[6] = {};
+	int n, t, p, resultT = 0, resultPSet = 0, resultPSingle = 0;
+	cin >> n;
+	for (int i = 0; i < 6; i++) cin >> shirt[i];
+	cin >> t >> p;
+	for (int i = 0; i < 6; i++) resultT += (shirt[i] % t == 0 ? shirt[i] / t : shirt[i] / t + 1);
+	resultPSet = n / p, resultPSingle = n % p;
+	cout << resultT << '\n' << resultPSet << ' ' << resultPSingle;
+
+	return 0;
+}
