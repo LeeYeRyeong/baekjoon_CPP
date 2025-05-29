c831b884d4cd6abcec981c8f6219192b0cfbba02
ryxxng <i876838@gmail.com>
2025-05-29 16:16:45 +0900

    baekjoon 3058

---
+++ baekjoon bronze3/baekjoon3058/baekjoon3058.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, min = 100, t, sum = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		for (int j = 0; j < 7; j++) {
+			cin >> n;
+			if (n % 2 == 0) {
+				sum += n;
+				if (min > n) min = n;
+			}
+		}
+		cout << sum << ' ' << min << '\n';
+		sum = 0, min = 100;
+	}
+
+	return 0;
+}
