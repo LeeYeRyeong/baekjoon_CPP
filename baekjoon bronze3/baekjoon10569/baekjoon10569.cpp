664b049b0f9515b74ef23f5b898236cf84aa2340
ryxxng <i876838@gmail.com>
2025-05-29 16:18:20 +0900

    baekjoon 10569

---
+++ baekjoon bronze3/baekjoon10569/baekjoon10569.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, v, e;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> v >> e;
+		cout << 2 + e - v << '\n';
+	}
+
+	return 0;
+}
