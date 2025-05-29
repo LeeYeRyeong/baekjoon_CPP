065cca23b425adbf6ecea2c1f1a91a4b30315d45
ryxxng <i876838@gmail.com>
2025-05-29 16:18:50 +0900

    baekjoon 1284

---
+++ baekjoon bronze3/baekjoon1284/baekjoon1284.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int width = 0;
+	while (1) {
+		cin >> s;
+		if (s == "0") break;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == '0') width += 4;
+			else if (s[i] == '1') width += 2;
+			else width += 3;
+		}
+		cout << width + s.size() + 1 << '\n';
+		width = 0;
+	}
+
+	return 0;
+}
