f0c5a855df4dd133ce3cb89004f8e50ca66ea32f
ryxxng <i876838@gmail.com>
2025-05-29 16:15:04 +0900

    baekjoon 9325

---
+++ baekjoon bronze3/baekjoon9325/baekjoon9325.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, s, n, p, q, sum = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> s >> n;
+		sum += s;
+		for (int j = 0; j < n; j++) {
+			cin >> q >> p;
+			sum += p * q;
+		}
+		cout << sum << '\n';
+		sum = 0;
+	}
+
+	return 0;
+}
