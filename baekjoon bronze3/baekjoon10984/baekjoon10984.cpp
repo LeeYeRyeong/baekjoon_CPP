00e2ee22ab1856c65826f94cc5eafe34086e57f3
ryxxng <i876838@gmail.com>
2025-05-30 16:33:32 +0900

    baekjoon 10984

---
+++ baekjoon bronze3/baekjoon10984/baekjoon10984.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double grade = 0.0, g;
+	int t, n, c, total = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> c >> g;
+			total += c;
+			grade += c * g;
+		}
+		cout << fixed << setprecision(1) << total << ' ' << grade / total << '\n';
+		total = 0, grade = 0.0;
+	}
+
+	return 0;
+}
