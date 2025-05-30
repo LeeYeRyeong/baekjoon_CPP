33730c727f743326625543e57c9b34da1a76a921
ryxxng <i876838@gmail.com>
2025-05-30 16:34:08 +0900

    baekjoon 5988

---
+++ baekjoon bronze3/baekjoon5988/baekjoon5988.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		if ((static_cast<int>(s[s.size() - 1]) - 48) % 2 == 0) cout << "even\n";
+		else cout << "odd\n";
+	}
+
+	return 0;
+}
