2ec2ce6acf166a84699d8646f8ab5351dd6989fa
ryxxng <i876838@gmail.com>
2025-05-30 16:34:38 +0900

    baekjoon 10707

---
+++ baekjoon bronze3/baekjoon10707/baekjoon10707.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, p, x = 0, y = 0;
+	cin >> a >> b >> c >> d >> p;
+	x = a * p;
+	y = (p > c ? b + (p - c) * d : b);
+	cout << (x < y ? x : y);
+
+	return 0;
+}
