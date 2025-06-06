32e9f42a33b63c25320b926b2abb0f710852e9f0
ryxxng <i876838@gmail.com>
2025-06-06 20:47:01 +0900

    baekjoon 3047

---
+++ baekjoon bronze3/baekjoon3047/baekjoon3047.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int order[3] = {};
+	int a, b, c, temp;
+	cin >> a >> b >> c >> s;
+	if (a > b) {
+		temp = a;
+		a = b;
+		b = temp;
+	}
+	if (a > c) {
+		temp = a;
+		a = c;
+		c = temp;
+	}
+	if (b > c) {
+		temp = b;
+		b = c;
+		c = temp;
+	}
+	for (int i = 0; i < 3; i++) {
+		if (s[i] == 'A') order[i] = 1;
+		else if (s[i] == 'B') order[i] = 2;
+		else if (s[i] == 'C') order[i] = 3;
+	}
+	for (int i = 0; i < 3; i++) {
+		if (order[i] == 1) cout << a << ' ';
+		else if (order[i] == 2) cout << b << ' ';
+		else if (order[i] == 3) cout << c << ' ';
+	}
+
+	return 0;
+}
