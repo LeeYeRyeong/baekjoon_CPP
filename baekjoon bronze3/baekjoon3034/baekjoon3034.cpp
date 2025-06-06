b96426ae266a804c13543a629685e8758d461d62
ryxxng <i876838@gmail.com>
2025-06-06 21:23:55 +0900

    baekjoon 3034

---
+++ baekjoon bronze3/baekjoon3034/baekjoon3034.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n, w, h, length, max;
+	cin >> n >> w >> h;
+	max = sqrt(w * w + h * h);
+	for (int i = 0; i < n; i++) {
+		cin >> length;
+		if (max < length) cout << "NE\n";
+		else cout << "DA\n";
+	}
+
+	return 0;
+}
