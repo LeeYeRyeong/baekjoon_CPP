f2b675afd2b91684582747d665ddfe3cc932a1c4
ryxxng <i876838@gmail.com>
2025-06-07 15:07:12 +0900

    baekjoon 10178

---
+++ baekjoon bronze3/baekjoon10178/baekjoon10178.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, c, v, m, d;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> c >> v;
+		m = c / v, d = c % v;
+		cout << "You get " << m << " piece(s) and your dad gets " << d << " piece(s).\n";
+	}
+
+	return 0;
+}
