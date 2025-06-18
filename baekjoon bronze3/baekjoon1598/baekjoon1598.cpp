9787191e026aaf63a471ca60f1cbabd783f45b5a
ryxxng <i876838@gmail.com>
2025-06-18 20:37:33 +0900

    baekjoon 1598

---
+++ baekjoon bronze3/baekjoon1598/baekjoon1598.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, length = 0;
+	cin >> n >> m;
+	length = (((n - 1) / 4) > ((m - 1) / 4) ? ((n - 1) / 4) - ((m - 1) / 4) : ((m - 1) / 4) - ((n - 1) / 4)) +
+		(((n - 1) % 4) > ((m - 1) % 4) ? ((n - 1) % 4) - ((m - 1) % 4) : ((m - 1) % 4) - ((n - 1) % 4));
+	cout << length;
+
+	return 0;
+}
