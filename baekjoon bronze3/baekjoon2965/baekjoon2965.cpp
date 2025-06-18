bba4a64e634fb9cd9f70d3dbf0c3c689e186a84d
ryxxng <i876838@gmail.com>
2025-06-18 20:35:54 +0900

    baekjoon 2965

---
+++ baekjoon bronze3/baekjoon2965/baekjoon2965.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, count = 0;
+	cin >> a >> b >> c;
+	while (!(b - a == 1 && c - b == 1)) {
+		count++;
+		if (b - a < c - b) {
+			a = b;
+			b++;
+		}
+		else {
+			c = b;
+			b--;
+		}
+	}
+	cout << count;
+
+	return 0;
+}
