1b9337c11726bfc2e1e5b5662f37218435e4934e
ryxxng <i876838@gmail.com>
2025-06-18 20:36:27 +0900

    baekjoon 4504

---
+++ baekjoon bronze3/baekjoon4504/baekjoon4504.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	cin >> n;
+	while (1) {
+		cin >> m;
+		if (m == 0) break;
+		cout << m << (m % n == 0 ? " is a multiple of " : " is NOT a multiple of ") << n << ".\n";
+	}
+
+	return 0;
+}
