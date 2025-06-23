32798c976a1e0871decbfdbe12ca9774ec944b85
ryxxng <i876838@gmail.com>
2025-06-23 18:50:28 +0900

    baekjoon 17496

---
+++ baekjoon bronze3/baekjoon17496/baekjoon17496.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, t, c, p, amount = 0;
+	cin >> n >> t >> c >> p;
+	amount = ((n - 1) / t) * c * p;
+	cout << amount;
+
+	return 0;
+}
