932a2ca92fa7850812f454c600846f9f745e2481
ryxxng <i876838@gmail.com>
2025-06-26 19:30:00 +0900

    baekjoon 10179

---
+++ baekjoon bronze3/baekjoon10179/baekjoon10179.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double price, sale;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> price;
+		sale = price * 0.8;
+		cout << '$' << fixed << setprecision(2) << sale << '\n';
+	}
+
+	return 0;
+}
