9f64c45b62dbcf472f85b6ad9a673c3b073b48d5
ryxxng <i876838@gmail.com>
2025-06-26 19:29:36 +0900

    baekjoon 5523

---
+++ baekjoon bronze3/baekjoon5523/baekjoon5523.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, aScore, bScore, a = 0, b = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> aScore >> bScore;
+		if (aScore > bScore) a++;
+		else if (aScore < bScore) b++;
+	}
+	cout << a << ' ' << b;
+
+	return 0;
+}
