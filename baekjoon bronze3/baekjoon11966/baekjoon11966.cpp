4050555468579fc3809852b3c60e54ccc58501d1
ryxxng <i876838@gmail.com>
2025-06-26 19:26:04 +0900

    baekjoon 11966

---
+++ baekjoon bronze3/baekjoon11966/baekjoon11966.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n, result = 0;
+	cin >> n;
+	for (int i = 0; pow(2, i) <= n; i++) {
+		if (pow(2, i) == n) result = 1;
+	}
+	cout << result;
+
+	return 0;
+}
