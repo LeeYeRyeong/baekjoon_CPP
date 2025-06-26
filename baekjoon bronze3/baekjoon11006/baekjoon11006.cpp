7d6e62aebf246dbaadb4302e7d87eb97c7859cdb
ryxxng <i876838@gmail.com>
2025-06-26 19:28:07 +0900

    baekjoon 11006

---
+++ baekjoon bronze3/baekjoon11006/baekjoon11006.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int testCase, n, m, u, t;
+	cin >> testCase;
+	for (int i = 0; i < testCase; i++) {
+		cin >> n >> m;
+		u = (m * 2 - n);
+		t = m - u;
+		cout << u << ' ' << t << '\n';
+	}
+
+	return 0;
+}
