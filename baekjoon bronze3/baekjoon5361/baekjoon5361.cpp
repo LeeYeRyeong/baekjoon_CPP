3fcec10e10e5b251bbd25650aac45e2f45c98613
ryxxng <i876838@gmail.com>
2025-06-26 19:30:28 +0900

    baekjoon 5361

---
+++ baekjoon bronze3/baekjoon5361/baekjoon5361.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double amount = 0;
+	int n, a, b, c, d, e;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b >> c >> d >> e;
+		amount = a * 350.34 + b * 230.90 + c * 190.55 + d * 125.30 + e * 180.90;
+		cout << '$' << fixed << setprecision(2) << amount << '\n';
+		amount = 0;
+	}
+
+	return 0;
+}
