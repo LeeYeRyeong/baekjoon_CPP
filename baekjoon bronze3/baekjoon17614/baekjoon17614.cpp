b491c24855928d5b090cad50db78b1b0b521390a
ryxxng <i876838@gmail.com>
2025-06-26 19:25:27 +0900

    baekjoon 17614

---
+++ baekjoon bronze3/baekjoon17614/baekjoon17614.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, count = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		s = to_string(i);
+		for (int j = 0; j < s.size(); j++) {
+			if (s[j] == '3' || s[j] == '6' || s[j] == '9') count++;
+		}
+	}
+	cout << count;
+
+	return 0;
+}
