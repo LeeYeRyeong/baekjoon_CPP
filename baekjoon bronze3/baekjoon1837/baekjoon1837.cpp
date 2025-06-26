8e077b82500d4a3ea3d4fa18b5098dfc7a1f1559
ryxxng <i876838@gmail.com>
2025-06-26 19:27:40 +0900

    baekjoon 1837

---
+++ baekjoon bronze3/baekjoon1837/baekjoon1837.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int mod_string(const string& p, int mod) {
+    int result = 0;
+    for (char ch : p) {
+        result = (result * 10 + (ch - '0')) % mod;
+    }
+    return result;
+}
+
+int main()
+{
+    string p;
+    int k, r = 0;
+    bool check = true;
+    cin >> p >> k;
+
+    for (int i = 2; i < k; i++) {
+        if (mod_string(p, i) == 0) {
+            r = i;
+            check = false;
+            break;
+        }
+    }
+
+    if (check) cout << "GOOD";
+    else cout << "BAD " << r;
+
+    return 0;
+}
