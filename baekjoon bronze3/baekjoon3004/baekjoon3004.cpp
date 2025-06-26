c88bf0bea2c51992fe542a134edc7700e41233ad
ryxxng <i876838@gmail.com>
2025-06-26 19:27:07 +0900

    baekjoon 3004

---
+++ baekjoon bronze3/baekjoon3004/baekjoon3004.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, chess = 0;
+	cin >> n;
+	if (n % 2 == 0) chess = (n / 2 + 1) * (n / 2 + 1);
+	else chess = (n / 2 + 1) * (n / 2 + 2);
+	cout << chess;
+
+	return 0;
+}
