01d2a7c08b8c3dea6a0a720726621247f68cc06c
ryxxng <i876838@gmail.com>
2025-06-27 15:49:18 +0900

    baekjoon 2783

---
+++ baekjoon bronze3/baekjoon2783/baekjoon2783.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double result, div;
+	int x, y, n, xe, ye;
+	cin >> x >> y >> n;
+	result = x * (1000.00 / y);
+	for (int i = 0; i < n; i++) {
+		cin >> xe >> ye;
+		div = xe * (1000.00 / ye);
+		result = (result > div ? div : result);
+	}
+	cout << fixed << setprecision(2) << result;
+
+	return 0;
+}
