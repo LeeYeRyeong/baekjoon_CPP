53cad245f33c83eee6be5735ac2e2b4ea59248ea
ryxxng <i876838@gmail.com>
2025-07-09 21:15:50 +0900

    baekjoon 12790

---
+++ baekjoon bronze3/baekjoon12790/baekjoon12790.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, hp, mp, attack, defense, hpGauge, mpGauge, attackGauge, defenseGauge, total = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> hp >> mp >> attack >> defense >> hpGauge >> mpGauge >> attackGauge >> defenseGauge;
+		total += (hp + hpGauge < 1 ? 1 : hp + hpGauge) + (mp + mpGauge < 1 ? 1 : mp + mpGauge) * 5 +
+			(attack + attackGauge < 0 ? 0 : attack + attackGauge) * 2 + (defense + defenseGauge) * 2;
+		cout << total << '\n';
+		total = 0;
+	}
+
+	return 0;
+}
