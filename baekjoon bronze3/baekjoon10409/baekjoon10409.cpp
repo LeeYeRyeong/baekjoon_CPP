0295f0a5467a1e2fc1c8dd1ae0d5466dfc32bc64
ryxxng <i876838@gmail.com>
2025-07-09 22:17:13 +0900

    baekjoon 10409

---
+++ baekjoon bronze3/baekjoon10409/baekjoon10409.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, t, wt, work = 0;
+	cin >> n >> t;
+	for (int i = 0; i < n; i++) {
+		cin >> wt;
+		if (t >= wt) {
+			work++;
+			t -= wt;
+		}
+		else t = 0;
+	}
+	cout << work;
+
+	return 0;
+}
