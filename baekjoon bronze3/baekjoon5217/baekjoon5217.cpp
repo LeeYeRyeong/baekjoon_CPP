4732b6e64b780dd9f674083f86dc609453017525
ryxxng <i876838@gmail.com>
2025-07-09 21:17:56 +0900

    baekjoon 5217

---
+++ baekjoon bronze3/baekjoon5217/baekjoon5217.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		cout << "Pairs for " << n << ": ";
+		for (int j = 1; j <= n / 2; j++) {
+			if (j == n - j) continue;
+			if (j > 1) cout << ", ";
+			cout << j << ' ' << n - j;
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
