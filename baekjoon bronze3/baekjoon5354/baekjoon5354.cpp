1d0f9877f21fd16f1bf0b384e99ab9c028ba612b
ryxxng <i876838@gmail.com>
2025-07-09 22:16:03 +0900

    baekjoon 5354

---
+++ baekjoon bronze3/baekjoon5354/baekjoon5354.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				if (j == 0 || j == n - 1 || k == 0 || k == n - 1) cout << '#';
+				else cout << 'J';
+			}
+			cout << '\n';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
