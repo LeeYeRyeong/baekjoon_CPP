a7efc6a3f0066f6bc8a1510133579abeab2f6687
ryxxng <i876838@gmail.com>
2025-08-01 21:31:20 +0900

    baekjoon 11586

---
+++ baekjoon bronze3/baekjoon11586/baekjoon11586.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s[100];
+	int n, state;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> s[i];
+	cin >> state;
+	if (state == 1) {
+		for (int i = 0; i < n; i++) cout << s[i] << '\n';
+	}
+	else if (state == 2) {
+		for (int i = 0; i < n; i++) {
+			for (int j = s[i].size() - 1; j >= 0; j--) cout << s[i][j];
+			cout << '\n';
+		}
+	}
+	else if (state == 3) {
+		for (int i = n - 1; i >= 0; i--) cout << s[i] << '\n';
+	}
+
+	return 0;
+}
