76d2732910b4efb794d203cc7fd72b9ba78f17bc
ryxxng <i876838@gmail.com>
2025-08-01 17:29:09 +0900

    baekjoon 5613

---
+++ baekjoon bronze3/baekjoon5613/baekjoon5613.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int result = 0, n;
+	char c;
+	cin >> n;
+	result = n;
+	while (1) {
+		cin >> c;
+		if (c == '=') break;
+		else {
+			cin >> n;
+			if (c == '+') result += n;
+			else if (c == '-') result -= n;
+			else if (c == '*') result *= n;
+			else if (c == '/') result /= n;
+		}
+	}
+	cout << result;
+
+	return 0;
+}
