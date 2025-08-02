f86a3f83701f852b1a46bda46ecd74b7c2b1f3df
ryxxng <i876838@gmail.com>
2025-08-02 09:57:52 +0900

    baekjoon 20053

---
+++ baekjoon bronze3/baekjoon20053/baekjoon20053.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, min = 1000000, max = -1000000, num;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> num;
+			if (num > max) max = num;
+			if (num < min) min = num;
+		}
+		cout << min << ' ' << max << '\n';
+		min = 1000000, max = -1000000;
+	}
+
+	return 0;
+}
