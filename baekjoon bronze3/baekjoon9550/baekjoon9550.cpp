181c925f4b5b2135a45af0db0ced3cdafc46d8a6
ryxxng <i876838@gmail.com>
2025-08-02 09:57:25 +0900

    baekjoon 9550

---
+++ baekjoon bronze3/baekjoon9550/baekjoon9550.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, k, count = 0, candy;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n >> k;
+		for (int j = 0; j < n; j++) {
+			cin >> candy;
+			count += candy / k;
+		}
+		cout << count << '\n';
+		count = 0;
+	}
+
+	return 0;
+}
