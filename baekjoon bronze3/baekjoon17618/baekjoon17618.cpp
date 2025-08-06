eb0ed97544a8f8746ab1d642519659afbb2590e7
ryxxng <i876838@gmail.com>
2025-08-06 17:53:38 +0900

    baekjoon 17618

---
+++ baekjoon bronze3/baekjoon17618/baekjoon17618.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, sum = 0, count = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		s = to_string(i);
+		for (int j = 0; j < s.size(); j++) sum += static_cast<int>(s[j]) - 48;
+		if (i % sum == 0) count++;
+		sum = 0;
+	}
+	cout << count;
+
+	return 0;
+}
