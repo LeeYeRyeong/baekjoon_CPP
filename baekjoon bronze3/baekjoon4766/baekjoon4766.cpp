f0be3115a3bb71e7cb505e9116a26d66aaf166bb
ryxxng <i876838@gmail.com>
2025-08-06 17:52:59 +0900

    baekjoon 4766

---
+++ baekjoon bronze3/baekjoon4766/baekjoon4766.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double tempPre, tempCur, gap = 0;
+	cin >> tempPre;
+	while (1) {
+		cin >> tempCur;
+		if (tempCur == 999) break;
+		gap = tempCur - tempPre;
+		cout << fixed << setprecision(2) << gap << '\n';
+		tempPre = tempCur;
+	}
+
+	return 0;
+}
