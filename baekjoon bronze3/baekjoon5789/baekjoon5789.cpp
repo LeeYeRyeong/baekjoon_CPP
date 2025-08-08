f90c62c1ef64f28f92908aa526effc8f6ad2d718
ryxxng <i876838@gmail.com>
2025-08-08 17:29:03 +0900

    baekjoon 5789

---
+++ baekjoon bronze3/baekjoon5789/baekjoon5789.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		if (s[s.size() / 2] == s[s.size() / 2 - 1]) cout << "Do-it\n";
+		else cout << "Do-it-Not\n";
+	}
+
+	return 0;
+}
