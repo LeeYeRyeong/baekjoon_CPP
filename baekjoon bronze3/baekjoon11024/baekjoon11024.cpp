76c44ab3c4982321f360bde2ecbdb4f47d8679c8
ryxxng <i876838@gmail.com>
2025-08-08 17:28:36 +0900

    baekjoon11024

---
+++ baekjoon bronze3/baekjoon11024/baekjoon11024.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s, temp = "";
+	int n, sum = 0;
+	cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; i++) {
+		getline(cin, s);
+		for (int j = 0; j < s.size(); j++) {
+			if (j == s.size() - 1) {
+				temp += s[j];
+				sum += stoi(temp);
+			}
+			else if (s[j] == ' ') {
+				sum += stoi(temp);
+				temp = "";
+			}
+			else temp += s[j];
+		}
+		cout << sum << '\n';
+		sum = 0, temp = "";
+	}
+
+	return 0;
+}
