e2c7b535a50a61d37aef7c393461c088203a2d88
ryxxng <i876838@gmail.com>
2025-08-08 17:28:14 +0900

    baekjoon 6378

---
+++ baekjoon bronze3/baekjoon6378/baekjoon6378.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int sum = 0;
+	while (1) {
+		cin >> s;
+		if (s == "0") break;
+		while (s.size() > 1) {
+			for (int i = 0; i < s.size(); i++) {
+				sum += s[i] - 48;
+			}
+			s = to_string(sum);
+			sum = 0;
+		}
+		cout << s << '\n';
+	}
+
+	return 0;
+}
