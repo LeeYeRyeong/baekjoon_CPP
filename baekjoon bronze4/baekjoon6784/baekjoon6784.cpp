ab735b8f5bf112a749185d05efbe006bbc1f1186
ryxxng <i876838@gmail.com>
2025-08-20 22:00:31 +0900

    baekjoon 6784

---
+++ baekjoon bronze4/baekjoon6784/baekjoon6784.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count = 0;
+	char ch[10000], c;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> ch[i];
+	for (int i = 0; i < n; i++) {
+		cin >> c;
+		if (c == ch[i]) count++;
+	}
+	cout << count;
+
+	return 0;
+}
