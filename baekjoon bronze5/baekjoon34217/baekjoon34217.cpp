92943d6169bcab7969fde7329a669af96959fe49
ryxxng <i876838@gmail.com>
2025-09-10 17:33:28 +0900

    baekjoon 34217

---
+++ baekjoon bronze5/baekjoon34217/baekjoon34217.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << (a + c == b + d ? "Either" : a + c < b + d ? "Hanyang Univ." : "Yongdap");
+
+	return 0;
+}
