05108fbe3546f3d9c161e73d6a8202e7b8aebb6e
ryxxng <i876838@gmail.com>
2025-09-10 17:34:18 +0900

    baekjoon 9306

---
+++ baekjoon bronze4/baekjoon9306/baekjoon9306.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name, title;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> name >> title;
+		cout << "Case " << i + 1 << ": " << title << ", " << name << '\n';
+	}
+
+	return 0;
+}
