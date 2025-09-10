455c2307033b1a41c75130c6b48869b11efc6aa9
ryxxng <i876838@gmail.com>
2025-09-10 17:35:52 +0900

    baekjoon 22279

---
+++ baekjoon bronze4/baekjoon22279/baekjoon22279.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double sum = 0, q, age;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> q >> age;
+		sum += (q * age);
+	}
+	cout << fixed << setprecision(3) << sum;
+
+	return 0;
+}
