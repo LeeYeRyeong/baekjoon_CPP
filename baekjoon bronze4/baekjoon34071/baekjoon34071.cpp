228a01ad5cb0e1bded0a4896754bddc1f844b3bd
ryxxng <i876838@gmail.com>
2025-09-10 17:36:33 +0900

    baekjoon 34071

---
+++ baekjoon bronze4/baekjoon34071/baekjoon34071.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, hardIndex, easyIndex, min = 1001, max = 0, level;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> level;
+		if (max < level) {
+			max = level;
+			hardIndex = i;
+		}
+		if (min > level) {
+			min = level;
+			easyIndex = i;
+		}
+	}
+	cout << (easyIndex == 0 ? "ez" : hardIndex == 0 ? "hard" : "?");
+
+	return 0;
+}
