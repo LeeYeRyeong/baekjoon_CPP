75f91e20e0dd6c4b1ade03c97e1711461b7c46f3
ryxxng <i876838@gmail.com>
2025-09-10 17:37:16 +0900

    baekjoon 34161

---
+++ baekjoon bronze4/baekjoon34161/baekjoon34161.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s >> s >> s;
+	for (int i = 1; i <= 10000; i++) {
+		cout << -1 << '\n';
+	}
+
+	return 0;
+}
