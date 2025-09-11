37b175f2b9d44f082d1efb4b691f183d94b5f775
ryxxng <i876838@gmail.com>
2025-09-11 15:12:33 +0900

    baekjoon 3449

---
+++ baekjoon bronze3/baekjoon3449/baekjoon3449.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s1, s2;
+	int n, hamming = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s1 >> s2;
+		for (int j = 0; j < s1.size(); j++) {
+			if (s1[j] != s2[j]) hamming++;
+		}
+		cout << "Hamming distance is " << hamming << ".\n";
+		hamming = 0;
+	}
+	
+	return 0;
+}
