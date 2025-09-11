b2e046a48124d6adabb868f7182eda6fbf20cbcb
ryxxng <i876838@gmail.com>
2025-09-11 15:10:14 +0900

    baekjoon 6321

---
+++ baekjoon bronze3/baekjoon6321/baekjoon6321.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		cout << "String #" << i + 1 << '\n';
+		for (int j = 0; j < s.size(); j++) {
+			cout << (s[j] == 'Z' ? 'A' : static_cast<char>(s[j] + 1));
+		}
+		cout << "\n\n";
+	}
+
+	return 0;
+}
