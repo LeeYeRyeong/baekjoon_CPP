18ad523e8d72941b403c06d16e91a95cbc54573d
ryxxng <i876838@gmail.com>
2025-09-11 15:11:32 +0900

    baekjoon 10474

---
+++ baekjoon bronze3/baekjoon10474/baekjoon10474.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, div, res;
+	while (1) {
+		cin >> n >> m;
+		if (n == 0 && m == 0) break;
+		res = n / m, div = n % m;
+		cout << res << ' ' << div << " / " << m << '\n';
+	}
+
+	return 0;
+}
