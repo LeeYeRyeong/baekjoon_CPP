d8447115925b4303257797428efdd553c8934f67
ryxxng <i876838@gmail.com>
2025-09-11 15:10:47 +0900

    baekjoon 14920

---
+++ baekjoon bronze3/baekjoon14920/baekjoon14920.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n = 1, c;
+	cin >> c;
+	while (c != 1) {
+		c = (c % 2 == 0 ? c / 2 : 3 * c + 1);
+		n++;
+	}
+	cout << n;
+
+	return 0;
+}
