91eace288d759538697a93c5073085ca94e10308
ryxxng <i876838@gmail.com>
2025-09-11 15:12:07 +0900

    baekjoon 10419

---
+++ baekjoon bronze3/baekjoon10419/baekjoon10419.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, d, n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> d;
+		for (int j = 0; j * j + j <= d; j++) t = j;
+		cout << t << '\n';
+	}
+	return 0;
+}
