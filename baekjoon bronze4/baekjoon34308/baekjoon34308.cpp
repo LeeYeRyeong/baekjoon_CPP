36925c205bb72fec5dd659d4c857f6c12ab7831c
ryxxng <i876838@gmail.com>
2025-10-01 16:26:52 +0900

    baekjoon 34308

---
+++ baekjoon bronze4/baekjoon34308/baekjoon34308.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, apple;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> apple;
+		cout << (n - apple < apple - 1 ? n : 1) << ' ';
+	}
+
+	return 0;
+}
