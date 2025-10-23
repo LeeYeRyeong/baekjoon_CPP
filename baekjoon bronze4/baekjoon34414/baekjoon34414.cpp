a15a2131cf9cb164ee6f3aedab8e3793cd3bb67d
ryxxng <i876838@gmail.com>
2025-10-23 15:58:24 +0900

    baekjoon 34414

---
+++ baekjoon bronze4/baekjoon34414/baekjoon34414.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, inch;
+	bool check = true;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> inch;
+		if (inch < 48) check = false;
+	}
+	cout << (check ? "True" : "False");
+
+	return 0;
+}
