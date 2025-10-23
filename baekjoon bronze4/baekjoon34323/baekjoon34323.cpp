72ed521aa41ab6706c38dc26491fc4e90f21ad93
ryxxng <i876838@gmail.com>
2025-10-23 15:54:46 +0900

    baekjoon 34323

---
+++ baekjoon bronze4/baekjoon34323/baekjoon34323.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long n, m, s, nPrice, mPrice;
+	cin >> n >> m >> s;
+	nPrice = (s * (100 - n) * (m + 1)) / 100;
+	mPrice = s * m;
+	cout << (nPrice < mPrice ? nPrice : mPrice);
+
+	return 0;
+}
