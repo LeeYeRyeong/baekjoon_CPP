758b5ea0d6f0d2a2300f8aab39faaf8717158360
ryxxng <i876838@gmail.com>
2025-10-23 15:56:45 +0900

    baekjoon 34363

---
+++ baekjoon bronze4/baekjoon34363/baekjoon34363.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	static const int feet = 5280;
+	double s, d, t;
+	cin >> s >> d >> t;
+	cout << ((s * feet / 3600.0) * t < d ? "FAILED TEST" : "MADE IT");
+
+	return 0;
+}
