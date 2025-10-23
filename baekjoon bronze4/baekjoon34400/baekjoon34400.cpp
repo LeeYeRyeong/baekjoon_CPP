d057c4b69d955cdb9ff9e6db50b9d7fabb8e760e
ryxxng <i876838@gmail.com>
2025-10-23 15:57:49 +0900

    baekjoon 34400

---
+++ baekjoon bronze4/baekjoon34400/baekjoon34400.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, t;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> t;
+		cout << (t % 25 < 17 ? "ONLINE\n" : "OFFLINE\n");
+	}
+
+	return 0;
+}
