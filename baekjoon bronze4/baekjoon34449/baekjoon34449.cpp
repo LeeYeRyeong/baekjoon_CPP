9de0f291c0229bf632ef27d0d1ca0c592ffcae8c
ryxxng <i876838@gmail.com>
2025-11-01 23:47:55 +0900

    baekjoon 34449

---
+++ baekjoon bronze4/baekjoon34449/baekjoon34449.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	static const double pi = 3.14159;
+	double d, w;
+	int n;
+	cin >> d >> w >> n;
+	cout << (d * pi >= w * n ? "YES" : "NO");
+
+	return 0;
+}
