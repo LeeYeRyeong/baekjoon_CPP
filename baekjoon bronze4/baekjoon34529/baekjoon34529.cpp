32f67a879fb82ad8e3d88560c133fa928ada167d
ryxxng <i876838@gmail.com>
2025-11-06 22:23:42 +0900

    baekjoon 34529

---
+++ baekjoon bronze4/baekjoon34529/baekjoon34529.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, y, z, u, v, w, total = 0;
+	cin >> x >> y >> z >> u >> v >> w;
+	total += x * (u / 100) + y * (v / 50) + z * (w / 20);
+	cout << total;
+
+	return 0;
+}
