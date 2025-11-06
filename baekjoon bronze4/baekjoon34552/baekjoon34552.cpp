a122e3a584602ab544bc16b1c49c1d22a6e62b57
ryxxng <i876838@gmail.com>
2025-11-06 22:25:02 +0900

    baekjoon 34552

---
+++ baekjoon bronze4/baekjoon34552/baekjoon34552.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int money[11] = {};
+	double l;
+	int n, b, s, total = 0;
+	for (int i = 0; i < 11; i++) cin >> money[i];
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> b >> l >> s;
+		if (l >= 2.0 && s >= 17) total += money[b];
+	}
+	cout << total;
+
+	return 0;
+}
