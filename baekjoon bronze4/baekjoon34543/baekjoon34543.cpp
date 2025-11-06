afb0549d89924990100a18ff7c19e87ea724128d
ryxxng <i876838@gmail.com>
2025-11-06 22:24:22 +0900

    baekjoon 345443

---
+++ baekjoon bronze4/baekjoon34543/baekjoon34543.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, walk, score = 0;
+	cin >> n >> walk;
+	score += (n * 10) + (n >= 3 ? 20 : 0) + (n == 5 ? 50 : 0) - (walk > 1000 ? 15 : 0);
+	cout << (score < 0 ? 0 : score);
+
+	return 0;
+}
