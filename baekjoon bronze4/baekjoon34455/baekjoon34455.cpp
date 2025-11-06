e019dffd75ae7edfbeea51cf820f8592a8b6894b
ryxxng <i876838@gmail.com>
2025-11-06 22:22:56 +0900

    baekjoon 34455

---
+++ baekjoon bronze4/baekjoon34455/baekjoon34455.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int d, e, q;
+	char c;
+	cin >> d >> e;
+	for (int i = 0; i < e; i++) {
+		cin >> c >> q;
+		d += (c == '+' ? q : -q);
+	}
+	cout << d;
+
+	return 0;
+}
