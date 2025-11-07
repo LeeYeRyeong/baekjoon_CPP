e3df470ad0294d224fe081b365b1380dface5d30
ryxxng <i876838@gmail.com>
2025-11-07 20:15:38 +0900

    baekjoon 24724

---
+++ baekjoon bronze4/baekjoon24724/baekjoon24724.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, a, b, u, v;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n >> a >> b;
+		for (int j = 0; j < n; j++) cin >> u >> v;
+		cout << "Material Management " << i + 1 << "\nClassification ---- End!\n";
+	}
+
+	return 0;
+}
