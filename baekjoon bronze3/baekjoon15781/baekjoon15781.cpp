19b14fa8d9b1e0af435ccd2115a1d77aaabdec9c
ryxxng <i876838@gmail.com>
2025-11-08 01:00:52 +0900

    baekjoon 15781

---
+++ baekjoon bronze3/baekjoon15781/baekjoon15781.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, h, a, maxH = 0, maxA = 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> h;
+		maxH = (maxH < h ? h : maxH);
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> a;
+		maxA = (maxA < a ? a : maxA);
+	}
+	cout << maxH + maxA;
+
+	return 0;
+}
