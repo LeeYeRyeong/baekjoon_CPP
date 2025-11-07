6f9fab104d3653ea3e1250f4ab62f4c639862348
ryxxng <i876838@gmail.com>
2025-11-08 01:00:20 +0900

    baekjoon 11134

---
+++ baekjoon bronze3/baekjoon11134/baekjoon11134.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, c, n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n >> c;
+		cout << (n % c == 0 ? n / c : n / c + 1) << '\n';
+	}
+
+	return 0;
+}
