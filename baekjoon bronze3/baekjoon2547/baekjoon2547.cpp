198861002352654a90b57e43af4b1ab4a61385af
ryxxng <i876838@gmail.com>
2025-11-08 01:01:30 +0900

    baekjoon 2547

---
+++ baekjoon bronze3/baekjoon2547/baekjoon2547.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long sum = 0, candy;
+	int t, n;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> candy;
+			sum = (sum + candy) % n;
+		}
+		cout << (sum % n == 0 ? "YES\n" : "NO\n");
+		sum = 0;
+	}
+
+	return 0;
+}
