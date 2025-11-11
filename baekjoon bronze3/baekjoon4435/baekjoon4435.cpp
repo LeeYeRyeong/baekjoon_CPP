1c5fd586c70f226e4bc99c3ac6439c735e644624
ryxxng <i876838@gmail.com>
2025-11-11 22:52:13 +0900

    baekjoon 4435

---
+++ baekjoon bronze3/baekjoon4435/baekjoon4435.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int gandalf[6] = { 1,2,3,3,4,10 }, sauron[7] = { 1,2,2,2,3,5,10 };
+	int n, gandalfScore = 0, sauronScore = 0, g, s;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 6; j++) {
+			cin >> g;
+			gandalfScore += gandalf[j] * g;
+		}
+		for (int j = 0; j < 7; j++) {
+			cin >> s;
+			sauronScore += sauron[j] * s;
+		}
+		cout << "Battle " << i+1 << ": " << (gandalfScore == sauronScore ? "No victor on this battle field\n" :
+			(gandalfScore > sauronScore ? "Good triumphs over Evil\n" : "Evil eradicates all trace of Good\n"));
+		gandalfScore =0,sauronScore=0;
+	}
+
+	return 0;
+}
