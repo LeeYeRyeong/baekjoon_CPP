79b12cc8c66ed1a998166df64fee8d7f2e8c6ebc
ryxxng <i876838@gmail.com>
2025-11-11 22:51:44 +0900

    baekjoon 14568

---
+++ baekjoon bronze3/baekjoon14568/baekjoon14568.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, count = 0;
+	cin >> n;
+	for (int i = 2; i <= n; i += 2) {
+		for (int j = 1; j <= n - i; j++) {
+			int c = n - i - j;
+			if (c < j + 2) break;
+			count++;
+		}
+	}
+	cout << count;
+
+	return 0;
+}
