3dad7b2312c9571c973ce594c9e0ee4c9eb58537
ryxxng <i876838@gmail.com>
2025-11-12 19:40:03 +0900

    baekjoon 9517

---
+++ baekjoon bronze3/baekjoon9517/baekjoon9517.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int start, n, time, sum = 0, result = 0;
+	char c;
+	cin >> start >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> time >> c;
+		sum += time;
+		if (sum >= 210 && result == 0) result = start;
+		if (c == 'T') start = (start == 8 ? 1 : ++start);
+	}
+	result = (result == 0 ? start : result);
+	cout << result;
+
+	return 0;
+}
