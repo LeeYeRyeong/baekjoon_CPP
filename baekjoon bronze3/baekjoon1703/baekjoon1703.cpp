976aa05cdcc081b27e57dd76b80e3ef54f1a905d
ryxxng <i876838@gmail.com>
2025-11-12 19:39:07 +0900

    baekjoon 1703

---
+++ baekjoon bronze3/baekjoon1703/baekjoon1703.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, p, m, branch = 1;
+	while (1) {
+		cin >> a;
+		if (a == 0) break;
+		for (int i = 0; i < a; i++) {
+			cin >> p >> m;
+			branch = branch * p - m;
+		}
+		cout << branch << '\n';
+		branch = 1;
+	}
+
+	return 0;
+}
