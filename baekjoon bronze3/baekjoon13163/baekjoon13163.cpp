0bd5456e9605ecf2f3c820f103e48146b2419871
ryxxng <i876838@gmail.com>
2025-11-12 19:39:31 +0900

    baekjoon 13163

---
+++ baekjoon bronze3/baekjoon13163/baekjoon13163.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s, result = "";
+	int n, index;
+	cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; i++) {
+		getline(cin, s);
+		for (int j = 0; s[j] != ' '; j++) index = j + 2;
+		for (int j = index; j < s.size(); j++) {
+			if (s[j] != ' ') result += s[j];
+		}
+		cout << "god" << result << '\n';
+		result = "";
+	}
+
+	return 0;
+}
