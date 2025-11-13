29ae4d4d987c2880fcc051a9d6487b8a47ab4290
ryxxng <i876838@gmail.com>
2025-11-13 15:46:32 +0900

    baekjoon 20361

---
+++ baekjoon bronze3/baekjoon20361/baekjoon20361.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, x, k, a, b;
+	cin >> n >> x >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> a >> b;
+		if (a == x) x = b;
+		else if (b == x) x = a;
+	}
+	cout << x;
+
+	return 0;
+}
