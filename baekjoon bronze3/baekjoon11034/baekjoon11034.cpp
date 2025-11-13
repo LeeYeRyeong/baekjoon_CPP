0e1ed4e7896195d9242d6cf683147d53450291b2
ryxxng <i876838@gmail.com>
2025-11-13 15:45:53 +0900

    baekjoon 11034

---
+++ baekjoon bronze3/baekjoon11034/baekjoon11034.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, count = 0;
+	while (cin >> a >> b >> c) {
+		cout << (b - a > c - b ? b - a - 1 : c - b - 1) << '\n';
+	}
+
+	return 0;
+}
