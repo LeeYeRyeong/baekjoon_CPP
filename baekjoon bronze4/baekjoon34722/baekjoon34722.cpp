25a824e1bea8c3c99564c6bdbb861cb3505dcfc9
ryxxng <i876838@gmail.com>
2025-11-14 23:26:09 +0900

    baekjoon 34722

---
+++ baekjoon bronze4/baekjoon34722/baekjoon34722.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, b, c, a, icpc, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> b >> c >> a >> icpc;
+		if (b >= 1000 || c >= 1600 || a >= 1500 || (icpc >= 1 && icpc <= 30)) count++;
+	}
+	cout << count;
+
+	return 0;
+}
