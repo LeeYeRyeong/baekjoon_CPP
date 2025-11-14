392fb0f306f78e437b1035490cfbca8e39e4cea2
ryxxng <i876838@gmail.com>
2025-11-15 00:10:26 +0900

    baekjoon 5612

---
+++ baekjoon bronze3/baekjoon5612/baekjoon5612.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, in, out, current, result;
+	bool check = true;
+	cin >> n >> m;
+	current = m, result = m;
+	for (int i = 0; i < n; i++) {
+		cin >> in >> out;
+		current = current + in - out;
+		if (current > result) result = current;
+		else if (current < 0) check = false;
+	}
+	cout << (check ? result : 0);
+
+	return 0;
+}
