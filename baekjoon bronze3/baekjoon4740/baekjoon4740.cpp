ba04e442b8fde8bc8d1007d28ad8741e89bb3069
ryxxng <i876838@gmail.com>
2025-11-15 00:11:14 +0900

    baekjoon 4740

---
+++ baekjoon bronze3/baekjoon4740/baekjoon4740.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	while (1) {
+		getline(cin, s);
+		if (s == "***") break;
+		for (int i = s.size() - 1; i >= 0; i--) cout << s[i];
+		cout << '\n';
+	}
+
+	return 0;
+}
