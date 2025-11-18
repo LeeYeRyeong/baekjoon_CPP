e8b7b7fa72bf941a033c7820ef62bf63a04eaa8a
ryxxng <i876838@gmail.com>
2025-11-18 23:02:37 +0900

    baekjoon 34691

---
+++ baekjoon bronze5/baekjoon34691/baekjoon34691.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	while (1) {
+		cin >> s;
+		if (s == "end") break;
+		if (s == "animal") cout << "Panthera tigris\n";
+		else if (s == "tree") cout << "Pinus densiflora\n";
+		else if (s == "flower") cout << "Forsythia koreana\n";
+	}
+
+	return 0;
+}
