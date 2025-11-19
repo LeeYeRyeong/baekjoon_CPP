9d02ad619b1e49617c867d880614d31d00c0404e
ryxxng <i876838@gmail.com>
2025-11-19 20:25:08 +0900

    baekjoon 14264

---
+++ baekjoon bronze3/baekjoon14264/baekjoon14264.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double s, a, b;
+	long long l;
+	cin >> l;
+	b = sqrt(((l * 2) * (l * 2)) - (l * l));
+	a = sqrt((l * l) - (b / 2) * (b / 2));
+	s = b * a * 1 / 2;
+	cout << fixed << setprecision(15) << s;
+
+	return 0;
+}
