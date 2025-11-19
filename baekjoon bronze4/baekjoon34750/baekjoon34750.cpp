56e483c7be3a08b3c950d21dd91248c8d3456cec
ryxxng <i876838@gmail.com>
2025-11-19 20:22:53 +0900

    baekjoon 34750

---
+++ baekjoon bronze4/baekjoon34750/baekjoon34750.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, give;
+	cin >> n;
+	if (n >= 1000000) give = n * 0.2;
+	else if (n >= 500000) give = n * 0.15;
+	else if (n >= 100000) give = n * 0.1;
+	else give = n * 0.05;
+	cout << give << ' ' << n - give;
+
+	return 0;
+}
