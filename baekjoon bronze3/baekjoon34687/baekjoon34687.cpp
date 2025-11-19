32a6469c9373c237805942d645712f20279f5b9b
ryxxng <i876838@gmail.com>
2025-11-19 20:24:38 +0900

    baekjoon 34687

---
+++ baekjoon bronze3/baekjoon34687/baekjoon34687.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double water;
+	int n, m;
+	cin >> n >> m;
+	water = static_cast<double>(m) / static_cast<double>(n);
+	cout << (water >= 0.81 ? "yaho" : "no");
+
+	return 0;
+}
