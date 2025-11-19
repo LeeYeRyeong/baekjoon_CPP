4407fa4398828b89303aac6933a2bbe6610d5e3d
ryxxng <i876838@gmail.com>
2025-11-19 20:24:00 +0900

    baekjoon 34798

---
+++ baekjoon bronze4/baekjoon34798/baekjoon34798.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int firstHour, firstMin, secondHour, secondMin;
+	char c;
+	cin >> firstHour >> c >> firstMin >> secondHour >> c >> secondMin;
+	if (firstHour > secondHour) cout << "NO";
+	else if (firstHour < secondHour) cout << "YES";
+	else if (firstMin < secondMin) cout << "YES";
+	else cout << "NO";
+
+	return 0;
+}
