42c62fff42a13fa3cae3cb1d2be6a1c26eb81dcb
ryxxng <i876838@gmail.com>
2025-11-20 22:07:25 +0900

    baekjoon 12756

---
+++ baekjoon bronze3/baekjoon12756/baekjoon12756.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int  attackA, attackB, lifeA, lifeB;
+	cin >> attackA >> lifeA >> attackB >> lifeB;
+	while (lifeA > 0 && lifeB > 0) {
+		lifeA -= attackB;
+		lifeB -= attackA;
+	}
+	cout << ((lifeA <= 0 && lifeB <= 0) ? "DRAW" : lifeA > lifeB ? "PLAYER A" : "PLAYER B");
+
+	return 0;
+}
