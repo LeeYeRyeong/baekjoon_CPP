6af9814b4296bb407b52135732da0880a17484f4
ryxxng <i876838@gmail.com>
2025-11-25 15:22:21 +0900

    baekjoon 5691

---
+++ baekjoon bronze3/baekjoon5691/baekjoon5691.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	while (1) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) break;
+		c = (3 * a) - (a + b);
+		cout << c << '\n';
+	}
+
+	return 0;
+}
