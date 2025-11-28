9798673cf6e68a7b58970b5b76b9f901c8720203
ryxxng <i876838@gmail.com>
2025-11-28 22:41:46 +0900

    baekjoon 9501

---
+++ baekjoon bronze3/baekjoon9501/baekjoon9501.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double g;
+	int t, n, d, v, f, c, count = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n >> d;
+		for (int j = 0; j < n; j++) {
+			cin >> v >> f >> c;
+			g = v * (f * 1.0 / c);
+			if(g >= d) count++;
+		}
+		cout << count << '\n';
+		count = 0;
+	}
+
+	return 0;
+}
