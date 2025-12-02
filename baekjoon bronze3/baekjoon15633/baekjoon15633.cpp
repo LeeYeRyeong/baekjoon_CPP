426b1ee7828738e4b474d4bb996de4a2ec0b8f55
ryxxng <i876838@gmail.com>
2025-12-02 12:55:42 +0900

    baekjoon 15633

---
+++ baekjoon bronze3/baekjoon15633/baekjoon15633.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long sum = 1;
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		if (n % i == 0) sum += i;
+	}
+	sum = sum * 5 - 24;
+	cout << sum;
+
+	return 0;
+}
