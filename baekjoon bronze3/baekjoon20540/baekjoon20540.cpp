b9521f5a23d1b31760b7e0d868d1e376b38068d3
ryxxng <i876838@gmail.com>
2025-12-02 13:00:09 +0900

    baekjoon 20540

---
+++ baekjoon bronze3/baekjoon20540/baekjoon20540.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char mbti[4], mbtiReverse[4];
+	for (int i = 0; i < 4; i++) cin >> mbti[i];
+	if (mbti[0] == 'E') mbtiReverse[0] = 'I';
+	else mbtiReverse[0] = 'E';
+	if (mbti[1] == 'S') mbtiReverse[1] = 'N';
+	else mbtiReverse[1] = 'S';
+	if (mbti[2] == 'T') mbtiReverse[2] = 'F';
+	else mbtiReverse[2] = 'T';
+	if (mbti[3] == 'J') mbtiReverse[3] = 'P';
+	else mbtiReverse[3] = 'J';
+	for (int i = 0; i < 4; i++) cout << mbtiReverse[i];
+
+	return 0;
+}
