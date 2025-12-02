1fc36c7834903a685afa0db05d38f929728b6276
ryxxng <i876838@gmail.com>
2025-12-02 12:56:36 +0900

    baekjoon 15780

---
+++ baekjoon bronze3/baekjoon15780/baekjoon15780.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, k, a, sum = 0;
+	cin >> n >> k;
+	for (int i = 0; i < k; i++) {
+		cin >> a;
+		sum += (a % 2 == 0 ? a / 2 : a / 2 + 1);
+	}
+	cout << (sum < n ? "NO" : "YES");
+
+	return 0;
+}
