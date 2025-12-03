2afb5b4e618864d18ab55806f2748c4c5dc0e032
ryxxng <i876838@gmail.com>
2025-12-03 16:52:48 +0900

    baekjoon 5692

---
+++ baekjoon bronze3/baekjoon5692/baekjoon5692.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int result = 0, mul = 1;
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	while (1) {
+		cin >> s;
+		if (s == "0") break;
+		for (int i = s.size(); i > 0; i--) mul *= i;
+		for (int i = s.size(); i > 0; i--) {
+			result += mul * (static_cast<int>(s[s.size() - i]) - '0');
+			mul /= i;
+		}
+		cout << result << '\n';
+		result = 0;
+	}
+
+	return 0;
+}
