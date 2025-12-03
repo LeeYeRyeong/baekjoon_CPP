790eda2a7e9037427bc15a067c5926fa7ae0ee58
ryxxng <i876838@gmail.com>
2025-12-03 16:51:46 +0900

    baekjoon 34824

---
+++ baekjoon bronze4/baekjoon34824/baekjoon34824.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	int n, rankY, rankK;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> name;
+		if (name == "yonsei") rankY = i;
+		else if (name == "korea") rankK = i;
+	}
+	cout << (rankY < rankK ? "Yonsei Won!" : "Yonsei Lost...");
+
+	return 0;
+}
