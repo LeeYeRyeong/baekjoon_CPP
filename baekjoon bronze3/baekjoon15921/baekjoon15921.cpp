a13008c9512c1af73c27978004e43730c3dc7ede
ryxxng <i876838@gmail.com>
2025-12-03 16:52:20 +0900

    baekjoon 15921

---
+++ baekjoon bronze3/baekjoon15921/baekjoon15921.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double a, e = 0, sum = 0;
+	int n, value;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> value;
+		e += value / static_cast<double>(n);
+		sum += value;
+	}
+	if (n == 0 || e == 0) cout << "divide by zero";
+	else {
+		a = sum / n;
+		cout << fixed << setprecision(2) << (a / e);
+	}
+
+	return 0;
+}
