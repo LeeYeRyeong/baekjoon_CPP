07b5a670a4659ceac785e728eee910e6e21e3e45
ryxxng <i876838@gmail.com>
2025-12-03 16:51:11 +0900

    baekjoon 34823

---
+++ baekjoon bronze4/baekjoon34823/baekjoon34823.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int y, c, p;
+	cin >> y >> c >> p;
+	cout << ((y < c / 2 ? y : c / 2) < p ? (y < c / 2 ? y : c / 2) : p);
+
+	return 0;
+}
