c1474a7b0b6c513c5dfee21a8152192914d7fd1c
ryxxng <i876838@gmail.com>
2025-12-04 22:10:31 +0900

    baekjoon 28214

---
+++ baekjoon bronze3/baekjoon28214/baekjoon28214.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, k, p, bread = 0, cream, count = 0;
+	cin >> n >> k >> p;
+	for (int i = 0; i < n * k; i++) {
+		cin >> cream;
+		if (cream == 1) bread++;
+		if ((i + 1) % k == 0) {
+			count += ((bread > k - p) ? 1 : 0);
+			bread = 0;
+		}
+	}
+	cout << count;
+
+	return 0;
+}
