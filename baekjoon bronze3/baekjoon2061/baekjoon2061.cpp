e6fa2fe7fb6931d6fb41c6833c52ff385df0fe58
ryxxng <i876838@gmail.com>
2025-12-05 23:06:20 +0900

    baekjoon 2061

---
+++ baekjoon bronze3/baekjoon2061/baekjoon2061.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string k;
+	int l, n = 0, result = 0;
+	cin >> k >> l;
+	for (int i = 2; i < l; i++) {
+		for (int j = 0; j < k.size(); j++) result = (result * 10 + (k[j] - '0')) % i;
+		if (result == 0) {
+			n = i;
+			break;
+		}
+		result = 0;
+	}
+	if (n == 0) cout << "GOOD";
+	else cout << "BAD " << n;
+
+	return 0;
+}
