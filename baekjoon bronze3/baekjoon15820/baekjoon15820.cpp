4d700eeedcf6eff176b1f017f078ed0fe82ad8c9
ryxxng <i876838@gmail.com>
2025-12-05 23:07:40 +0900

    baekjoon 15820

---
+++ baekjoon bronze3/baekjoon15820/baekjoon15820.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sampleTC, systemTC, s, a;
+	bool systemCheck = true, sampleCheck = true;
+	cin >> sampleTC >> systemTC;
+	for (int i = 0; i < sampleTC; i++) {
+		cin >> s >> a;
+		if (s != a) sampleCheck = false;
+	}
+	for (int i = 0; i < systemTC; i++) {
+		cin >> s >> a;
+		if (s != a) systemCheck = false;
+	}
+	if (sampleCheck && systemCheck) cout << "Accepted";
+	else if (!sampleCheck) cout << "Wrong Answer";
+	else if (!systemCheck) cout << "Why Wrong!!!";
+
+	return 0;
+}
