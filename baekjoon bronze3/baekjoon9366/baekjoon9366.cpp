c4f9459efcb90ca5360e3ce852c6c0cbaad6890e
ryxxng <i876838@gmail.com>
2025-12-06 13:59:30 +0900

    baekjoon 9366

---
+++ baekjoon bronze3/baekjoon9366/baekjoon9366.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, temp, t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b >> c;
+		if (a > b) {
+			temp = a;
+			a = b;
+			b = temp;
+		}
+		if (b > c) {
+			temp = b;
+			b = c;
+			c = temp;
+		}
+		if (a > c) {
+			temp = a;
+			a = c;
+			c = temp;
+		}
+		cout << "Case #" << i + 1 << ": ";
+		if (a + b <= c) cout << "invalid!\n";
+		else if (a == b && b == c) cout << "equilateral\n";
+		else if (a == b || b == c || a == c) cout << "isosceles\n";
+		else cout << "scalene\n";
+	}
+
+	return 0;
+}
