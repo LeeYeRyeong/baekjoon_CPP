a43ea57df0808e23cb6c44a77da5f23736ff881e
ryxxng <i876838@gmail.com>
2025-12-07 22:58:18 +0900

    baekjoon 17094

---
+++ baekjoon bronze3/baekjoon17094/baekjoon17094.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, count2 = 0, countE = 0;
+    char c;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> c;
+        if (c == '2') count2++;
+        else if (c == 'e') countE++;
+    }
+    cout << (count2 == countE ? "yee" : (count2 > countE ? "2" : "e"));
+
+    return 0;
+}
