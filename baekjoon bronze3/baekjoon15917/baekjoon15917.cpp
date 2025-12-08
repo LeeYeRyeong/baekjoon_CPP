81b093795303f1dac4fe3d7be8d4fb26c8d947c6
ryxxng <i876838@gmail.com>
2025-12-08 22:01:28 +0900

    baekjoon 15917

---
+++ baekjoon bronze3/baekjoon15917/baekjoon15917.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int q, n;
+	bool check = false;
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cin >> q;
+	for (int i = 0; i < q; i++) {
+		cin >> n;
+		for (int j = 0; pow(2, j) <= n; j++) {
+			if (pow(2, j) == n) check = true;
+		}
+		cout << (check ? 1 : 0) << '\n';
+		check = false;
+	}
+
+	return 0;
+}
