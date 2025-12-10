93a8028fe9d95bfef8ed953571fd0b4c89acd340
ryxxng <i876838@gmail.com>
2025-12-10 21:08:47 +0900

    baekjoon 25703

---
+++ baekjoon bronze3/baekjoon25703/baekjoon25703.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << "int a;\nint *ptr = &a;\n";
+	for (int i = 1; i < n; i++) {
+		cout << "int ";
+		for (int j = 0; j <= i; j++) cout << "*";
+		cout << "ptr" << i + 1 << " = &ptr";
+		if (i != 1) cout << i;
+		cout << ";\n";
+	}
+
+	return 0;
+}
