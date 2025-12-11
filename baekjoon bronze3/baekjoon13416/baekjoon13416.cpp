09f3aa3bc158c76082082e4295aa633c4f3964a0
ryxxng <i876838@gmail.com>
2025-12-11 18:42:12 +0900

    baekjoon 13416

---
+++ baekjoon bronze3/baekjoon13416/baekjoon13416.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, a, b, c, max = 0, total = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		for (int j = 0; j < n; j++) {
+			cin >> a >> b >> c;
+			if (a > max) max = a;
+			if (b > max) max = b;
+			if (c > max) max = c;
+			total += max;
+			max = 0;
+		}
+		cout << total << '\n';
+		total = 0;
+	}
+
+	return 0;
+}
