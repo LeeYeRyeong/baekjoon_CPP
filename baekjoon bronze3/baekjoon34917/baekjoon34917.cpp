b7206c40e54378338e3d5d310a2f6e8deec9458b
ryxxng <i876838@gmail.com>
2025-12-12 21:31:42 +0900

    baekjoon 34917

---
+++ baekjoon bronze3/baekjoon34917/baekjoon34917.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, gap;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n;
+		gap = n - 1;
+		for (int j = 0; j < n; j++) {
+			for (int k = 0; k < n; k++) {
+				if (k == 0 || k == n - 1 || (j <= n / 2 && j == k) || (j <= n / 2 && j + gap == k)) cout << '#';
+				else cout << '.';
+			}
+			cout << '\n';
+			gap -= 2;
+		}
+	}
+
+	return 0;
+}
