dde74310dac576a4810facf1543d465df25d23ea
ryxxng <i876838@gmail.com>
2025-12-12 21:32:15 +0900

    baekjoon 11320

---
+++ baekjoon bronze3/baekjoon11320/baekjoon11320.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, a, b, count;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		count = ((a * a) % (b * b) == 0 ? (a * a) / (b * b) : (a * a) / (b * b) + 1);
+		cout << count << '\n';
+	}
+
+	return 0;
+}
