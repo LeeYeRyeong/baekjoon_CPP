012245de096bffb0e20f3eb4c10950b62cb3d884
ryxxng <i876838@gmail.com>
2025-12-12 21:33:45 +0900

    baekjoon 28014

---
+++ baekjoon bronze3/baekjoon28014/baekjoon28014.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, h, max = 0, count = 0;
+	cin >> n;
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	for (int i = 0; i < n; i++) {
+		cin >> h;
+		if (h >= max) count++;
+		max = h;
+	}
+	cout << count;
+
+	return 0;
+}
