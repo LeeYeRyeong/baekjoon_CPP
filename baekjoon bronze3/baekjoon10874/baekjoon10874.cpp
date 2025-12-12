f0ef9b4fc6a6c842827158516a07ff4e5862db44
ryxxng <i876838@gmail.com>
2025-12-12 21:32:48 +0900

    baekjoon 10874

---
+++ baekjoon bronze3/baekjoon10874/baekjoon10874.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int answer[10];
+	int n;
+	bool check = true;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 10; j++) cin >> answer[j];
+		for (int j = 0; j < 10; j++) {
+			if (answer[j] != j % 5 + 1) check = false;
+		}
+		if (check) cout << i + 1 << '\n';
+		check = true;
+	}
+
+	return 0;
+}
