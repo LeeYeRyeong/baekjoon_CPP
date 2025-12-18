ae40259b9dad17019ca6fcf50aae95eb78d53f53
ryxxng <i876838@gmail.com>
2025-12-18 22:44:04 +0900

    baekjoon 17350

---
+++ baekjoon bronze3/baekjoon17350/baekjoon17350.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	bool check = false;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		if (s == "anj") check = true;
+	}
+	cout << (check ? "뭐야;" : "뭐야?");
+
+	return 0;
+}
