17214879075f79743d05ff5e9f932e1368d97276
ryxxng <i876838@gmail.com>
2025-12-18 22:43:24 +0900

    baekjoon 16479

---
+++ baekjoon bronze3/baekjoon16479/baekjoon16479.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double h;
+	int k, d1, d2;
+	cin >> k >> d1 >> d2;
+	h = (k * k) - ((d1 - d2) / 2.0) * ((d1 - d2) / 2.0);
+	cout << fixed << setprecision(2) << h;
+
+	return 0;
+}
