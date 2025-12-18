62cf1626c5b36fc583b738448cb1a788c5fa09dc
ryxxng <i876838@gmail.com>
2025-12-18 22:44:33 +0900

    baekjoon 23803

---
+++ baekjoon bronze3/baekjoon23803/baekjoon23803.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < 5 * n; i++) {
+		if (i < 5 * n - n) {
+			for (int j = 0; j < n; j++) cout << '@';
+		}
+		else {
+			for (int j = 0; j < 5 * n; j++) cout << '@';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
