3d89695a9a89351035af81062ede0c4c20c36fa4
ryxxng <i876838@gmail.com>
2025-12-18 22:42:51 +0900

    baekjoon 11109

---
+++ baekjoon bronze3/baekjoon11109/baekjoon11109.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, d, n, s, p, totalS = 0, totalP = 0;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> d >> n >> s >> p;
+		totalS = s * n;
+		totalP = (p * n) + d;
+		cout << (totalS == totalP ? "does not matter\n" : totalS < totalP ? "do not parallelize\n" : "parallelize\n");
+	}
+
+	return 0;
+}
