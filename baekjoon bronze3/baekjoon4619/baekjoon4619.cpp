5deee9be983a1db54367328806380fc68ab6196a
ryxxng <i876838@gmail.com>
2025-12-19 21:05:53 +0900

    baekjoon 4619

---
+++ baekjoon bronze3/baekjoon4619/baekjoon4619.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int a, b, n, gap = 1000000;
+	while (1) {
+		cin >> b >> n;
+		if (b == 0 && n == 0) break;
+		for (int i = 1; pow(i, n) <= b; i++) {
+			if (b - pow(i, n) < gap) {
+				a = i;
+				gap = b - pow(i, n);
+			}
+		}
+		a = (gap < pow(a + 1, n) - b ? a : a + 1);
+		cout << a << '\n';
+		gap = 1000000;
+	}
+
+	return 0;
+}
