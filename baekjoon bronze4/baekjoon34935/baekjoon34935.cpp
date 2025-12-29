296a076e27c89a02a97bc2863bf1e2df7cbc795e
ryxxng <i876838@gmail.com>
2025-12-30 01:38:18 +0900

    baekjoon 34935

---
+++ baekjoon bronze4/baekjoon34935/baekjoon34935.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string pre, s;
+	int n;
+	bool check = true;
+	cin >> n >> s;
+	pre = s;
+	for (int i = 0; i < n - 1; i++) {
+		cin >> s;
+		if (s == pre) check = false;
+		pre = s;
+	}
+	cout << check;
+
+	return 0;
+}
