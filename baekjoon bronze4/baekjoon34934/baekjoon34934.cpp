711117be50a1143e4b8d9ab79dcce2533332c083
ryxxng <i876838@gmail.com>
2025-12-30 01:37:48 +0900

    baekjoon 34934

---
+++ baekjoon bronze4/baekjoon34934/baekjoon34934.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name, major;
+	int n, year, resultYear = 2000;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> name >> year;
+		if (year > resultYear) {
+			major = name;
+			resultYear = year;
+		}
+	}
+	cout << major;
+
+	return 0;
+}
