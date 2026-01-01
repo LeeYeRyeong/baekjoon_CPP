581c35f30e1df0ea3d83cae4feb51cf73ba875d2
ryxxng <i876838@gmail.com>
2026-01-01 22:44:08 +0900

    baekjoon 23348

---
+++ baekjoon bronze3/baekjoon23348/baekjoon23348.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, maxScore = 0, score = 0, oneHand, nolook, phone, n;
+	cin >> a >> b >> c >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 3; j++) {
+			cin >> oneHand >> nolook >> phone;
+			score += oneHand * a + nolook * b + phone * c;
+		}
+		maxScore = (maxScore > score ? maxScore : score);
+		score = 0;
+	}
+	cout << maxScore;
+
+	return 0;
+}
