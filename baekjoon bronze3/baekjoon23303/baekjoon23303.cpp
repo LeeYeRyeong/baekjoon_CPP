b8218ab14d9aa4009907594a3a31ec900a62f333
ryxxng <i876838@gmail.com>
2026-01-02 22:33:18 +0900

    baekjoon 23303

---
+++ baekjoon bronze3/baekjoon23303/baekjoon23303.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	bool check = false;
+	getline(cin, s);
+	for (int i = 0; i < s.size() - 1; i++) {
+		if ((s[i] == 'd' || s[i] == 'D') && s[i + 1] == '2') check = true;
+	}
+	cout << (check ? "D2" : "unrated");
+
+	return 0;
+}
