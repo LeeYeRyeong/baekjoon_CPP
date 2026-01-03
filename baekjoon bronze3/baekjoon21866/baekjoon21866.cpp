8c7c6f39fb2aed8a2464b52f012352aad743a979
ryxxng <i876838@gmail.com>
2026-01-03 23:31:56 +0900

    baekjoon 21866

---
+++ baekjoon bronze3/baekjoon21866/baekjoon21866.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int total = 0, score;
+	bool hacker = false;
+	for (int i = 0; i < 9; i++) {
+		cin >> score;
+		if (score > ((i / 2 + 1) * 100)) hacker = true;
+		total += score;
+	}
+	cout << (hacker ? "hacker" : total < 100 ? "none" : "draw");
+
+	return 0;
+}
