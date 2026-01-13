e5623960190017cc4000515c212421038b931825
ryxxng <i876838@gmail.com>
2026-01-13 22:24:20 +0900

    baekjoon 16483

---
+++ baekjoon bronze3/baekjoon16483/baekjoon16483.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double t, result;
+	cin >> t;
+	result = pow(t / 2, 2);
+	cout << fixed << setprecision(0) << result;
+
+	return 0;
+}
