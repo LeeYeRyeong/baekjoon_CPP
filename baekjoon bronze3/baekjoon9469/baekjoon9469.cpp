ea173a38ac8bdc84fb0356547d04785a8156e2f8
ryxxng <i876838@gmail.com>
2026-01-13 22:23:53 +0900

    baekjoon 9469

---
+++ baekjoon bronze3/baekjoon9469/baekjoon9469.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a, b, m, f, d;
+	int n, o;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> o >> m >> a >> b >> f;
+		d = (m / (a + b)) * f;
+		cout << o << ' ' << fixed << setprecision(6) << d << '\n';
+	}
+
+	return 0;
+}
