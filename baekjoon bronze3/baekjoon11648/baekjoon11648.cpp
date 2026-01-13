26c0a951358e460281845e08c09c68246b33554e
ryxxng <i876838@gmail.com>
2026-01-13 22:24:52 +0900

    baekjoon 11648

---
+++ baekjoon bronze3/baekjoon11648/baekjoon11648.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int count = 0, num = 1;
+	cin >> s;
+	while (s.size() > 1) {
+		count++;
+		for (int i = 0; i < s.size(); i++) num *= s[i] - '0';
+		s = to_string(num);
+		num = 1;
+	}
+	cout << count;
+
+	return 0;
+}
