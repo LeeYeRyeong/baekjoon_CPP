e0ac0d96298e71281bc14b94d5fa28d3dfbd111a
ryxxng <i876838@gmail.com>
2026-01-14 09:45:54 +0900

    baekjoon 15813

---
+++ baekjoon bronze3/baekjoon15813/baekjoon15813.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, score = 0;
+	char c;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> c;
+		score += c - 'A' + 1;
+	}
+	cout << score;
+
+	return 0;
+}
