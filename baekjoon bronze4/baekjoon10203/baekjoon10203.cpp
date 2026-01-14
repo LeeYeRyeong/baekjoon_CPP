0d54d0022a1cf443c8d19eda94874f8b40d517fc
ryxxng <i876838@gmail.com>
2026-01-14 09:44:32 +0900

    baekjoon 10203

---
+++ baekjoon bronze4/baekjoon10203/baekjoon10203.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string word;
+	int n, count = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> word;
+		for (int j = 0; j < word.size(); j++) {
+			if (word[j] == 'a' || word[j] == 'e' || word[j] == 'i' || word[j] == 'o' || word[j] == 'u') count++;
+		}
+		cout << "The number of vowels in " << word << " is " << count << ".\n";
+		count = 0;
+	}
+
+	return 0;
+}
