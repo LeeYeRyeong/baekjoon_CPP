db7dccb9c9fdf8ef6ef3ab4a5714727135db1e2c
ryxxng <i876838@gmail.com>
2026-01-14 09:45:10 +0900

    baekjoon 35097

---
+++ baekjoon bronze4/baekjoon35097/baekjoon35097.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, total = 0;
+	while (1) {
+		cin >> n;
+		if (n == 0) break;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				total += i * j;
+			}
+		}
+		cout << total << '\n';
+		total = 0;
+	}
+
+	return 0;
+}
