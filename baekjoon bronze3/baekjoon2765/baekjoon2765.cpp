f615f38e9177ed1676f852881541c9a298b45e9a
ryxxng <i876838@gmail.com>
2026-01-23 09:25:02 +0900

    baekjoon 2765

---
+++ baekjoon bronze3/baekjoon2765/baekjoon2765.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	static const double pi = 3.1415927;
+	double inch, distance, averageSpeed, sec;
+	int count, order = 1;
+	while (1) {
+		cin >> inch >> count >> sec;
+		if (count == 0) break;
+		distance = (inch / (12 * 5280)) * pi * count;
+		averageSpeed = distance / (sec / 3600);
+		cout << "Trip #" << order++ << ": " << fixed << setprecision(2) << distance << ' ' << averageSpeed << '\n';
+	}
+
+	return 0;
+}
