af9deadb2adc58df57148a4a9d3b8a07be69cdee
ryxxng <i876838@gmail.com>
2026-01-24 09:45:38 +0900

    baekjoon 10865

---
+++ baekjoon bronze3/baekjoon10865/baekjoon10865.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int friendCount[1000000] = {};
+	int n, m, a, b;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b;
+		friendCount[a - 1]++;
+		friendCount[b - 1]++;
+	}
+	for (int i = 0; i < n; i++) cout << friendCount[i] << '\n';
+
+	return 0;
+}
