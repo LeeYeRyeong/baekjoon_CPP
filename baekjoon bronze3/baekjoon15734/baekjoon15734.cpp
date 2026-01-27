b19cc80d682752cbbf2df3180ff17503ef8e0351
ryxxng <i876838@gmail.com>
2026-01-27 23:23:40 +0900

    baekjoon 15734

---
+++ baekjoon bronze3/baekjoon15734/baekjoon15734.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int l, r, a, player = 0;
+	cin >> l >> r >> a;
+	if (l + a < r) player = (l + a) * 2;
+	else if (r + a < l) player = (r + a) * 2;
+	else if (l == r) player = (l + (a / 2)) * 2;
+	else if ((l + r + a) % 2 == 0) player = l + r + a;
+	else player = l + r + a - 1;
+	cout << player;
+
+	return 0;
+}
