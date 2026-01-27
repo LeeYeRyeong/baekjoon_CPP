d910183cbba78b337168734d0ec90619655c7640
ryxxng <i876838@gmail.com>
2026-01-27 23:24:04 +0900

    baekjoon 21312

---
+++ baekjoon bronze3/baekjoon21312/baekjoon21312.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, mul = 1;
+	bool check = false;
+	cin >> a >> b >> c;
+	if (a % 2 == 1) {
+		mul *= a;
+		check = true;
+	}
+	if (b % 2 == 1) {
+		mul *= b;
+		check = true;
+	}
+	if (c % 2 == 1) {
+		mul *= c;
+		check = true;
+	}
+	cout << (check ? mul : a * b * c);
+
+	return 0;
+}
