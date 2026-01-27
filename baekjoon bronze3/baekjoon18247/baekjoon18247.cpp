e3f639dee30be50f93d3be9aa5cf85204739a617
ryxxng <i876838@gmail.com>
2026-01-27 23:24:34 +0900

    baekjoon 18247

---
+++ baekjoon bronze3/baekjoon18247/baekjoon18247.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, n, m, number = -1;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> n >> m;
+		if (n >= 12 && m >= 4) number = (11 * m) + 4;
+		cout << number << '\n';
+		number = -1;
+	}
+
+	return 0;
+}
