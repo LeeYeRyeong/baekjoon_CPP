08afecd6cf5df93bc766288392ec16c9ca7136dc
ryxxng <i876838@gmail.com>
2026-01-29 23:34:17 +0900

    baekjoon 14579

---
+++ baekjoon bronze3/baekjoon14579/baekjoon14579.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, result = 1, sum = 0;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		for (int j = 1; j <= i; j++) sum += j;
+		result = (result * sum) % 14579;
+		sum = 0;
+	}
+	cout << result;
+
+	return 0;
+}
