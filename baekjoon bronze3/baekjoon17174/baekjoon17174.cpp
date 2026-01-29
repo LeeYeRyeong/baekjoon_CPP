b1f561f45415eab087c09b5606a34728f263296f
ryxxng <i876838@gmail.com>
2026-01-30 08:52:29 +0900

    baekjoon 17174

---
+++ baekjoon bronze3/baekjoon17174/baekjoon17174.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, count = 0;
+	cin >> n >> m;
+	count += n;
+	while (n >= m) {
+		n /= m;
+		count += n;
+	}
+	cout << count;
+
+	return 0;
+}
