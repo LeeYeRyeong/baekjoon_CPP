30a797f3fd6117b3fb323e2bb99491e34259eee8
ryxxng <i876838@gmail.com>
2026-02-02 23:26:36 +0900

    baekjoon 23080

---
+++ baekjoon bronze3/baekjoon23080/baekjoon23080.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string security, result = "";
+	int n;
+	cin >> n >> security;
+	for (int i = 0; i < security.size(); i += n) result += security[i];
+	cout << result;
+
+	return 0;
+}
