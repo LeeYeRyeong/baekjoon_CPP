69c2547ab37b20b6aa80fa05c202f4efeb6d5116
ryxxng <i876838@gmail.com>
2026-02-03 23:58:46 +0900

    baekjoon 14913

---
+++ baekjoon bronze3/baekjoon14913/baekjoon14913.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, d, k, n;
+	cin >> a >> d >> k;
+	n = (((k - a) % d == 0 && ((k - a) * d >= 0)) ? (k - a) / d + 1 : -1);
+	if (n == -1) cout << 'X';
+	else cout << n;
+
+	return 0;
+}
