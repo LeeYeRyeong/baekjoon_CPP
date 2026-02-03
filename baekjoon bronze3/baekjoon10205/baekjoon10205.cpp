c9653fee1e69655b0f49e73096905da586c62e3c
ryxxng <i876838@gmail.com>
2026-02-03 23:58:13 +0900

    baekjoon 10205

---
+++ baekjoon bronze3/baekjoon10205/baekjoon10205.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, head;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> head >> s;
+		for (int j = 0; j < s.size(); j++) {
+			if (s[j] == 'c') head++;
+			else if (s[j] == 'b') head--;
+		}
+		cout << "Data Set " << i + 1 << ":\n" << (head > 0 ? head : 0) << "\n\n";
+	}
+
+	return 0;
+}
