0c08fa145db5e8df26f700d3c1ea9aab743e4874
ryxxng <i876838@gmail.com>
2026-02-04 23:16:02 +0900

    baekjoon 16504

---
+++ baekjoon bronze3/baekjoon16504/baekjoon16504.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long n, sum = 0, number;
+	cin >> n;
+	for (int i = 0; i < n * n; i++) {
+		cin >> number;
+		sum += number;
+	}
+	cout << sum;
+
+	return 0;
+}
