1022c41a3e96d1ef075a788bae48a18417a53d30
ryxxng <i876838@gmail.com>
2026-02-04 23:16:33 +0900

    baekjoon 27159

---
+++ baekjoon bronze3/baekjoon27159/baekjoon27159.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int card, num, sum = 0, pre;
+	cin >> card >> num;
+	sum += num;
+	pre = num;
+	for (int i = 0; i < card - 1; i++) {
+		cin >> num;
+		sum += (num - pre != 1 ? num : 0);
+		pre = num;
+	}
+	cout << sum;
+
+	return 0;
+}
