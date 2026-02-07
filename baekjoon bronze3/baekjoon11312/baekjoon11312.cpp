7af0e069dd8206f8754488d171b48a9026a2ad0a
ryxxng <i876838@gmail.com>
2026-02-07 23:52:14 +0900

    baekjoon 11312

---
+++ baekjoon bronze3/baekjoon11312/baekjoon11312.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long a, b;
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> b;
+		cout << (a * a) / (b * b) << '\n';
+	}
+
+	return 0;
+}
