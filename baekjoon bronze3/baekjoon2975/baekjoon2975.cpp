35961b13783f743d9e4045ef0e6ec09b4ef6057c
ryxxng <i876838@gmail.com>
2026-02-08 22:38:54 +0900

    baekjoon 2975

---
+++ baekjoon bronze3/baekjoon2975/baekjoon2975.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, value;
+	char c;
+	while (1) {
+		cin >> a >> c >> b;
+		if (a == 0 && b == 0) break;
+		value = (c == 'W' ? a - b : a + b);
+		if (value < -200) cout << "Not allowed\n";
+		else cout << value << '\n';
+	}
+
+	return 0;
+}
