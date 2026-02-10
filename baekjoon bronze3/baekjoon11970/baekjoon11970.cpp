db0b8f3915ff3ee8ba5ca789bbc2e5f478a98988
ryxxng <i876838@gmail.com>
2026-02-10 22:55:04 +0900

    baekjoon 11970

---
+++ baekjoon bronze3/baekjoon11970/baekjoon11970.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, line;
+	cin >> a >> b >> c >> d;
+	if ((b < c) || (a > d)) line = (b - a) + (d - c);
+	else line = (b > d ? b : d) - (a < c ? a : c);
+	cout << line;
+
+	return 0;
+}
