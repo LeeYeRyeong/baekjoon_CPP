a183bac1accbfc4921e5a39833b4b4987f3bed70
ryxxng <i876838@gmail.com>
2026-02-11 23:52:23 +0900

    baekjoon 29752

---
+++ baekjoon bronze3/baekjoon29752/baekjoon29752.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, maxstrick = 0, strick = 0, problem;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> problem;
+		if (problem == 0) {
+			maxstrick = (maxstrick < strick ? strick : maxstrick);
+			strick = 0;
+		}
+		else strick++;
+	}
+	maxstrick = (maxstrick < strick ? strick : maxstrick);
+	cout << maxstrick;
+
+	return 0;
+}
