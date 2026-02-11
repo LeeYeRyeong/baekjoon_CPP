89eabf86d9ec909c773e74bad568bee48bfb66f3
ryxxng <i876838@gmail.com>
2026-02-11 23:52:48 +0900

    baekjoon 23811

---
+++ baekjoon bronze3/baekjoon23811/baekjoon23811.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n * 5; i++) {
+		if ((i >= n * 0 && i < n) || (i >= n * 2 && i < n * 3) || (i >= n * 4)) {
+			for (int j = 0; j < n * 5; j++) cout << '@';
+		}
+		else {
+			for (int j = 0; j < n; j++) cout << '@';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
