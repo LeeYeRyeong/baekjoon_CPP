c7af729502650f6dc1ebde7c3fc0b903f1219c01
ryxxng <i876838@gmail.com>
2026-02-21 00:27:49 +0900

    baekjoon 15818

---
+++ baekjoon bronze3/baekjoon15818/baekjoon15818.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long n, m, mul = 1, number;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> number;
+		mul *= (number % m);
+		mul %= m;
+	}
+	cout << mul;
+
+	return 0;
+}
