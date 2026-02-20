3878c89f6ceea62148a98afbc83f053788633207
ryxxng <i876838@gmail.com>
2026-02-20 23:58:13 +0900

    baekjoon 13223

---
+++ baekjoon bronze3/baekjoon13223/baekjoon13223.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int startHour, startMin, startSec, endHour, endMin, endSec, h, m, s;
+	char c;
+	cin >> startHour >> c >> startMin >> c >> startSec >> endHour >> c >> endMin >> c >> endSec;
+	if (endSec < startSec) {
+		endMin--;
+		endSec += 60;
+	}
+	if (endMin < startMin) {
+		endHour--;
+		endMin += 60;
+	}
+	if (endHour < startHour) endHour += 24;
+	h = endHour - startHour, m = endMin - startMin, s = endSec - startSec;
+	h = (h == 24 ? 0 : h);
+	m = (m == 60 ? 0 : m);
+	s = (s == 60 ? 0 : s);
+	printf("%02d:%02d:%02d", h, m, s);
+
+	return 0;
+}
