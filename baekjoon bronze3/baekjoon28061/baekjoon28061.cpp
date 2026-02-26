d01a481e91923747d299279b632b50bf65184f7e
ryxxng <i876838@gmail.com>
2026-02-26 21:43:10 +0900

    baekjoon 28061

---
+++ baekjoon bronze3/baekjoon28061/baekjoon28061.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, lemon, max = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> lemon;
+		if (max < (lemon - (n - i))) max = lemon - (n - i);
+	}
+	cout << max;
+
+	return 0;
+}
