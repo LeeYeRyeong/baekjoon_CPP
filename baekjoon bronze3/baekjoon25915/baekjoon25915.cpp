0f634d1fbdcda691f5752e96307eb5bbfea1bf03
ryxxng <i876838@gmail.com>
2026-02-26 21:42:38 +0900

    baekjoon 25915

---
+++ baekjoon bronze3/baekjoon25915/baekjoon25915.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int count = 84;
+	char c;
+	cin >> c;
+	if (c < 'I') count += ('I' - c);
+	else count += (c - 'I');
+	cout << count;
+
+	return 0;
+}
