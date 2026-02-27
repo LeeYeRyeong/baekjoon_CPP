6ef8c393a25ad9c5db2bf538342d666e79e40175
ryxxng <i876838@gmail.com>
2026-02-27 09:49:46 +0900

    baekjoon 26264

---
+++ baekjoon bronze3/baekjoon26264/baekjoon26264.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, bigData = 0, security = 0;
+	cin >> n >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 's') security++;
+		else if (s[i] == 'b') bigData++;
+	}
+	if (bigData == security) cout << "bigdata? security!";
+	else if (bigData > security) cout << "bigdata?";
+	else cout << "security!";
+
+	return 0;
+}
