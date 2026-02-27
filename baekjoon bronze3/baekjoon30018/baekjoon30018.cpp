fd93b8f53f3add158251db06814877665af01c4a
ryxxng <i876838@gmail.com>
2026-02-27 09:49:19 +0900

    baekjoon 30018

---
+++ baekjoon bronze3/baekjoon30018/baekjoon30018.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[100], b[100];
+	int n, d, move = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)cin >> a[i];
+	for (int i = 0; i < n; i++)cin >> b[i];
+	for (int i = 0; i < n; i++) {
+		d = a[i] - b[i];
+		if (d > 0) move += d;
+	}
+	cout << move;
+
+	return 0;
+}
