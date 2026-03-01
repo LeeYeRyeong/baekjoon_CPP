4b1d68908434f8b61fdff4228fa95423966b37bd
ryxxng <i876838@gmail.com>
2026-03-01 09:56:25 +0900

    baekjoon 25630

---
+++ baekjoon bronze3/baekjoon25630/baekjoon25630.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n, count = 0;
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	cin >> n >> s;
+	for (int i = 0; i < s.size() / 2; i++) {
+		if (s[i] != s[s.size() - i - 1]) count++;
+	}
+	cout << count;
+
+	return 0;
+}
