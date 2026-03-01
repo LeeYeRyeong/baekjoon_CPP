7720d760a193ac3c88cac91463d6e1ef6d9f851a
ryxxng <i876838@gmail.com>
2026-03-01 09:56:00 +0900

    baekjoon 32978

---
+++ baekjoon bronze3/baekjoon32978/baekjoon32978.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string pre[1000], matter, forgot;
+	bool check[1000] = {};
+	int n;
+	cin >> n;
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	for (int i = 0; i < n; i++) {
+		cin >> pre[i];
+		check[i] = false;
+	}
+	for (int i = 0; i < n - 1; i++) {
+		cin >> matter;
+		for (int j = 0; j < n; j++) {
+			if (matter == pre[j]) {
+				check[j] = true;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (check[i] == false) {
+			forgot = pre[i];
+			break;
+		}
+	}
+	cout << forgot;
+
+	return 0;
+}
