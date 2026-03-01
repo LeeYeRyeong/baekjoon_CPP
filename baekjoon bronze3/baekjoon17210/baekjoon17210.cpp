3c3a93c59c7b2f2e099eb302b1d6555143e3556b
ryxxng <i876838@gmail.com>
2026-03-01 09:56:52 +0900

    baekjoon 17210

---
+++ baekjoon bronze3/baekjoon17210/baekjoon17210.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, first;
+	cin >> n >> first;
+	if (n >= 6) cout << "Love is open door";
+	else {
+		if (first == 0) {
+			for (int i = 0; i < n - 1; i++) {
+				if (i % 2 == 0) cout << 1 << '\n';
+				else cout << 0 << '\n';
+			}
+		}
+		else {
+			for (int i = 0; i < n - 1; i++) {
+				if (i % 2 == 0) cout << 0 << '\n';
+				else cout << 1 << '\n';
+			}
+		}
+	}
+
+	return 0;
+}
