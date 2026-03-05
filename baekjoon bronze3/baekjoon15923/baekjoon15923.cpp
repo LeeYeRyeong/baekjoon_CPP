9188869de6666c64e23333afc994589666db7ff9
ryxxng <i876838@gmail.com>
2026-03-05 09:43:35 +0900

    baekjoon 15923

---
+++ baekjoon bronze3/baekjoon15923/baekjoon15923.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, firstX, firstY, x, y, preX, preY, length = 0;
+	cin >> n >> firstX >> firstY;
+	preX = firstX, preY = firstY;
+	for (int i = 0; i < n - 1; i++) {
+		cin >> x >> y;
+		length += (preX > x ? preX - x : x - preX) + (preY > y ? preY - y : y - preY);
+		preX = x, preY = y;
+	}
+	length += (firstX > preX ? firstX - preX : preX - firstX) + (firstY > preY ? firstY - preY : preY - firstY);
+	cout << length;
+
+	return 0;
+}
