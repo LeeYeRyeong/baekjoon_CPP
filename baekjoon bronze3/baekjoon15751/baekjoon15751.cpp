f0670c6924a143ee94086f73ca93a583f822e3a1
ryxxng <i876838@gmail.com>
2026-03-10 23:18:32 +0900

    baekjoon 15751

---
+++ baekjoon bronze3/baekjoon15751/baekjoon15751.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, x, y, dis = 0;
+	cin >> a >> b >> x >> y;
+	dis = ((y - a > 0 ? y - a : a - y) + (x - b > 0 ? x - b : b - x) < (x - a > 0 ? x - a : a - x) + (y - b > 0 ? y - b : b - y) ?
+		(y - a > 0 ? y - a : a - y) + (x - b > 0 ? x - b : b - x) : (x - a > 0 ? x - a : a - x) + (y - b > 0 ? y - b : b - y));
+	dis = (dis < (a - b > 0 ? a - b : b - a) ? dis : (a - b > 0 ? a - b : b - a));
+	cout << dis;
+
+	return 0;
+}
