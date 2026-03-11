0bf013e831257e81c458ce4499a4a6dd21240e65
ryxxng <i876838@gmail.com>
2026-03-12 08:25:24 +0900

    baekjoon 5666

---
+++ baekjoon bronze3/baekjoon5666/baekjoon5666.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double average;
+	int h, p;
+	while (cin >> h >> p) {
+		average = static_cast<double>(h) / static_cast<double>(p);
+		cout << fixed << setprecision(2) << average << '\n';
+	}
+	
+	return 0;
+}
