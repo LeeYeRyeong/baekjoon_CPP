a345f652b08760aba5489b4f1b36918c43a2dc6e
ryxxng <i876838@gmail.com>
2026-03-11 18:42:21 +0900

    baekjoon 8932

---
+++ baekjoon bronze3/baekjoon8932/baekjoon8932.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a[7] = { 9.23076,1.84523,56.0211,4.99087,0.188807,15.9803,0.11193 },
+	b[7] = { 26.7,75,1.5,42.5,210,3.8,254 }, c[7] = { 1.835, 1.348, 1.05, 1.81, 1.41, 1.04, 1.88 };
+	int p, n, score = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 7; j++) {
+			cin >> p;
+			if (j == 0 || j == 3 || j == 6) score += (a[j] * pow((b[j] - p), c[j]));
+			else score += (a[j] * pow((p - b[j]), c[j]));
+		}
+		cout << score << '\n';
+		score = 0;
+	}
+
+	return 0;
+}
