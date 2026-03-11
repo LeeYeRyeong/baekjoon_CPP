fef697f358732da50134eadfb1aa7131c93545f1
ryxxng <i876838@gmail.com>
2026-03-12 08:25:47 +0900

    baekjoon 17201

---
+++ baekjoon bronze3/baekjoon17201/baekjoon17201.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int n;
+	bool check = true;
+	cin >> n >> s;
+	for (int i = 0; i < s.size() - 1; i++) {
+		if (s[i] == '1' && s[i + 1] != '2') check = false;
+		else if (s[i] == '2' && s[i + 1] != '1') check = false;
+	}
+	cout << (check ? "Yes" : "No");
+
+	return 0;
+}
