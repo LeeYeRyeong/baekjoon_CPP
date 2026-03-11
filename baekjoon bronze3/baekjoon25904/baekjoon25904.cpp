e85f81de372295e1dd5e61f8352651878eedd8b2
ryxxng <i876838@gmail.com>
2026-03-12 08:26:11 +0900

    baekjoon 25904

---
+++ baekjoon bronze3/baekjoon25904/baekjoon25904.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int voice[100] = {};
+	int n, x, index = 0;
+	cin >> n >> x;
+	for (int i = 0; i < n; i++) cin >> voice[i];
+	while (x <= voice[index]) {
+		x++;
+		index++;
+		if (index >= n) index = 0;
+	}
+	cout << index + 1;
+
+	return 0;
+}
