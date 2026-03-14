44265e9c1fd8bd1a39b83b3f2bf8ed553a5c97f7
ryxxng <i876838@gmail.com>
2026-03-14 10:05:31 +0900

    baekjoon 16503

---
+++ baekjoon bronze3/baekjoon16503/baekjoon16503.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c, answer1, answer2;
+	char math1, math2;
+	cin >> a >> math1 >> b >> math2 >> c;
+	if (math1 == '+') answer1 = a + b;
+	else if (math1 == '-') answer1 = a - b;
+	else if (math1 == '*') answer1 = a * b;
+	else if (math1 == '/') answer1 = a / b;
+	if (math2 == '+') answer1 += c;
+	else if (math2 == '-') answer1 -= c;
+	else if (math2 == '*') answer1 *= c;
+	else if (math2 == '/') answer1 /= c;
+	if (math2 == '+') answer2 = b + c;
+	else if (math2 == '-') answer2 = b - c;
+	else if (math2 == '*') answer2 = b * c;
+	else if (math2 == '/') answer2 = b / c;
+	if (math1 == '+') answer2 = a + answer2;
+	else if (math1 == '-') answer2 = a - answer2;
+	else if (math1 == '*') answer2 = a * answer2;
+	else if (math1 == '/') answer2 = a / answer2;
+	cout << (answer1 < answer2 ? answer1 : answer2) << '\n' << (answer1 > answer2 ? answer1 : answer2);
+
+	return 0;
+}
