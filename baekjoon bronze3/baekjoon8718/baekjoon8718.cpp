54c6f3cd7659ad405bf3ef6f2e3e206a1df2076b
ryxxng <i876838@gmail.com>
2026-03-17 23:58:44 +0900

    baekjoon 8718

---
+++ baekjoon bronze3/baekjoon8718/baekjoon8718.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, k, n;
+	cin >> x >> k;
+	x *= 1000, k *= 1000;
+	n = k * 2 + k * 4 + k;
+	if (n > x) n = k * 2 + k + k / 2;
+	if (n > x) n = k + k / 2 + k / 4;
+	if (n > x) n = 0;
+	cout << n;
+
+	return 0;
+}
