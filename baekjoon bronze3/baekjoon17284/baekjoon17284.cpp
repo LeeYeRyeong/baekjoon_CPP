32f7ea54bbdadd6c428cd870304bce1483ef763e
ryxxng <i876838@gmail.com>
2026-03-17 23:52:03 +0900

    baekjoon 17284

---
+++ baekjoon bronze3/baekjoon17284/baekjoon17284.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, money = 5000;
+	while (cin >> n) {
+		if (n == 1) money -= 500;
+		else if (n == 2) money -= 800;
+		else if (n == 3) money -= 1000;
+	}
+	cout << money;
+
+	return 0;
+}
