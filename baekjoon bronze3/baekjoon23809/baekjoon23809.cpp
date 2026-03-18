f40b39adffc9be48d6c1935a8c2ae11ee8e5a9f6
ryxxng <i876838@gmail.com>
2026-03-18 19:15:40 +0900

    baekjoon 23809

---
+++ baekjoon bronze3/baekjoon23809/baekjoon23809.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n * 5; j++) {
+			if (j < n || j >= n * (5 - 1)) cout << '@';
+			else cout << ' ';
+		}
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n * 4; j++) {
+			if (j < n || (j >= n * (4 - 1))) cout << '@';
+			else cout << ' ';
+		}
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n * 3; j++) {
+			cout << '@';
+		}
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n * 4; j++) {
+			if (j < n || (j >= n * (4 - 1))) cout << '@';
+			else cout << ' ';
+		}
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n * 5; j++) {
+			if (j < n || j >= n * (5 - 1)) cout << '@';
+			else cout << ' ';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
