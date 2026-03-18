65e127cb27963f4043842b03721a6ce03561a3f5
ryxxng <i876838@gmail.com>
2026-03-18 19:15:16 +0900

    baekjoon 19572

---
+++ baekjoon bronze3/baekjoon19572/baekjoon19572.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double a, b, c;
+	int d1, d2, d3;
+	cin >> d1 >> d2 >> d3;
+	a = (d1 + (d2 - d3)) / 2.0;
+	b = d1 - a;
+	c = d2 - a;
+	if (a <= 0 || b <= 0 || c <= 0) cout << -1;
+	else cout << 1 << '\n' << fixed << setprecision(1) << a << ' ' << b << ' ' << c;
+
+	return 0;
+}
