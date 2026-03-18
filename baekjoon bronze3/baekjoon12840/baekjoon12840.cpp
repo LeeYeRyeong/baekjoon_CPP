41363a49c8eee0eff51792ef3f9ed05766d30d2d
ryxxng <i876838@gmail.com>
2026-03-18 19:16:08 +0900

    baekjoon 12840

---
+++ baekjoon bronze3/baekjoon12840/baekjoon12840.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int h, m, s, n, q, c, time = 0;
+	cin >> h >> m >> s >> n;
+	time = h * 3600 + m * 60 + s;
+	for (int i = 0; i < n; i++) {
+		cin >> q;
+		if (q == 1) {
+			cin >> c;
+			time += c;
+		}
+		else if (q == 2) {
+			cin >> c;
+			time -= c;
+		}
+		else if (q == 3) {
+			time %= 86400;
+			if (time < 0) time += 86400;
+			h = time / 3600;
+			m = time % 3600 / 60;
+			s = time % 60;
+			if (s < 0) {
+				m--;
+				s += 60;
+			}
+			else if (s > 59) {
+				m++;
+				s %= 60;
+			}
+			if (m < 0) {
+				h--;
+				m += 60;
+			}
+			else if (m > 59) {
+				h++;
+				m %= 60;
+			}
+			if (h < 0) h +=
+				24;
+			else if (h > 23) h %= 24;
+			cout << h << ' ' << m << ' ' << s << '\n';
+		}
+	}
+
+	return 0;
+}
