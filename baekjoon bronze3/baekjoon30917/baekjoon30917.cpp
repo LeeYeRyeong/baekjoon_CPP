82ab72dc6e368e9fbd1ea64e610db4c6d7899c23
ryxxng <i876838@gmail.com>
2026-03-19 10:07:16 +0900

    baekjoon 30917

---
+++ baekjoon bronze3/baekjoon30917/baekjoon30917.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 1, b = 1, c;
+	cout << "? A " << a << '\n';
+	while (1) {
+		cin >> c;
+		if (c == 1) break;
+		cout << "? A " << ++a << '\n';
+	}
+	cout << "? B " << b << '\n';
+	while (1) {
+		cin >> c;
+		if (c == 1) break;
+		cout << "? B " << ++b << '\n';
+	}
+	cout << "! " << a + b;
+
+	return 0;
+}
