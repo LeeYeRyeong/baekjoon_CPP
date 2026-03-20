18db80283c8d84bb711e3dc1c9e5bd1feaafa7a6
ryxxng <i876838@gmail.com>
2026-03-20 09:30:12 +0900

    baekjoon 28289

---
+++ baekjoon bronze3/baekjoon28289/baekjoon28289.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int p, g, c, n, software = 0, embedded = 0, ai = 0, grade1 = 0;
+	cin >> p;
+	for (int i = 0; i < p; i++) {
+		cin >> g >> c >> n;
+		if (g == 1) grade1++;
+		else if (c == 1 || c == 2) software++;
+		else if (c == 3) embedded++;
+		else if (c == 4) ai++;
+	}
+	cout << software << '\n' << embedded << '\n' << ai << '\n' << grade1;
+
+	return 0;
+}
