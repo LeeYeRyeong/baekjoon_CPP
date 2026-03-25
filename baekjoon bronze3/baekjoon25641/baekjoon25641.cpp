50aa5bb7587f15296cfa4a75ff2d35c3a610c908
ryxxng <i876838@gmail.com>
2026-03-25 22:57:50 +0900

    baekjoon 25641

---
+++ baekjoon bronze3/baekjoon25641/baekjoon25641.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n, countS = 0, countT = 0, index;
+	string s;
+	cin >> n >> s;
+	for (int i = 0; i < s.size(); i++) {
+		for (int j = i; j < s.size(); j++) {
+			if (s[j] == 's')countS++;
+			else if (s[j] == 't') countT++;
+		}
+		if (countS == countT) {
+			index = i;
+			break;
+		}
+		countS = 0, countT = 0;
+	}
+	for (int i = index; i < s.size(); i++) cout << s[i];
+
+	return 0;
+}
