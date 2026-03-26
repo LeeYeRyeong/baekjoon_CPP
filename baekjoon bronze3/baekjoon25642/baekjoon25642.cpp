1678b93fe8b5b6e08a1c50d5ca228c20b7f8bd65
ryxxng <i876838@gmail.com>
2026-03-27 00:25:22 +0900

    baekjoon 25642

---
+++ baekjoon bronze3/baekjoon25642/baekjoon25642.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	while (a < 5 && b < 5) {
+		b += a;
+		if (b >= 5) {
+			cout << "yt";
+			break;
+		}
+		a += b;
+		if (a >= 5) {
+			cout << "yj";
+			break;
+		}
+	}
+
+	return 0;
+}
