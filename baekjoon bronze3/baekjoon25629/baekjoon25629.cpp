d1debd29c5b7226887ff28aab29e7fc0dbe98de6
ryxxng <i876838@gmail.com>
2026-03-26 23:42:39 +0900

    baekjoon 25629

---
+++ baekjoon bronze3/baekjoon25629/baekjoon25629.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, a, even = 0, odd = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+		if (a % 2 == 0) even++;
+		else odd++;
+	}
+	cout << (n % 2 == 0 ? (even == odd ? 1 : 0) : (odd == even + 1 ? 1 : 0));
+
+	return 0;
+}
