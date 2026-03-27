2d686a40d0ba49123e66eac3c218235bb4f6bfdb
ryxxng <i876838@gmail.com>
2026-03-28 02:18:33 +0900

    baekjoon 11772

---
+++ baekjoon bronze3/baekjoon11772/baekjoon11772.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	long sum = 0;
+	int n, top, bottom;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		//cout << "substr : " << s.substr(0, s.size() - 1) << '\n';
+		bottom = s[s.size() - 1] - '0';
+		top = stoi(s.substr(0, s.size() - 1));
+		sum += pow(top, bottom);
+	}
+	cout << sum;
+
+	return 0;
+}
