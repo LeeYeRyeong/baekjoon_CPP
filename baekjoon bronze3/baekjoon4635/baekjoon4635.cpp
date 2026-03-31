f00ae9bab50aea1313f52f7bd4e8a0e3481e0752
ryxxng <i876838@gmail.com>
2026-03-31 21:06:08 +0900

    baekjoon 4635

---
+++ baekjoon bronze3/baekjoon4635/baekjoon4635.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, speed, time, preTime = 0, distance = 0;
+	while (1) {
+		cin >> n;
+		if (n == -1) break;
+		for (int i = 0; i < n; i++) {
+			cin >> speed >> time;
+			distance += (time - preTime) * speed;
+			preTime = time;
+		}
+		cout << distance << " miles\n";
+		distance = 0, preTime = 0;
+	}
+
+	return 0;
+}
