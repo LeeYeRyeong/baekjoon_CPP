7340132cdce73a42e4c7691e59ab5ccacc24c590
ryxxng <i876838@gmail.com>
2026-03-31 21:06:36 +0900

    baekjoon 30958

---
+++ baekjoon bronze3/baekjoon30958/baekjoon30958.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int max[26] = {};
+	int n, count = 0;
+	char c;
+	cin >> n;
+	cin.ignore();
+	getline(cin, s);
+	for (int i = 0; i < n; i++) {
+		if (s[i] >= 'a' && s[i] <= 'z') max[s[i] - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		if (max[i] > count) count = max[i];
+	}
+	cout << count;
+
+	return 0;
+}
