6293d92f08f51a7a588e55484a672167e97d44d0
ryxxng <i876838@gmail.com>
2026-04-02 00:45:10 +0900

    baekjoon 26594

---
+++ baekjoon bronze3/baekjoon26594/baekjoon26594.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	int count = 1;
+	cin >> s;
+	for (int i = 1; s[i] == s[0]; i++) {
+		if (s[i] == s[0]) count++;
+	}
+	cout << count;
+
+	return 0;
+}
