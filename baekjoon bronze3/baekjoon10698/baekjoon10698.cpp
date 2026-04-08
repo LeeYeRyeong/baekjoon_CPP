063590a978de6070d0144003abb79895643e196f
ryxxng <i876838@gmail.com>
2026-04-08 21:49:04 +0900

    baekjoon 10698

---
+++ baekjoon bronze3/baekjoon10698/baekjoon10698.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, a, b, answer, corretAnswer;
+	char c, e;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> a >> c >> b >> e >> answer;
+		if (c == '+') corretAnswer = a + b;
+		else if (c == '-') corretAnswer = a - b;
+		cout << "Case " << i + 1 << ": " << (corretAnswer == answer ? "YES\n" : "NO\n");
+	}
+
+	return 0;
+}
