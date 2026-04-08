d90b2e73606d774ee9bc4ddd32de9aca46e6f7bb
ryxxng <i876838@gmail.com>
2026-04-08 21:48:26 +0900

    baekjoon 31628

---
+++ baekjoon bronze3/baekjoon31628/baekjoon31628.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string matrix[10][10] = {};
+	bool check = true, result = false;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			cin >> matrix[i][j];
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (matrix[i][0] != matrix[i][j]) check = false;
+		}
+		if (check) result = true;
+		else check = true;
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (matrix[0][i] != matrix[j][i]) check = false;
+		}
+		if (check) result = true;
+		else check = true;
+	}
+	cout << result;
+
+	return 0;
+}
