e300bc4886a1599dbf4c1b3ee78da6358e12a7f5
ryxxng <i876838@gmail.com>
2026-04-08 21:49:36 +0900

    baekjoon 26040

---
+++ baekjoon bronze3/baekjoon26040/baekjoon26040.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	char c;
+	cin >> s;
+	cin.tie(0);
+	while (cin >> c) {
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == c) s[i] += 32;
+		}
+	}
+	cout << s;
+
+	return 0;
+}
