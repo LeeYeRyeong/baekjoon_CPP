b50298f72b8ee55d470fc2bcb95aad9d6d5ad5b4
ryxxng <i876838@gmail.com>
2026-04-09 23:35:20 +0900

    baekjoon 23627

---
+++ baekjoon bronze3/baekjoon23627/baekjoon23627.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s, driip = "driip";
+	bool cute = true;
+	cin >> s;
+	if (s.size() < 5) cute = false;
+	else {
+		for (int i = s.size() - 5; i < s.size(); i++) {
+			if (s[i] != driip[i - (s.size() - 5)]) cute = false;
+		}
+	}
+	cout << (cute ? "cute" : "not cute");
+
+	return 0;
+}
