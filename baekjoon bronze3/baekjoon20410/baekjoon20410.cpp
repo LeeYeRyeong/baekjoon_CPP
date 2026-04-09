51353f371779ddfe12f6815897f75daf02252c4a
ryxxng <i876838@gmail.com>
2026-04-10 01:20:12 +0900

    baekjoon 20410

---
+++ baekjoon bronze3/baekjoon20410/baekjoon20410.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, seed, x1, x2, a, c;
+	bool check = false;
+	cin >> m >> seed >> x1 >> x2;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) {
+			if ((i * seed + j) % m == x1 && ((i * x1 + j) % m == x2)) {
+				a = i;
+				c = j;
+				check = true;
+				break;
+			}
+		}
+		if (check) break;
+	}
+	cout << a << ' ' << c;
+
+	return 0;
+}
