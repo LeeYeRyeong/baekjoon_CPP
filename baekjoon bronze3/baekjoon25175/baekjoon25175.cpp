a27db7370ba3618b8ae42e9516e1f910f472aff5
ryxxng <i876838@gmail.com>
2026-04-11 01:47:40 +0900

    baekjoon 25175

---
+++ baekjoon bronze3/baekjoon25175/baekjoon25175.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, m, k, next;
+	cin >> n >> m >> k;
+	if (k < 3) next = (n + ((m + (k - 3) % n))) % n;
+	else next = (m + (k - 3)) % n;
+	cout << (next == 0 ? n : next);
+
+	return 0;
+}
