b430f15a74b403d2c0db5c6754a76f0fb6632d0a
ryxxng <i876838@gmail.com>
2026-04-10 10:00:13 +0900

    baekjoon 14682

---
+++ baekjoon bronze3/baekjoon14682/baekjoon14682.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int n, k, sum = 0;
+	cin >> n >> k;
+	sum += n;
+	for (int i = 0; i < k; i++) sum += (n * pow(10, i + 1));
+	cout << sum;
+
+	return 0;
+}
