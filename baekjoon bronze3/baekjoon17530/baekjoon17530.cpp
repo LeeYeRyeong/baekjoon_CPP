3359d8cf1ff52bb1ad05fd57a585fa5e79f9b0ae
ryxxng <i876838@gmail.com>
2026-04-11 01:48:10 +0900

    baekjoon 17530

---
+++ baekjoon bronze3/baekjoon17530/baekjoon17530.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n, vote, max = 0, maxIndex = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> vote;
+		if (max < vote) {
+			max = vote;
+			maxIndex = i + 1;
+		}
+	}
+	cout << (maxIndex == 1 ? 'S' : 'N');
+
+	return 0;
+}
